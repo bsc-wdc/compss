26547db31fbf4a2a07359a98e43949710adada48
palvarez <palvarez@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-04-19 08:50:10 +0000

    Added emitEvent to commons-binding
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2059 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/programming_model/bindings/bindings-common/src/GS_compss.h
+++ compss/programming_model/bindings/bindings-common/src/GS_compss.h
@@ -13,5 +13,6 @@ extern "C" void GS_ExecuteTask(long appId, char *class_name, char *method_name, 
 extern "C" void GS_Get_File(char *file_name, int mode, char **buf);
 extern "C" void GS_Get_AppDir(char **buf);
 extern "C" void GS_RegisterCE(long appId, char *class_name, char *method_name, int priority, int has_target, int num_params, void **params, char *constraints);
+extern "C" void GS_EmitEvent(int type, long id);
 
 #endif /* GS_COMPSS_H */

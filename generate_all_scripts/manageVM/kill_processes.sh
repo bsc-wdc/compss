fec6de29dd8d35e9e20f338a56b1e4973a819b5a
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-04-19 13:31:22 +0000

    Move kill procs to clean pre-snapshot
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2066 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- generate_all_scripts/manageVM/kill_processes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-  
-  # Kill possible remaining processes of tests
-  exitValue=$(ps -elfa | grep java | grep "integratedtoolkit.nio.worker.NIOWorker" | awk {' print $4 '} | xargs -r kill -9)
-
-  exit $exitValue
-

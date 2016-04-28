1743d303a7626c6d4689d1e9ad78f459b46c4edb
palvarez <palvarez@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-04-28 06:42:54 +0000

    Added MPI parallel merger support if present in installation
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2091 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/runtime/scripts/system/trace.sh
+++ compss/runtime/scripts/system/trace.sh
@@ -67,7 +67,13 @@
         rm -rf $tmpDir $file
     done
     sec=$(/bin/date +%s)
-    ${extraeDir}/bin/mpi2prv -f TRACE.mpits -o ./trace/${appName}_compss_trace_${sec}.prv
+    # Check if parallel merge is available
+    configuration=$(${extraeDir}/etc/configured.sh | grep "enable-parallel-merge")
+    if [ -z "${configuration}" ]; then
+        ${extraeDir}/bin/mpi2prv -f TRACE.mpits -o ./trace/${appName}_compss_trace_${sec}.prv
+    else
+        mpirun -np $numberOfResources ${extraeDir}/bin/mpimpi2prv -f TRACE.mpits -o ./trace/${appName}_compss_trace_${sec}.prv
+    fi
     endCode=$?
     rm -rf set-0/ TRACE.mpits TRACE.sym
   fi

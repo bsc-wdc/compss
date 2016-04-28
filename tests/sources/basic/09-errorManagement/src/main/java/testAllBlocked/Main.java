adf6b383b56db4d7c83d486d5b70dbf6d3de47de
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-04-28 10:02:18 +0000

    Update errorManagement test to handle rescheduled tasks
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2096 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- tests/sources/basic/09-errorManagement/src/main/java/testAllBlocked/Main.java
+++ tests/sources/basic/09-errorManagement/src/main/java/testAllBlocked/Main.java
@@ -1,30 +1,31 @@
 package testAllBlocked;
 
-//No available workers to reschedule to...
-//
-// (using impossible constraints (see MainItf.java))
-public class Main 
-{
-	private static int NUM_TASKS = 2;
+
+// No available workers to execute the application
+// (using impossible constraints - see MainItf.java)
+public class Main {
 	
-	public static void main(String[] args)
-	{
+	private static int NUM_TASKS = 2;
+
+	public static void main(String[] args) {
 		Dummy[] dummies = new Dummy[NUM_TASKS];
-		for(int i = 0; i < NUM_TASKS; ++i)
-		{
+		for (int i = 0; i < NUM_TASKS; ++i) {
 			dummies[i] = normalTask(i, new Dummy());
 		}
-		
-		for(int i = 0; i < NUM_TASKS; ++i)
-		{
-			System.out.println("Finished task " + (i+1) + " (" + dummies[i] + ")");
+
+		for (int i = 0; i < NUM_TASKS; ++i) {
+			System.out.println("Finished task " + (i + 1) + " (" + dummies[i] + ")");
 		}
 	}
-	
-	public static Dummy normalTask(int x, Dummy din)
-	{
-		Dummy d = null; 
-		try { Thread.sleep(2000); } catch(Exception e) {}
+
+	public static Dummy normalTask(int x, Dummy din) {
+		Dummy d = null;
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// No need to catch such exception
+		}
 		return d;
 	}
+	
 }
--- tests/sources/basic/09-errorManagement/src/main/java/testNonExistingInFile/MainItf.java
+++ tests/sources/basic/09-errorManagement/src/main/java/testNonExistingInFile/MainItf.java
@@ -5,11 +5,10 @@ import integratedtoolkit.types.annotations.Parameter;
 import integratedtoolkit.types.annotations.Parameter.Direction;
 import integratedtoolkit.types.annotations.Parameter.Type;
 
-public interface MainItf 
-{
-	@Method(declaringClass="testNonExistingInFile.Main")
-	Dummy errorTask
-	(
-			@Parameter(type = Type.FILE, direction = Direction.IN) String file
+
+public interface MainItf {
+	@Method(declaringClass = "testNonExistingInFile.Main")
+	Dummy errorTask(
+		@Parameter(type = Type.FILE, direction = Direction.IN) String file
 	);
 }

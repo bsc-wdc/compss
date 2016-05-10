261a185dcb86f3d427cc5f19d5f1d8743bce9182
palvarez <palvarez@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-05-10 13:39:51 +0000

    Removed comment loggers
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2144 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/ExternalExecutor.java
+++ compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/ExternalExecutor.java
@@ -76,8 +76,6 @@ public abstract class ExternalExecutor extends Executor {
 
     @Override
     void removeSandBox(String sandBox) throws IOException {
-        logger.debug("callin' removeSandBox(string) upon "+ sandBox + " xlcs");
-
         File wdirFile = new File(sandBox);
         removeSandBox(wdirFile);
     }

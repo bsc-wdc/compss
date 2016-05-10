f79f7d54eaf29a4191185c7d7b02ba409cef75d0
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-05-10 15:35:39 +0000

    Fixed test 19
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2151 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- tests/sources/basic/19-test_python/test/test_mp.py
+++ tests/sources/basic/19-test_python/test/test_mp.py
@@ -9,13 +9,13 @@ PyCOMPSs Testbench
 """
 
 from pycompss.api.api import compss_open, compss_wait_on
-from test.test_tasks import function_primitives, function_files, function_objects, function_return_primitive
-from test.test_tasks import function_return_object, MyClass, par_func
-from test.test_tasks import function_function_parameter, formula2, function_default_parameter_values, function_order_parameters
-from test.test_tasks import function_fu_object, function_fu_in_task
-from test.test_tasks import function_fu_list_object, function_fu_list_in_task
-from test.test_tasks import function_iterable_object_wait, function_wait_on_string
-from test.test_tasks import function_time_decorated_master, function_time_decorated_worker
+from modules.test_tasks import function_primitives, function_files, function_objects, function_return_primitive
+from modules.test_tasks import function_return_object, MyClass, par_func
+from modules.test_tasks import function_function_parameter, formula2, function_default_parameter_values, function_order_parameters
+from modules.test_tasks import function_fu_object, function_fu_in_task
+from modules.test_tasks import function_fu_list_object, function_fu_list_in_task
+from modules.test_tasks import function_iterable_object_wait, function_wait_on_string
+from modules.test_tasks import function_time_decorated_master, function_time_decorated_worker
 
 
 def main_program():

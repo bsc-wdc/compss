89efcffc2e9f429f812f018561368887905679e5
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-05-19 07:46:49 +0000

    Add test 24
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2166 9ab3861e-6c05-4e1b-b5ef-99af60850597

---
+++ tests/sources/basic/24-noTask/src/main/java/noTask/Main.java
@@ -0,0 +1,34 @@
+package noTask;
+
+
+public class Main {
+	
+	private static final int SLEEP_TIME = 5_000;
+
+	public static void main(String[] args) {
+		// Check and get parameters
+		if (args.length != 0) {
+			System.out.println("[ERROR] Bad number of parameters");
+			System.out.println("    Usage: noTask.Main");
+			System.exit(-1);
+		}
+
+		// ------------------------------------------------------------------------
+		// Write a hello message from a function that it is NOT a task
+		String msg = "Hello World";
+		printMessage(msg);
+	}
+
+	public static void printMessage(String msg) {
+		// Sleep to allow runtime workers to be ready
+		try {
+			Thread.sleep(SLEEP_TIME);
+		} catch (InterruptedException e) {
+			// No need to handle such exceptions
+		}
+		
+		// Print out message
+		System.out.println(msg);
+	}
+
+}

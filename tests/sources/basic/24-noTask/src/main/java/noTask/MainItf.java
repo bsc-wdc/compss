40f7e5e4ccd49c1fa13fe53004009667e893fae1
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-05-19 07:46:49 +0000

    Add test 24
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2166 9ab3861e-6c05-4e1b-b5ef-99af60850597

---
+++ tests/sources/basic/24-noTask/src/main/java/noTask/MainItf.java
@@ -0,0 +1,12 @@
+package noTask;
+
+
+public interface MainItf {
+	
+	/*@Method(declaringClass = "noTask.Main")
+	void printMessage(
+		@Parameter(type = Type.STRING, direction = Direction.IN)
+		String msg
+	);*/
+
+}

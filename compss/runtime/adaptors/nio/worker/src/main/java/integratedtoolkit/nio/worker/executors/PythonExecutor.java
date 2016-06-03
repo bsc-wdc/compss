e2adeff8dd9f0299b3ba22ccd7b44942de3367b5
palvarez <palvarez@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-06-03 10:11:38 +0000

    Python tasks tracing proof of concept working
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2205 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/PythonExecutor.java
+++ compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/PythonExecutor.java
@@ -12,6 +12,9 @@ public class PythonExecutor extends ExternalExecutor {
 	
 	private static final String PYCOMPSS_RELATIVE_PATH = File.separator + "Bindings" + File.separator + "python";
 	private static final String WORKER_PY_RELATIVE_PATH = File.separator + "pycompss" + File.separator + "worker" + File.separator + "worker.py";
+	private static final String PYEXTRAE_RELATIVE_PATH = File.separator + "Dependencies" + File.separator + "extrae" + File.separator + "libexec";
+	private static final String WORKER_TRACING_CONFIG_FILE_RELATIVE_PATH = File.separator + "Runtime" + File.separator + "configuration" +
+            File.separator + "xml" + File.separator + "tracing";
 
 	@Override
 	public ArrayList<String> getLaunchCommand(NIOTask nt) {
@@ -20,7 +23,7 @@ public class PythonExecutor extends ExternalExecutor {
 		/*lArgs.add("/bin/bash");
 		lArgs.add("-e");
 		lArgs.add("-c");*/
-		lArgs.add("export EXTRAE_CONFIG_FILE=INSERT_PATH_HERE/extrae_task.xml;");
+		lArgs.add("export EXTRAE_CONFIG_FILE=" + nt.getInstallDir() + WORKER_TRACING_CONFIG_FILE_RELATIVE_PATH + File.separator + "extrae_task.xml;");
 		lArgs.add("python");
 		lArgs.add("-u");
 		lArgs.add(pycompssHome + WORKER_PY_RELATIVE_PATH);
@@ -41,6 +44,12 @@ public class PythonExecutor extends ExternalExecutor {
 		} else {
 			pythonPath = pycompssHome + ":" + nt.getPythonPath() + ":" + nt.getAppDir() + pythonPath;
 		}
+
+        // ADD PYEXTRAE IF TRACING
+        if (tracing){
+            String pyextrae_path = nt.getInstallDir() + PYEXTRAE_RELATIVE_PATH;
+            pythonPath += ":" + pyextrae_path;
+        }
 		env.put("PYTHONPATH", pythonPath);
 		
 		// LD_LIBRARY_PATH

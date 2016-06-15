16ea23a673f4c83221fcd45e4f3b116326ef9d7f
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-06-15 09:51:35 +0000

    Addded lib and libexec to pythonpath in python executor for pyextrae
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2226 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/PythonExecutor.java
+++ compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/PythonExecutor.java
@@ -11,8 +11,13 @@ import java.util.Map;
 public class PythonExecutor extends ExternalExecutor {
 	
 	private static final String PYCOMPSS_RELATIVE_PATH = File.separator + "Bindings" + File.separator + "python";
+	private static final String EXTRAE_RELATIVE_PATH = File.separator + "Dependencies" + File.separator + "extrae";
+	
 	private static final String WORKER_PY_RELATIVE_PATH = File.separator + "pycompss" + File.separator + "worker" + File.separator + "worker.py";
-	private static final String PYEXTRAE_RELATIVE_PATH = File.separator + "Dependencies" + File.separator + "extrae" + File.separator + "libexec";
+	
+	private static final String LIBEXEC_EXTRAE_RELATIVE_PATH = EXTRAE_RELATIVE_PATH + File.separator + "libexec";
+	private static final String LIB_EXTRAE_RELATIVE_PATH = EXTRAE_RELATIVE_PATH + File.separator + "lib";
+	
 	private static final String WORKER_TRACING_CONFIG_FILE_RELATIVE_PATH = File.separator + "Runtime" + File.separator + "configuration" +
             File.separator + "xml" + File.separator + "tracing";
 
@@ -20,9 +25,7 @@ public class PythonExecutor extends ExternalExecutor {
 	public ArrayList<String> getLaunchCommand(NIOTask nt, String sandBox) {
 		ArrayList<String> lArgs = new ArrayList<String>();
 		String pycompssHome = nt.getInstallDir() + PYCOMPSS_RELATIVE_PATH;
-		/*lArgs.add("/bin/bash");
-		lArgs.add("-e");
-		lArgs.add("-c");*/
+
 		if (tracing){
 			int taskId = nt.getTaskId();
 			String baseConfigFile = nt.getInstallDir() + WORKER_TRACING_CONFIG_FILE_RELATIVE_PATH + File.separator + "extrae_task.xml";
@@ -30,9 +33,11 @@ public class PythonExecutor extends ExternalExecutor {
 			lArgs.add("echo $(sed s/{{NAME}}/task" + taskId + "/g <<< $(cat " + baseConfigFile +")) > " + taskConfigFile + ";");
 			lArgs.add("export EXTRAE_CONFIG_FILE=" + taskConfigFile + ";");
 		}
+		
 		lArgs.add("python");
 		lArgs.add("-u");
 		lArgs.add(pycompssHome + WORKER_PY_RELATIVE_PATH);
+		
 		return lArgs;
 	}
 
@@ -53,8 +58,9 @@ public class PythonExecutor extends ExternalExecutor {
 
         // ADD PYEXTRAE IF TRACING
         if (tracing){
-            String pyextrae_path = nt.getInstallDir() + PYEXTRAE_RELATIVE_PATH;
-            pythonPath += ":" + pyextrae_path;
+            String libexec_extrae_path = nt.getInstallDir() + LIBEXEC_EXTRAE_RELATIVE_PATH;
+            String lib_extrae_path = nt.getInstallDir() + LIB_EXTRAE_RELATIVE_PATH;
+            pythonPath += ":" + libexec_extrae_path + ":" + lib_extrae_path ;
         }
 		env.put("PYTHONPATH", pythonPath);
 		

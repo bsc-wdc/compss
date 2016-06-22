39cf262f4c7fda51175e08ca5fcd58faa622a60a
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-06-22 07:08:22 +0000

    Add cleaner traps on install c binding
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2237 9ab3861e-6c05-4e1b-b5ef-99af60850597

---
+++ compss/programming_model/bindings/c/src/bindinglib/clean.sh
@@ -0,0 +1,37 @@
+#/bin/bash -e
+
+  rm -rf aclocal.m4
+  rm -rf compile
+  rm -rf config.h.in
+  rm -rf configure
+  rm -rf autom4te.cache
+  rm -rf config.guess
+  rm -rf config.h
+  rm -rf config.h.in
+  rm -rf config.log
+  rm -rf config.status
+  rm -rf config.status.lineno
+  rm -rf config.sub
+  rm -rf configure
+  rm -rf install-sh
+  rm -rf missing
+  rm -rf INSTALL
+  rm -rf install-sh
+  rm -rf depcomp
+  rm -rf Makefile
+  rm -rf Makefile.in
+  rm -rf config.status
+  rm -rf Makefile
+  rm -rf stamp-h1
+  rm -rf libtool
+  rm -rf ltmain.sh
+  rm -rf config.log
+  rm -rf libcbindings_la-GS_compss.o
+  rm -rf libcbindings_la-GS_compss.lo
+  rm -rf libcbindings_la-base64.o
+  rm -rf libcbindings_la-base64.lo
+  rm -rf .deps
+  rm -rf libcbindings.la
+  rm -rf .libs
+  rm -rf m4
+

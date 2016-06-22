8697153004539f5806cf5aed8c9fc2147c1cf6c5
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-06-22 07:23:22 +0000

    Add clean trap on bindings_common installer
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2238 9ab3861e-6c05-4e1b-b5ef-99af60850597

---
+++ compss/programming_model/bindings/bindings-common/clean.sh
@@ -0,0 +1,23 @@
+#/bin/bash -e
+
+  rm -rf src/libbindings_common.la
+  rm -rf src/libbindings_common_la-GS_compss.lo
+  rm -rf src/libbindings_common_la-GS_compss.o
+  rm -rf src/.deps
+  rm -rf src/.libs
+  rm -rf src/Makefile
+  rm -rf src/Makefile.in
+  rm -rf lib
+  rm -rf include
+  rm -rf m4
+  rm -rf aclocal.m4
+  rm -rf autom4te.cache
+  rm -rf configure
+  rm -rf INSTALL
+  rm -rf Makefile.in
+  rm -rf config
+  rm -rf config.status
+  rm -rf Makefile
+  rm -rf libtool
+  rm -rf config.log
+

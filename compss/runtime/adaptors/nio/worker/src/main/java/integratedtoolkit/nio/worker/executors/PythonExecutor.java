c08865aca5fa26e2d6371e3b8a8f5001958ff42c
palvarez <palvarez@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-07-07 15:15:42 +0000

    Removed tracing in python
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2273 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/PythonExecutor.java
+++ compss/runtime/adaptors/nio/worker/src/main/java/integratedtoolkit/nio/worker/executors/PythonExecutor.java
@@ -16,11 +16,7 @@ import java.util.Map;
 public class PythonExecutor extends ExternalExecutor {
 	
 	public static final String PYCOMPSS_RELATIVE_PATH = File.separator + "Bindings" + File.separator + "python";
-	
-	private static final String EXTRAE_RELATIVE_PATH = File.separator + "Dependencies" + File.separator + "extrae";
-	private static final String LIBEXEC_EXTRAE_RELATIVE_PATH = EXTRAE_RELATIVE_PATH + File.separator + "libexec";
-	private static final String LIB_EXTRAE_RELATIVE_PATH = EXTRAE_RELATIVE_PATH + File.separator + "lib";
-	
+
 	//private static final String WORKER_TRACING_CONFIG_FILE_RELATIVE_PATH = File.separator + "Runtime" + File.separator + "configuration" +
     //        File.separator + "xml" + File.separator + "tracing";
 	
@@ -52,12 +48,6 @@ public class PythonExecutor extends ExternalExecutor {
 			pythonPath = pycompssHome + ":" + nw.getPythonpath() + ":" + nw.getAppDir() + pythonPath;
 		}
 
-        // ADD PYEXTRAE IF TRACING
-        if (tracing){
-            String libexec_extrae_path = nw.getInstallDir() + LIBEXEC_EXTRAE_RELATIVE_PATH;
-            String lib_extrae_path = nw.getInstallDir() + LIB_EXTRAE_RELATIVE_PATH;
-            pythonPath += ":" + libexec_extrae_path + ":" + lib_extrae_path ;
-        }
 		env.put("PYTHONPATH", pythonPath);
 		
 		// LD_LIBRARY_PATH

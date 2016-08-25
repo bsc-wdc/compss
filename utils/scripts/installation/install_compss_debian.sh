83a1d5ac354e5cf65f44f40de2fa5475b636459e
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-08-25 07:19:02 +0000

    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2399 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- utils/scripts/installation/install_compss_debian.sh
+++ utils/scripts/installation/install_compss_debian.sh
@@ -2,6 +2,10 @@
 
   cleanRepo() {
     rm -f /etc/apt/sources.list.d/compss-framework_x86-64.list
+
+    if [ "${pack_folder}" != "" ]; then
+      rm -rf ${pack_folder}
+    fi
   }
 
   # Trap for clean
@@ -12,8 +16,16 @@
   wget http://compss.bsc.es/releases/repofiles/repo_deb_debian_noarch_unstable.list -O /etc/apt/sources.list.d/compss-framework_noarch.list
   
   # Refresh and install
-  apt-get update
-  apt-get -y --force-Yes install compss-framework
+  #apt-get update
+  #apt-get -y --force-Yes install compss-framework
+
+  # Java8 is not supported officially in our debian distribution so we break the packages to perform the installation
+  pack_folder=$(mktemp -d)
+  cd ${pack_folder}
+  apt-get download compss-*
+  apt-get -y --force-Yes install libboost-serialization-dev libboost-iostreams-dev
+  dpkg -i --force-all compss-*
+  cd -
 
   # Exit with status from last command
   exit 

56e676bc089ffae01c6b72fde83c12eb3cabc0d4
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-09-15 06:55:40 +0000

    Enable repo on centos Cleanup
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2446 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- utils/scripts/installation/unninstall_compss_centos.sh
+++ utils/scripts/installation/unninstall_compss_centos.sh
@@ -6,7 +6,9 @@
   # Purge compss
 
   # Clean all
-  yum clean all
+  wget http://compss.bsc.es/releases/repofiles/repo_rpm_centos_x86-64_unstable.repo -O /etc/yum.repos.d/compss-framework_x86_64.repo
+  yum --enablerepo=compss clean all
+  rm -f /etc/yum.repos.d/compss-framework_x86_64.repo
 
   # Update DB
   yum update -y

d97e8706e4c2aa1d7445c36132a1f2a5a345ab3c
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-10-18 07:46:19 +0000

    Add modifications to test 38
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2509 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- tests/sources/basic/38-environmentVariables_python/src/tasks.py
+++ tests/sources/basic/38-environmentVariables_python/src/tasks.py
@@ -10,22 +10,22 @@ from pycompss.api.constraint import constraint
 from pycompss.api.parameter import *
 
 
-@constraint(computingUnits = "${computingUnits}",
-            processorName = "${processorName}",
-            processorSpeed = "${processorSpeed}",
-            processorArchitecture = "${processorArchitecture}",
-            processorPropertyName = "${processorPropertyName}",
-            processorPropertyValue = "${processorPropertyValue}",
-            memorySize = "${memorySize}",
-            memoryType = "${memoryType}",
-            storageSize = "${storageSize}",
-            storageType = "${storageType}",
-            operatingSystemType = "${operatingSystemType}",
-            operatingSystemDistribution = "${operatingSystemDistribution}",
-            operatingSystemVersion = "${operatingSystemVersion}",
-            appSoftware = "${appSoftware}",
-            hostQueues = "${hostQueues}",
-            wallClockLimit = "${wallClockLimit}")
+@constraint(ComputingUnits = "${computingUnits}",
+            ProcessorName = "${processorName}",
+            ProcessorSpeed = "${processorSpeed}",
+            ProcessorArchitecture = "${processorArchitecture}",
+            ProcessorPropertyName = "${processorPropertyName}",
+            ProcessorPropertyValue = "${processorPropertyValue}",
+            MemorySize = "${memorySize}",
+            MemoryType = "${memoryType}",
+            StorageSize = "${storageSize}",
+            StorageType = "${storageType}",
+            OperatingSystemType = "${operatingSystemType}",
+            OperatingSystemDistribution = "${operatingSystemDistribution}",
+            OperatingSystemVersion = "${operatingSystemVersion}",
+            AppSoftware = "${appSoftware}",
+            HostQueues = "${hostQueues}",
+            WallClockLimit = "${wallClockLimit}")
 @task(returns = int)
 def constrained_func(value):
     return value * value * value

0fa5f8118321a2eb56cc9b5803ee9eea543d1b3c
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-10-19 13:30:45 +0000

    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2514 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- tests/sources/basic/24-noTask/src/main/java/noTask/Main.java
+++ tests/sources/basic/24-noTask/src/main/java/noTask/Main.java
@@ -2,9 +2,6 @@ package noTask;
 
 public class Main {
 
-    private static final int SLEEP_TIME = 5_000;
-
-
     public static void main(String[] args) {
         // Check and get parameters
         if (args.length != 0) {
@@ -20,13 +17,6 @@ public class Main {
     }
 
     public static void printMessage(String msg) {
-        // Sleep to allow runtime workers to be ready
-        /*try {
-            Thread.sleep(SLEEP_TIME);
-        } catch (InterruptedException e) {
-            // No need to handle such exceptions
-        }*/
-
         // Print out message
         System.out.println(msg);
     }

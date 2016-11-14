5b766d383a68bbb24e15df0d9804dda8f1171707
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-11-14 11:28:14 +0000

    Add force delete on sync ova script
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2582 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- utils/ova/sync_with_svn.sh
+++ utils/ova/sync_with_svn.sh
@@ -17,11 +17,13 @@
 
   # Check Out svns
   cd $HOME
+  rm -rf trunk tutorial-apps traces
   svn co http://compss.bsc.es/svn/compss/framework/trunk trunk --username $svnUser
   svn co http://compss.bsc.es/svn/bar/tutorial-apps/ tutorial-apps --username $svnUser
   svn co http://compss.bsc.es/svn/bar/traces traces --username $svnUser
 
   cd $SHAREDDISK
+  rm -rf $SHAREDDISK*
   svn co http://compss.bsc.es/svn/bar/datasets . --username $svnUser
 
   # Retrieve status

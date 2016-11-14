956df8cfed560e612cd26ed9b5138975bb22dbfe
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-11-14 11:32:52 +0000

    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2583 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- utils/ova/clean_ova.sh
+++ utils/ova/clean_ova.sh
@@ -4,6 +4,7 @@
   SHAREDDISK=/sharedDisk/
  
   # Clean apt
+  echo "Cleaning APT-GET"
   sudo apt-get -y --force-Yes update
   sudo apt-get -y --force-Yes autoremove
   sudo apt-get -y --force-Yes autoclean
@@ -11,10 +12,14 @@
   sudo apt-get -y --force-Yes clean
 
   # Clean subversion
+  echo "Cleanning SVN/MAVEN/COMPSs files"
   cd $ROOT_FOLDER
   sudo find . -name ".svn" | xargs -r -i -t rm -rf {}
   sudo find . -name ".subversion" | xargs -r -i -t rm -rf {}
+  sudo find . -name ".m2" | xargs -r -i -t rm -rf {}
+  sudo find . -name ".COMPSs" | xargs -r -i -t rm -rf {}
  
+  echo "Adding 0's"
   # Add 0's to HOME
   cd $HOME
   dd if=/dev/zero of=zeroFile bs=1M
@@ -29,4 +34,8 @@
   cd $ROOT_FOLDER
   sudo dd if=/dev/zero of=zeroFile bs=1M
   sudo rm -f zeroFile
+
+  # End
+  echo "DONE!"
+  exit 0
 

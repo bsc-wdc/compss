6d2d6d2608df088bd6c75802fe11c2fb3eb16c8b
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-11-15 08:19:57 +0000

    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2586 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- utils/ova/update_ova.sh
+++ utils/ova/update_ova.sh
@@ -7,7 +7,12 @@
   # Sync data
   ./sync_with_svn.sh
 
+  # Test tutorial apps
+  # TODO: Currently this step is done manually
+
   # Clean
   ./clean_ova.sh
+
+  # TODO: Check if clean history removes all the current bash_history
   ./clean_history.sh
 

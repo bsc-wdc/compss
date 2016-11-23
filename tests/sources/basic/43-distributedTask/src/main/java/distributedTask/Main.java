a1e81fd49331b300b57eecb2ef00bedd85a32fdd
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-11-23 08:51:14 +0000

    Add isDistributed management in scheduler. Add test for it
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2606 9ab3861e-6c05-4e1b-b5ef-99af60850597

---
+++ tests/sources/basic/43-distributedTask/src/main/java/distributedTask/Main.java
@@ -0,0 +1,44 @@
+package distributedTask;
+
+import integratedtoolkit.api.COMPSs;
+
+
+public class Main {
+
+    private static final int WAIT_RUNTIME = 5_000;
+    private static final int NUM_WORKERS = 2;
+
+
+    public static void main(String[] args) {
+
+        // ----------------------------------------------------------------------------
+        // Wait for Runtime to have both workers available
+        try {
+            Thread.sleep(WAIT_RUNTIME);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        // ----------------------------------------------------------------------------
+        // Launch 2 normal tasks (should go to the same worker)
+        System.out.println("[LOG] Launch normal tasks");
+        String msg = "Hello World!";
+        for (int i = 0; i < NUM_WORKERS; ++i) {
+            MainImpl.normalTask(msg);
+        }
+
+        // ----------------------------------------------------------------------------
+        // Wait completion
+        System.out.println("[LOG] Wait for normal tasks completion");
+        COMPSs.waitForAllTasks();
+
+        // ----------------------------------------------------------------------------
+        // Launch 2 distributed tasks (should go 1 to each worker)
+        System.out.println("[LOG] Launch distributed tasks");
+        for (int i = 0; i < NUM_WORKERS; ++i) {
+            MainImpl.distributedTask(msg);
+        }
+
+    }
+
+}

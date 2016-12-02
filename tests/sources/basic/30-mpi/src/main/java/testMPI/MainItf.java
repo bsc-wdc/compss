5675081e74ab50259419a5d127df3739ed6e660b
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2016-12-02 13:47:49 +0000

    Add skip on new test 45 python pscos
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2631 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- tests/sources/basic/30-mpi/src/main/java/testMPI/MainItf.java
+++ tests/sources/basic/30-mpi/src/main/java/testMPI/MainItf.java
@@ -16,9 +16,42 @@ public interface MainItf {
 	);
 	
 	@MPI(binary = "${VEC_SUM_MPI_BINARY}", mpiRunner = "mpirun", computingNodes = "2")
-	@Constraints(computingUnits = "2")
+    @Constraints(computingUnits = "2")
+    public String taskMultipleMPI(
+        @Parameter(type = Type.OBJECT, direction = Direction.IN) int[] data
+    );
+	
+	/*
+	@MPI(binary = "${VEC_SUM_MPI_BINARY}", mpiRunner = "mpirun", computingNodes = "2")
+	@Constraints(computingUnits = "Eval(#par[1]+ size(#stdin)")
 	public String taskMultipleMPI(
-		@Parameter(type = Type.OBJECT, direction = Direction.IN) int[] data
+	    @Parameter(type = Type.INT, direction = Direction.IN, name="val") int val,
+		@Parameter(type = Type.OBJECT, direction = Direction.IN) int[] data,
+		@Parameter(type = Type.STRING, name = Binary.STDIN) String fileIn,
+		@Parameter(type = Type.FILE, direction = INOUT, name = Binary.STDOUT) String fileO,
+		@Parameter(type = Type.FILE, name = Binary.STDERR) String fileE,
+		@Parameter(type = Type.STRING, name = Binary.STDIN) String fileIn2,
 	);
-
+	
+	type = STDIN --> file IN
+	        STDOUT --> file OUT / INOUT
+	        STDERR --> file OUT / INOUT
+	
+	@MPI(binary = "${VEC_SUM_MPI_BINARY}", mpiRunner = "mpirun", computingNodes = "2", stdIn = 2, stdOut = 3, stdErr = 4)
+    @Constraints(computingUnits = "Eval(#par[1]+ size(#par[3])")
+    public String taskMultipleMPI(
+        @Parameter(type = Type.INT, direction = Direction.IN) int val,
+        @Parameter(type = Type.OBJECT, direction = Direction.IN) int[] data,
+        @Parameter(type = Type.FILE) String fileIn,
+        @Parameter(type = Type.FILE) String fileO,
+        @Parameter(type = Type.FILE) String fileE,
+    );
+	       
+    PARA HACER: 
+	cmd $* < stdIN 1>> stdout 2>> stderr | tee 1> jobN_NEW.out 2> jobN_NEW.err
+	
+	AHORA SE HACE:
+	si la tarea falla o no: $?
+	return cmd $*
+    */
 }

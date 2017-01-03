21dd1d6173c78b865c95c196147e043a650b856a
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-01-03 14:06:58 +0000

    Add Action log by Scheduler. Add more things to test 48
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2693 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- compss/runtime/scheduler/fifoScheduler/src/main/java/integratedtoolkit/scheduler/types/FIFOScore.java
+++ compss/runtime/scheduler/fifoScheduler/src/main/java/integratedtoolkit/scheduler/types/FIFOScore.java
@@ -7,12 +7,36 @@ import integratedtoolkit.types.resources.Worker;
 
 public class FIFOScore extends Score {
 
+    /**
+     * Creates a new FIFOScore from the given values
+     * 
+     * @param actionScore
+     * @param waiting
+     * @param res
+     * @param impl
+     */
     public FIFOScore(double actionScore, double waiting, double res, double impl) {
         super(actionScore, waiting, res, impl);
     }
 
+    /**
+     * Creates a copy of @clone FIFOScore
+     * 
+     * @param clone
+     */
     public FIFOScore(FIFOScore clone) {
         super(clone);
+    }
+
+    /**
+     * Calculates the score for a given worker @w and a given parameters @params
+     * 
+     * @param params
+     * @param w
+     * @return
+     */
+    public static double calculateScore(TaskDescription params, Worker<?> w) {
+        return (1.0 / (double) params.getId());
     }
 
     @Override
@@ -23,19 +47,10 @@ public class FIFOScore extends Score {
         return implementationScore > other.implementationScore;
     }
 
-    public static double calculateScore(TaskDescription params, Worker<?> w) {
-        return (1.0 / (double) params.getId());
-    }
-
     @Override
     public String toString() {
-        /*
-         * return "[FIFOScore = [action:" + actionScore + ", resource:" + resourceScore + ", load:" + waitingScore +
-         * ", implementation:" + implementationScore + "]" + "]";
-         */
-        return "[action:" + actionScore + ", resource:" + resourceScore + ", load:" + waitingScore + ", implementation:"
+        return "[FIFOScore = [action:" + actionScore + ", resource:" + resourceScore + ", load:" + waitingScore + ", implementation:"
                 + implementationScore + "]";
-        // + "]";
     }
 
 }

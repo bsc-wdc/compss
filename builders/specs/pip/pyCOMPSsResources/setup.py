32ca50210aa2b4c344c336f19946af364a26743c
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-01-03 14:53:05 +0000

    added additional metadata to the installer
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2694 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- builders/specs/pip/pyCOMPSsResources/setup.py
+++ builders/specs/pip/pyCOMPSsResources/setup.py
@@ -11,7 +11,7 @@ from distutils import log
 '''
 	Distutils installer. This script will be called by pip when:
 	- We want to create a distributable (sdist) tar.gz
-	- We want to build the C extension (build)
+	- We want to build the C extension (build and build_ext)
 	- We want to install pyCOMPSs (install)
 
 
@@ -20,45 +20,40 @@ from distutils import log
 '''
 
 bindings_location = os.path.join('COMPSs', 'Bindings')
-
-
-'''
-	Check that system requeriments are satisfied
-'''
-def check_system():
-	if 'win' in sys.platform:
-		raise Exception('COMPSs does not support Windows')
-
-	def cool_version_info(info):
-		return '%s.%s.%s'%(info[0], info[1], info[2])
-
-	if (2, 7) != sys.version_info[:2]:
-		raise Exception('COMPSs does not support Python version %s'%(cool_version_info(sys.version_info)))
-
-def check_dependencies():
-	pass
-
 target_path = os.path.join(site.getsitepackages()[0], 'pycompss')
+
+def check_system():
+        '''
+                Checks that we have a proper python version and a
+                proper OS (i.e: not windows)
+        '''
+        if sys.version_info[:2] != (2, 7):
+                raise Exception('COMPSs does not support Python version %s', sys.version)
+        if 'win' in sys.platform:
+                raise Exception('COMPSs does not support Windows')
 
 '''
 	Pre-install operation: download and install COMPSs
 	This will try to stop the installation if some error is
-	found during that part. However, some sub-scripts don't
+	found during that part. However, some sub-scripts dont
 	propagate the errors they find, so there is not absolute
 	guarantee that this script will lead to a perfect, clean
 	installation.
 '''
-if 'install' in sys.argv:
+if 'install' in sys.argv[1:]:
 	check_system()
-	check_dependencies()
 	try:
 		backend_install(target_path)
 	except:
-		raise Exception('Something went wrong during COMPSs install process.')
+		raise Exception('Something went wrong during COMPSs installation process.')
+
 
 '''
 	C extension for pyCOMPSs. Declaring it that way allows us
-	to compile and place it with setup.py build	
+	to compile and place it with setup.py build.
+	This extension will generate a Python extension named compss
+	and will place a compss.so file in our site-packages.
+	This file will be properly deleted with pip uninstall.
 '''
 compssmodule = Extension('compss',
         include_dirs = [
@@ -82,9 +77,18 @@ setup (name='pycompss',
 	long_description=open('README.txt').read(),
 	author='The COMPSs team',
 	author_email='distributed_computing@bsc.es',
+	maintainer='The COMPSs team',
+	maintainer_email='distributed_computing@bsc.es',
 	url='http://compss.bsc.es',
+	classifiers = [
+        	'Development Status :: 5 - Stable',
+        	'Intended Audience :: Developers',
+        	'License :: OSI Approved :: Apache 2.0',
+        	'Programming Language :: Python :: 2.7'
+	],
 	install_requires=['wget'],
 	license='Apache 2.0',
+	platforms=['Linux', 'Mac OS-X'],
 	package_dir={'pycompss':os.path.join('src','pycompss')},
 	packages=['pycompss', 'pycompss.api', 'pycompss.runtime', 'pycompss.worker', 'pycompss.storage', 'pycompss.util', 'pycompss.util.serialization', 'pycompss.api.dummy', 'pycompss.functions', 'pycompss.matlib', 'pycompss.matlib.algebra', 'pycompss.matlib.classification', 'pycompss.matlib.clustering'],
 	package_data={'' : [os.path.join('log','logging.json'), os.path.join('log','logging.json.debug'), os.path.join('log','logging.json.off'), os.path.join('bin','worker_python.sh')]},

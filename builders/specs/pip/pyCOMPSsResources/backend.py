00d8ddb793ee2b666d8935bf1e9bd261ad82d53e
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-01-17 10:21:53 +0000

    changed package name to pycompss again
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2718 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- builders/specs/pip/pyCOMPSsResources/backend.py
+++ builders/specs/pip/pyCOMPSsResources/backend.py
@@ -1,7 +1,8 @@
 import os
 import sys
-import wget
+import urllib
 import site
+import glob
 import shutil
 import tarfile
 import subprocess
@@ -20,17 +21,16 @@ def install(target_path):
 	# download and decompress the COMPSs_version.tar.gz file	
 	base_url = open('url','r').read().rstrip()
 	version_name = open('VERSION.txt','r').read().rstrip()
-	bulk_name = 'COMPSs_{0}.tar.gz'.format(version_name)
+	tgz_name = 'COMPSs_{0}.tar.gz'.format(version_name)
 
-	full_url = os.path.join(base_url, bulk_name)
+	full_url = os.path.join(base_url, tgz_name)
 
-	file_name = wget.download(full_url)
+	urllib.urlretrieve(full_url, tgz_name)
 
-
-	tar = tarfile.open(file_name, 'r:gz')
+	tar = tarfile.open(tgz_name, 'r:gz')
 	tar.extractall('.')
 
-	os.remove(file_name)
+	os.remove(tgz_name)
 
 	# call the SuperComputers install script
 	if subprocess.call(['./COMPSs/install', target_path]) != 0:
@@ -51,11 +51,18 @@ def install(target_path):
 		s = s.replace(key, value)
 	open('/etc/profile.d/compss.sh', 'w').write(s)
 
-	# create symbolic links to the python package and its C extension, respectively
-	target_files = ['pycompss', 'compss.so']
-	for target_file in target_files:
-		try:
-			os.remove(os.path.join(site.getsitepackages()[0], target_file))
-		except:
-			os.symlink(os.path.join(pref, "Bindings", "python", target_file), os.path.join(site.getsitepackages()[0], target_file))
 
+	def create_symlink(original_file, symbolic_place):
+		try:
+			os.remove(symbolic_place)
+		except:
+			pass
+		os.symlink(original_file, symbolic_place)
+
+	# create symbolic links to the python package contents
+	original_compss_path = os.path.join(pref, 'Bindings', 'python', 'pycompss')
+	pycompss_sitepackages_path = os.path.join(site.getsitepackages()[0], 'pycompss')
+
+	for target_file in glob.iglob( os.path.join(original_compss_path, "*") ):
+		symbolic_place = os.path.join(pycompss_sitepackages_path, os.path.split(target_file)[-1])
+		create_symlink(target_file, symbolic_place)

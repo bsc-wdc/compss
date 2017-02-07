487d94de0b2af8f804115abd4c0132c6e61ddfe7
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-02-07 08:04:36 +0000

    added some additional tests to test49 (still in skip)
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@2769 9ab3861e-6c05-4e1b-b5ef-99af60850597

--- tests/sources/basic/49-localtask_python/src/modules/testLocalTask.py
+++ tests/sources/basic/49-localtask_python/src/modules/testLocalTask.py
@@ -1,9 +1,11 @@
-import unittest
 import gc
+import unittest
+import numpy as np
+from C import C
 from pycompss.api.task import task
+from pycompss.api.parameter import *
 from pycompss.api.api import compss_wait_on
 from pycompss.api.localtask import localtask
-from pycompss.api.parameter import *
 
 @task(v=IN, returns=list)
 def inc(v):
@@ -12,6 +14,28 @@ def inc(v):
 @task(v=INOUT)
 def append_three_ones(v):
     v += [1, 1, 1]
+
+@task(m=IN, returns=list)
+def square(m):
+    return m**2
+
+@task(v=IN, returns=list)
+def normalize(m):
+    return m / np.linalg.norm(m)
+
+@task(d=INOUT)
+def merge_dict(d):
+    sub_dict = d['dict']
+    d.update(sub_dict)
+    del d['dict']
+
+@localtask
+def remove_trump(d):
+    del d['trump']
+
+@localtask
+def solve_equation_system(A, b):
+    return np.linalg.solve(A, b)
 
 @localtask
 def scale_vector(v, k):
@@ -29,9 +53,7 @@ class testLocalTask(unittest.TestCase):
         # scale_vector should sync u
         # if it doesn't, then the program will
         # crash
-        print bb
         bb = compss_wait_on(bb)
-        print bb
         w = scale_vector(bb, 2)
         self.assertEqual([2, 2, 2], w)
 
@@ -51,6 +73,7 @@ class testLocalTask(unittest.TestCase):
         # in fact, v should be equal to [1, 1, 1]
         self.assertEqual([1, 1, 1], v)
         w = scale_vector(v, 2)
+        self.assertEqual([1, 1, 1], v)
         self.assertEqual([2, 2, 2], w)
 
     def testArgsKwargsAreRespected(self):
@@ -58,7 +81,6 @@ class testLocalTask(unittest.TestCase):
         # are arguments respected? (i.e: aren't we merging positional arguments)
         # with non-positional and/or keywords?
         self.assertEqual((1, 3, 3, 2), ret_val)
-
 
     def testArgsKwargsAreProperlySynced(self):
         v = []
@@ -74,3 +96,62 @@ class testLocalTask(unittest.TestCase):
         self.assertEqual([2, 3, 4], u)
         self.assertEqual([7, 6, 5], w)
         self.assertEqual(([1, 1, 1], 3, 1, 1), ret_val)
+
+    def testClassAttributes(self):
+        my_C = C()
+        my_C.vector = []
+        append_three_ones(my_C.vector)
+        my_C.vector = scale_vector(my_C.vector, 2)
+        # is the class attribute updated?
+        self.assertEqual([2, 2, 2], my_C.vector)
+
+    def testMultipleReferences(self):
+        original_object = [[]]
+        reference_1 = original_object
+        reference_2 = original_object
+        reference_3 = [original_object]
+        my_C = C()
+        my_C.vector = reference_2
+        reference_4 = my_C.vector
+        append_three_ones(original_object[0])
+        original_object[0] = scale_vector(original_object[0], 2)
+        # are all these references updated?
+        self.assertEqual([2, 2, 2], original_object[0])
+        self.assertEqual([2, 2, 2], reference_1[0])
+        self.assertEqual([2, 2, 2], reference_2[0])
+        self.assertEqual([2, 2, 2], reference_3[0][0])
+        self.assertEqual([2, 2, 2], my_C.vector[0])
+        self.assertEqual([2, 2, 2], reference_4[0])
+
+    '''
+        These tests simply try to invoke complex use cases.
+    '''
+    def testComplexDataStructures1(self):
+        original_object = {
+            'dog': 'woof',
+            13: 37,
+            'dict': {
+                'sheep': 'animal',
+                'cactus': 'plant',
+                'trump': 'unknown'
+            }
+        }
+        # lets move the contents of the sub dictionary 'dict' to
+        # the main dictionary and then, remove the item with key 'trump'
+        merge_dict(original_object)
+        remove_trump(original_object)
+        self.assertEqual({
+            'dog': 'woof',
+            13: 37,
+            'sheep': 'animal',
+            'cactus': 'plant'},
+            original_object);
+
+
+    def testHugeObjects(self):
+        N = 5000
+        A = square(np.eye(N)*np.random.rand(N))
+        b = normalize(np.random.rand(N))
+        x = solve_equation_system(A, b)
+        # well... does it works if the objects are huge?
+        self.assertTrue(np.allclose(np.dot(A, x), b))

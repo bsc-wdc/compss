737a6332c00802e60746414239ed1a82985bd02b
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-05-16 13:53:11 +0000

    Add new cases to the affinity binder
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3003 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: f5ee9a3d46ca113aa10922c9fa65a9d258ba9b30

--- compss/runtime/adaptors/nio/worker/src/test/java/integratedtoolkit/nio/workers/binders/test/MapTest.java
+++ compss/runtime/adaptors/nio/worker/src/test/java/integratedtoolkit/nio/workers/binders/test/MapTest.java
@@ -36,4 +36,28 @@ public class MapTest {
         assertEquals(mapExpected, mapGot);
     }
 
+    @Test
+    public void complexSocketMap() throws InvalidMapException {
+        final String lscpuOutput = "Architecture:          x86_64\nCPU op-mode(s):        32-bit, 64-bit\n"
+                + "Byte Order:            Little Endian\nCPU(s):                56\nOn-line CPU(s) list:   0-55\n"
+                + "Thread(s) per core:    2\nCore(s) per socket:    14\nSocket(s):             2\nNUMA node(s):          2\n"
+                + "Vendor ID:             GenuineIntel\nCPU family:            6\nModel:                 79\n"
+                + "Model name:            Intel(R) Xeon(R) CPU E5-2690 v4 @ 2.60GHz\nStepping:              1\n"
+                + "CPU MHz:               1200.328\nCPU max MHz:           3500.0000\nCPU min MHz:           1200.0000\n"
+                + "BogoMIPS:              5188.20\nVirtualization:        VT-x\nL1d cache:             32K\nL1i cache:             32K\n"
+                + "L2 cache:              256K\nL3 cache:              35840K\nNUMA node0 CPU(s):     0-13,28-41\n"
+                + "NUMA node1 CPU(s):     14-27,42-55\nFlags:                 fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge"
+                + " mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx pdpe1gb rdtscp lm constant_tsc"
+                + " arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc aperfmperf eagerfpu pni pclmulqdq dtes64 monitor ds_cpl"
+                + " vmx smx est tm2 ssse3 sdbg fma cx16 xtpr pdcm pcid dca sse4_1 sse4_2 x2apic movbe popcnt tsc_deadline_timer aes"
+                + " xsave avx f16c rdrand lahf_lm abm 3dnowprefetch ida arat epb pln pts dtherm intel_pt tpr_shadow vnmi flexpriority"
+                + " ept vpid fsgsbase tsc_adjust bmi1 hle avx2 smep bmi2 erms invpcid rtm cqm rdseed adx smap xsaveopt"
+                + " cqm_llc cqm_occup_llc";
+
+        final String mapExpected = "0-13,28-41/14-27,42-55";
+        String mapGot = BindToMap.processLsCpuOutput(lscpuOutput);
+
+        assertEquals(mapExpected, mapGot);
+    }
+
 }

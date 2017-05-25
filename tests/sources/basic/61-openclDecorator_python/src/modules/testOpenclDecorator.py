a664708ec6ced93d718db8134502c62224591eb4
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-05-25 14:12:13 +0000

    Added new test for evaluating the @opencl decorator in PyCOMPSs. Currently in skip mode.
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3037 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: e1dd7db4011f51f3a264d8ead0c40cef4f2bb119

---
+++ tests/sources/basic/61-openclDecorator_python/src/modules/testOpenclDecorator.py
@@ -0,0 +1,28 @@
+import unittest
+from pycompss.api.task import task
+from pycompss.api.parameter import *
+from pycompss.api.api import barrier
+from pycompss.api.opencl import opencl
+from pycompss.api.constraint import constraint
+
+@opencl(kernel="date", workingDir="/tmp")
+@task()
+def myDate(dprefix, param):
+    pass
+
+@constraint(computingUnits="2")
+@opencl(kernel="date", workingDir="/tmp")
+@task()
+def myDateConstrained(dprefix, param):
+    pass
+
+
+class testOpenclDecorator(unittest.TestCase):
+
+    def testFunctionalUsage(self):
+        myDate("-d", "next friday")
+        barrier()
+
+    def testFunctionalUsageWithConstraint(self):
+        myDateConstrained("-d", "next monday")
+        barrier()

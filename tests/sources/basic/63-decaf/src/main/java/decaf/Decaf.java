be39c1034fb23bda0d746869d2c5efba3f276004
jorgee <jorgee@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-05-31 14:51:15 +0000

    added decaf test
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3063 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 97fca4ae00aa3fb60e947288720f74f819bc647a

---
+++ tests/sources/basic/63-decaf/src/main/java/decaf/Decaf.java
@@ -0,0 +1,17 @@
+package decaf;
+
+public class Decaf {
+
+    public static int taskSingleDecaf(String fileOut) {
+        return -1;
+    }
+    
+    public static int taskMultipleDecaf(String fileOut) {
+        return -1;
+    }
+    
+    public static int taskConcurrentMultipleDecaf(String fileOut) {
+        return -1;
+    }
+
+}

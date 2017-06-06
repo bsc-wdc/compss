629c9f7a1a774a941fd496270a6ebaff8114bb66
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-06-06 13:05:54 +0000

    [PyCOMPSs] Updated support for PSCOs.
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3069 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 4f3a569e55d4673267080160adf01464716f2839

--- tests/sources/basic/45-pscos_python/src/pscos.py
+++ tests/sources/basic/45-pscos_python/src/pscos.py
@@ -8,15 +8,15 @@ from models import InputData
 
 # # BUG: Pickling error in the dummy deserialization.
 # from pycompss.storage.Object import SCO
-# 
+#
 # class mySO(SCO):
 #     value = 0
 #     def __init__(self, v):
 #         self.value = v
-#         
+#
 #     def get(self):
 #         return self.value
-#       
+#
 #     def put(self, v):
 #         self.value = v
 
@@ -64,6 +64,11 @@ def reduceTask(dic1, dic2):
 
 @task(final=INOUT)
 def reduceTaskPSCOs(final, dic2):
+    print "REDUCE TASK PSCOs"
+    print "PPPPPPPPPPPPPPPPPPP"
+    print final.get()
+    print dic2
+    print "PPPPPPPPPPPPPPPPPPP"
     dic1 = final.get()
     for k in dic2:
         if k in dic1:
@@ -71,6 +76,9 @@ def reduceTaskPSCOs(final, dic2):
         else:
             dic1[k] = dic2[k]
     final.set(dic1)
+    print "PPPPPPPPPPPPPPPPPPP"
+    print final.get()
+    print "PPPPPPPPPPPPPPPPPPP"
 
 
 def basicTest():
@@ -100,7 +108,7 @@ def basicTest():
 	    return False
 
 
-def basic2Test():    
+def basic2Test():
 	'''
 	This Test:
 	    - Instantiates a SCO.
@@ -149,7 +157,7 @@ def WordCount():
 	    partialResults = wordcountTask(w)
 	    reduceTask(result, partialResults)
 	result = compss_wait_on(result)
-	
+
 	if result['This'] == 4 and result['is'] == 4 and result['a'] == 4 and result['test'] == 4:
 		print "- Python Wordcount 1 with PSCOs: OK"
 		return True
@@ -159,32 +167,38 @@ def WordCount():
 
 
 def WordCount2():
-	from pycompss.api.api import compss_wait_on
-	'''
+    from pycompss.api.api import compss_wait_on
+    '''
 	WordCount Test
 	    - Wordcount task receives a PSCO and returns a dictionary.
 	    - Reduce task receives a INOUT PSCO (result) where accumulates the partial results.
-	'''
-	words = [Words('This is a test'), Words('This is a test'), Words('This is a test'), Words('This is a test')]
-	for w in words:
-	    w.makePersistent()
-	result = Result()
-	result.makePersistent()
+    '''
+    words = [Words('This is a test'), Words('This is a test')] #, Words('This is a test'), Words('This is a test')]
+    for w in words:
+        w.makePersistent()
+    result = Result()
+    result.makePersistent()
 
-	localResults = []
-	for w in words:
-	    partialResults = wordcountTask(w)
-	    reduceTaskPSCOs(result, partialResults)
-	final = compss_wait_on(result)
+    localResults = []
+    for w in words:
+        partialResults = wordcountTask(w)
+        reduceTaskPSCOs(result, partialResults)
 
-	result = final.get()
+    from pycompss.api.api import barrier
+    barrier()
 
-	if result['This'] == 4 and result['is'] == 4 and result['a'] == 4 and result['test'] == 4:
-		print "- Python Wordcount 2 with PSCOs: OK"
-		return True
-	else:
-		print "- Python Wordcount 2 with PSCOs: ERROR"
-		return False
+    final = compss_wait_on(result)
+    print "XXXXXXXXXXXXXXXXXX"
+    print final.myd
+    print "XXXXXXXXXXXXXXXXXX"
+    result = final.get()
+
+    if result['This'] == 4 and result['is'] == 4 and result['a'] == 4 and result['test'] == 4:
+        print "- Python Wordcount 2 with PSCOs: OK"
+        return True
+    else:
+        print "- Python Wordcount 2 with PSCOs: ERROR"
+        return False
 
 
 @task(o2=INOUT)
@@ -199,7 +213,7 @@ def transform1(o1, o2):
         o2.set(pow2)
         print "Function: Pow 2."
         print "Transformation 1 result in o2: ", o2.get()
-      
+
 @task(o2=INOUT)
 def transform2(o1, o2):
         add1 = {}
@@ -212,7 +226,7 @@ def transform2(o1, o2):
         o2.set(add1)
         print "Function: Add 1."
         print "Transformation 2 result in o2: ", o2.get()
-      
+
 @task(returns=InputData)
 def transform3(o1, o2):
         mult3 = {}
@@ -242,17 +256,17 @@ def TiramisuMockup():
         out1.makePersistent()
         out2.makePersistent()
         out3.makePersistent()
-        
+
         result = ''
         transform1(myobj, out1)
         transform2(out1, out2)
         result = transform3(out2, out3)
-        
+
         result = compss_wait_on(result)
         outTrans1 = compss_wait_on(out1)
         outTrans2 = compss_wait_on(out2)
         outTrans3 = compss_wait_on(out3)
-        
+
         '''
         print "OUTPUTS:"
         print "Transformation 1: ", outTrans1.get()
@@ -262,15 +276,15 @@ def TiramisuMockup():
         print "RESULT: ", result.get()
         print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
         '''
-        
+
         finalResults = result.get()
         if all(x == 6 for x in finalResults['first']) and all(x == 15 for x in finalResults['second']) and all(x == 30 for x in finalResults['third']) and all(x == 51 for x in finalResults['fourth']):
                 print "- Python Tiramisu Mockup with PSCOs: OK"
 		return True
 	else:
 		print "- Python Tiramisu Mockup with PSCOs: ERROR"
-		return False	     
-        
+		return False
+
 
 @task(o2=OUT)
 def transform1_2(o1, o2):
@@ -284,7 +298,7 @@ def transform1_2(o1, o2):
         o2.set(pow2)
         print "Function: Pow 2."
         print "Transformation 1 result in o2: ", o2.get()
-      
+
 @task(o2=OUT)
 def transform2_2(o1, o2):
         add1 = {}
@@ -297,7 +311,7 @@ def transform2_2(o1, o2):
         o2.set(add1)
         print "Function: Add 1."
         print "Transformation 2 result in o2: ", o2.get()
-      
+
 @task(returns=InputData, o2=OUT)
 def transform3_2(o1, o2):
         mult3 = {}
@@ -313,7 +327,7 @@ def transform3_2(o1, o2):
         return o2
 
 
-        
+
 def TiramisuMockup2():
         from pycompss.api.api import compss_wait_on
         '''
@@ -328,21 +342,21 @@ def TiramisuMockup2():
         out1.makePersistent()
         out2.makePersistent()
         out3.makePersistent()
-        
+
         result = ''
         transform1_2(myobj, out1)
         transform2_2(out1, out2)
         result = transform3_2(out2, out3)
-        
+
         result = compss_wait_on(result)
-        
+
         import time
         time.sleep(60)
-        
+
         outTrans1 = compss_wait_on(out1)
         outTrans2 = compss_wait_on(out2)
         outTrans3 = compss_wait_on(out3)
-        
+
         print "OUTPUTS:"
         print "Transformation 1: ", outTrans1.get()
         print "Transformation 2: ", outTrans2.get()
@@ -359,8 +373,8 @@ def TiramisuMockup2():
 		print "- Python Tiramisu Mockup 2 with PSCOs: ERROR"
 		return False
 
-  
-  
+
+
 def main():
 	results = {}
 	#results['basic'] =  basicTest()

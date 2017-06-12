332c2616f2f9aa78e75b37e3f6bc17fdc76f0422
jorgee <jorgee@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-06-12 13:05:30 +0000

    update decaf test
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3083 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 992c6a0ad3aa04ad49a7fd0823c0ef89465748af

--- tests/sources/basic/64-decaf_python/src/modules/testDecafDecorator.py
+++ tests/sources/basic/64-decaf_python/src/modules/testDecafDecorator.py
@@ -5,18 +5,18 @@ from pycompss.api.api import barrier
 from pycompss.api.decaf import decaf
 from pycompss.api.constraint import constraint
 
-@decaf(dfScript="decaf/test-auto.py")
+@decaf(dfScript="$PWD/decaf/test-auto.py")
 @task(param=FILE_OUT)
 def myDecaf(dprefix, param):
     pass
 
-@decaf(workingDir=".", runner="mpirun", dfScript="decaf/test.py", dfExecutor="test.sh", dfLib="lib")
+@decaf(workingDir=".", runner="mpirun", dfScript="$PWD/decaf/test.py", dfExecutor="test.sh", dfLib="lib")
 @task(param=FILE_OUT)
 def myDecafAll(dprefix, param):
     pass
 
 @constraint(computingUnits="2")
-@decaf(runner="mpirun", computingNodes=2, dfScript="myscript", dfExecutor="executor", dfLib="lib")
+@decaf(runner="mpirun", computingNodes=2, dfScript="$PWD/decaf/test-2.py", dfExecutor="test-2.sh", dfLib="lib")
 @task(param=FILE_OUT)
 def myDecafConstrained(dprefix, param):
     pass

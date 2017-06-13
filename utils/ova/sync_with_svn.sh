0c3636cdaccb26d9d1796233bc61441f6b78955a
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-06-13 11:47:25 +0000

    Update OVA update scripts
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3094 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 4db0ef4b9280f9fc1cdd08a9e2494d7213f4ed5b

--- utils/ova/sync_with_svn.sh
+++ utils/ova/sync_with_svn.sh
@@ -18,14 +18,23 @@
   # Check Out svns
   cd $HOME
   #sudo rm -rf trunk
-  sudo rm -rf tutorial-apps traces
+  sudo rm -rf tutorial_apps traces
   #svn co http://compss.bsc.es/svn/compss/framework/trunk trunk --username $svnUser
-  svn co http://compss.bsc.es/svn/bar/tutorial-apps/ tutorial-apps --username $svnUser
+  svn co http://compss.bsc.es/svn/bar/tutorial_apps/ tutorial_apps --username $svnUser
   svn co http://compss.bsc.es/svn/bar/traces traces --username $svnUser
+  
+  # Clean unneeded files
+  rm -rf tutorial_apps/ide/
 
+  # Check out datasets
   cd $SHAREDDISK
   rm -rf $SHAREDDISK*
   svn co http://compss.bsc.es/svn/bar/datasets . --username $svnUser
+
+  # Clean unneeded files
+  rm -rf Discrete/ GeneDetection/ Hmmer/ nmmb/
+
+  cd -
 
   # Retrieve status
   echo "DONE!"

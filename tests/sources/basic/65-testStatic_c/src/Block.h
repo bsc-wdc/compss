0fd295c075746a29aa465e3d54bfa88be996f102
jorgee <jorgee@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-06-23 10:11:03 +0000

    add testStatic
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3142 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 74c388fee37535370c37c042c6e4879842b70ccc

---
+++ tests/sources/basic/65-testStatic_c/src/Block.h
@@ -0,0 +1,66 @@
+/*
+ *  Copyright 2002-2015 Barcelona Supercomputing Center (www.bsc.es)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <vector>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/vector.hpp>
+
+using namespace std;
+using namespace boost;
+using namespace serialization;
+
+class Block {
+
+public:
+	Block(){};
+
+	Block(int bSize);
+
+	void init(int bSize, double initVal);
+	
+	static Block create( int bSize, double initVal);
+
+	static void generate(Block *b, int bSize, double initVal);
+
+	void multiply(Block block1, Block block2);
+
+	void print();
+
+	void result();
+
+	std::vector< std::vector< double > > data;
+
+private:
+	int M;
+
+	friend class::serialization::access;
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version) {
+		ar & M;
+		if (Archive::is_loading::value){
+			data.clear();
+		}
+		ar & data;
+	}
+};
+
+#endif

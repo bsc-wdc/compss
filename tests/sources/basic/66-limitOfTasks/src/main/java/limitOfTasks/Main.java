452b6031d8bbbfb21b2129e0ac62224aa2b58f50
cramonco <cramonco@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-07-05 07:43:32 +0000

    Add test for limit of tasks
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3174 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: f035f319444a496959a5b8edc0d4fabbb8180803

---
+++ tests/sources/basic/66-limitOfTasks/src/main/java/limitOfTasks/Main.java
@@ -0,0 +1,50 @@
+package limitOfTasks;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Check and get parameters
+        if (args.length != 1) {
+            System.out.println("[ERROR] Bad number of parameters");
+            System.out.println("    Usage: simple.Simple <counterValue>");
+            System.exit(-1);
+        }
+        String counterName = "counter";
+        int initialValue = Integer.parseInt(args[0]);
+
+        // ------------------------------------------------------------------------
+        // Write value
+        try {
+            FileOutputStream fos = new FileOutputStream(counterName);
+            fos.write(initialValue);
+            System.out.println("Initial counter value is " + initialValue);
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.exit(-1);
+        }
+
+        // ------------------------------------------------------------------------
+        // Execute increment
+        MainImpl.increment(counterName);
+        MainImpl.increment(counterName);
+
+        // ------------------------------------------------------------------------
+        // Read new value
+        System.out.println("After Sending task");
+        try {
+            FileInputStream fis = new FileInputStream(counterName);
+            System.out.println("Final counter value is " + fis.read());
+            fis.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+}

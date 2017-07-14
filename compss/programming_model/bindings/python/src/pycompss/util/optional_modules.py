166613a12121e623bac1f15211de13be5fa3b75e
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-07-14 14:05:42 +0000

    small fixes
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3207 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 6066e5c0da9026d3b51e2e6a4765427922e40c86

--- compss/programming_model/bindings/python/src/pycompss/util/optional_modules.py
+++ compss/programming_model/bindings/python/src/pycompss/util/optional_modules.py
@@ -33,10 +33,10 @@ handle synchronizations implictly.""",
 
 
 def get_optional_module_warning(module_name, module_description):
-    ret = ["[ WARNING ]:\t%s module is not installed."%module_name,
+    ret = ["\n[ WARNING ]:\t%s module is not installed."%module_name,
           "\t\t%s"%module_description.replace('\n', '\n\t\t'),
           "\t\tPyCOMPSs can work withouth %s, but it is recommended to have it installed."%module_name,
-          "\t\tYou can install it via pip typing pip install %s, or (probably with your package manager)."%module_name
+          "\t\tYou can install it via pip typing pip install %s, or (probably with your package manager).\n"%module_name
     ]
     return '\n'.join(ret)
 
@@ -44,6 +44,4 @@ def show_optional_module_warnings():
     from pycompss.util.object_properties import is_module_available
     for (name, description) in optional_modules.items():
         if not is_module_available(name):
-            print
             print get_optional_module_warning(name, description)
-            print

4440e1bbbc4c28e046b677923a0a4d56988417ad
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-07-18 05:56:03 +0000

    optionals are now well formatted
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3215 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: dab6343e46f216270c71dd5153f15b5a1e2a17a7

--- compss/programming_model/bindings/python/src/ext/thread_affinity.h
+++ compss/programming_model/bindings/python/src/ext/thread_affinity.h
@@ -47,11 +47,11 @@ static PyObject* pysched_getaffinity(PyObject* self, PyObject* args);
 
 PyMethodDef module_methods[] = {
     {"setaffinity", pysched_setaffinity, METH_VARARGS,
-    "Args: (mask, pid[OPTIONAL]) -> set the affinity for the thread with given pid\
+    "Args: (mask, [pid=0]) -> set the affinity for the thread with given pid\
     to given mask. If pid equals zero, then the current thread's affinity\
     will be changed."},
     {"getaffinity", pysched_getaffinity, METH_VARARGS,
-    "Args: (pid[OPTIONAL]) -> returns the affinity for the thread with given\
+    "Args: ([pid=0]) -> returns the affinity for the thread with given\
     pid. If not specified, returns the affinity for the current thread."},
     {NULL, NULL, 0, NULL} /* Sentinel */
 };

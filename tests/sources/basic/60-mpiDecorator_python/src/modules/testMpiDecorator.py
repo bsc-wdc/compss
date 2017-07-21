c0a11f160e24ef15876e9645ff056f858e414021
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-07-21 12:59:19 +0000

    Extended Python MPI test (60) with streams and prefixes. Updated binary test with propper decorator.
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3230 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: e272bf6798e911299a3ecd9ff30039b4ffce74be

--- tests/sources/basic/60-mpiDecorator_python/src/modules/testMpiDecorator.py
+++ tests/sources/basic/60-mpiDecorator_python/src/modules/testMpiDecorator.py
@@ -16,7 +16,33 @@ def myDate(dprefix, param):
 def myDateConstrained(dprefix, param):
     pass
 
-# TODO: ADD SUPPORT FOR STREAMS !!!
+@mpi(binary="sed", workingDir=".", runner="mpirun", computingNodes=4)
+@task(file=FILE_IN)
+def mySedIN(expression, file):
+    pass
+
+@mpi(binary="sed", workingDir=".", runner="mpirun")
+@task(file=FILE_INOUT)
+def mySedINOUT(flag, expression, file):
+    pass
+
+@mpi(binary="grep", workingDir=".", runner="mpirun")
+#@task(infile=Parameter(TYPE.FILE, DIRECTION.IN, STREAM.STDIN), result=Parameter(TYPE.FILE, DIRECTION.OUT, STREAM.STDOUT))
+#@task(infile={Type:FILE_IN, Stream:STDIN}, result={Type:FILE_OUT, Stream:STDOUT})
+@task(infile={Type:FILE_IN_STDIN}, result={Type:FILE_OUT_STDOUT})
+def myGrepper(keyword, infile, result):
+    pass
+
+@mpi(binary="ls", runner="mpirun", computingNodes=2)
+@task(hide={Type:FILE_IN, Prefix:"--hide="}, sort={Type:IN, Prefix:"--sort="})
+def myLs(flag, hide, sort):
+    pass
+
+@mpi(binary="ls", runner="mpirun", computingNodes=2)
+@task(hide={Type:FILE_IN, Prefix:"--hide="}, sort={Prefix:"--sort="})
+def myLsWithoutType(flag, hide, sort):
+    pass
+
 
 class testMpiDecorator(unittest.TestCase):
 
@@ -26,4 +52,41 @@ class testMpiDecorator(unittest.TestCase):
 
     def testFunctionalUsageWithConstraint(self):
         myDateConstrained("-d", "next monday")
+        barrier()
+
+    def testFileManagementIN(self):
+        infile = "src/infile"
+        mySedIN('s/Hi/HELLO/g', infile)
+        barrier()
+
+    # Fails when retrieving the file... doesn't exist?
+    @unittest.skip("Fails to retrieve the inout file.")
+    def testFileManagementINOUT(self):
+        inoutfile = "src/inoutfile"
+        mySedINOUT('-i', 's/Hi/HELLO/g', inoutfile)
+        with compss_open(inoutfile, "r") as finout_r:
+            content_r = finout_r.read()
+        # Check if there are no Hi words, and instead there is HELLO
+        print "XXXXXXXXXXXX"
+        print content_r
+        print "XXXXXXXXXXXX"
+
+    def testFileManagement(self):
+        infile = "src/infile"
+        outfile = "src/grepoutfile"
+        myGrepper("Hi", infile, outfile)
+        barrier()
+
+    def testFilesAndPrefix(self):
+        flag = '-l'
+        infile = "src/infile"
+        sort = "size"
+        myLs(flag ,infile, sort)
+        barrier()
+
+    def testFilesAndPrefixWithoutType(self):
+        flag = '-l'
+        infile = "src/inoutfile"
+        sort = "time"
+        myLsWithoutType(flag ,infile, sort)
         barrier()

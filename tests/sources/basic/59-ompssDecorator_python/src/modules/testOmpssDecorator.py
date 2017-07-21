047b963d1ade001f7d60d825df07fb7999f439c1
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-07-21 13:06:41 +0000

    Extended Python OMPSSs test (59) with streams and prefixes. Updated binary and mpi tests result scripts.
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3231 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 213ed9ce6aafecd29cdd546f1083298991c228a5

--- tests/sources/basic/59-ompssDecorator_python/src/modules/testOmpssDecorator.py
+++ tests/sources/basic/59-ompssDecorator_python/src/modules/testOmpssDecorator.py
@@ -17,7 +17,32 @@ def myDate(dprefix, param):
 def myDateConstrained(dprefix, param):
     pass
 
-# TODO: ADD STREAMS
+@ompss(binary="sed", workingDir=".")
+@task(file=FILE_IN)
+def mySedIN(expression, file):
+    pass
+
+@ompss(binary="sed", workingDir=".")
+@task(file=FILE_INOUT)
+def mySedINOUT(flag, expression, file):
+    pass
+
+@ompss(binary="grep", workingDir=".")
+#@task(infile=Parameter(TYPE.FILE, DIRECTION.IN, STREAM.STDIN), result=Parameter(TYPE.FILE, DIRECTION.OUT, STREAM.STDOUT))
+#@task(infile={Type:FILE_IN, Stream:STDIN}, result={Type:FILE_OUT, Stream:STDOUT})
+@task(infile={Type:FILE_IN_STDIN}, result={Type:FILE_OUT_STDOUT})
+def myGrepper(keyword, infile, result):
+    pass
+
+@ompss(binary="ls")
+@task(hide={Type:FILE_IN, Prefix:"--hide="}, sort={Type:IN, Prefix:"--sort="})
+def myLs(flag, hide, sort):
+    pass
+
+@ompss(binary="ls")
+@task(hide={Type:FILE_IN, Prefix:"--hide="}, sort={Prefix:"--sort="})
+def myLsWithoutType(flag, hide, sort):
+    pass
 
 class testOmpssDecorator(unittest.TestCase):
 
@@ -27,4 +52,41 @@ class testOmpssDecorator(unittest.TestCase):
 
     def testFunctionalUsageWithConstraint(self):
         myDateConstrained("-d", "next monday")
+        barrier()
+
+    def testFileManagementIN(self):
+        infile = "src/infile"
+        mySedIN('s/Hi/HELLO/g', infile)
+        barrier()
+
+    # Fails when retrieving the file... doesn't exist?
+    @unittest.skip("Fails to retrieve the inout file.")
+    def testFileManagementINOUT(self):
+        inoutfile = "src/inoutfile"
+        mySedINOUT('-i', 's/Hi/HELLO/g', inoutfile)
+        with compss_open(inoutfile, "r") as finout_r:
+            content_r = finout_r.read()
+        # Check if there are no Hi words, and instead there is HELLO
+        print "XXXXXXXXXXXX"
+        print content_r
+        print "XXXXXXXXXXXX"
+
+    def testFileManagement(self):
+        infile = "src/infile"
+        outfile = "src/grepoutfile"
+        myGrepper("Hi", infile, outfile)
+        barrier()
+
+    def testFilesAndPrefix(self):
+        flag = '-l'
+        infile = "src/infile"
+        sort = "size"
+        myLs(flag ,infile, sort)
+        barrier()
+
+    def testFilesAndPrefixWithoutType(self):
+        flag = '-l'
+        infile = "src/inoutfile"
+        sort = "time"
+        myLsWithoutType(flag ,infile, sort)
         barrier()

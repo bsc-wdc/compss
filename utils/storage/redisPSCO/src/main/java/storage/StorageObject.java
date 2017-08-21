47b338611f33a93d4468c559be79cc956f510376
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-08-21 06:58:22 +0000

    improved redisPSCO api
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3271 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: fc4346bb73907e4d84c19747524d1ff969224b3a

--- utils/storage/redisPSCO/src/main/java/storage/StorageObject.java
+++ utils/storage/redisPSCO/src/main/java/storage/StorageObject.java
@@ -4,11 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import redis.clients.jedis.Jedis;
 
+import java.util.UUID;
+
 
 public class StorageObject implements StubItf {
 
     // Logger: According to Loggers.STORAGE
-    private static final Logger logger = LogManager.getLogger("integratedtoolkit.Storage");
+    private static final Logger logger = LogManager.getLogger("compss.Storage");
 
     private String id = null;
 
@@ -44,21 +46,54 @@ public class StorageObject implements StubItf {
      */
     @Override
     public void makePersistent(String id) {
-
+        // The object is already persisted
+        if(this.id != null) return;
+        // There was no given identifier, lets compute a random one
+        if(id == null) {
+            id = UUID.randomUUID().toString();
+        }
+        this.id = id;
+        // Call the storage API
+        try {
+            StorageItf.makePersistent(this, id);
+        } catch (StorageException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Persist the object.
+     * The identifier will be a pseudo-randomly generated UUID
+     */
+    public void makePersistent() {
+        this.makePersistent(null);
+    }
     /**
      * Deletes the persistent object occurrences
      */
     @Override
     public void deletePersistent() {
+        // The object is not persisted, do nothing
+        if(this.id == null) return;
+        // Call the storage API
+        StorageItf.removeById(this.id);
+        // Set the id as null
         this.id = null;
     }
 
     /**
-     * Sets the ID (only used by this implementation)
+     * Sets the ID
      */
     protected void setID(String id) {
+        // Remove the object from the storage system (old id)
+        StorageItf.removeById(id);
+        // Set the new id an re-add it to the storage
+        // This may cause Redis to store the object on a different node than the previous one
+        try {
+            StorageItf.makePersistent(this, id);
+        } catch (StorageException e) {
+            e.printStackTrace();
+        }
         this.id = id;
     }
 

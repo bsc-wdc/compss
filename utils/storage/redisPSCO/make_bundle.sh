bbbbd437cbe919add7239359753d743c12601df8
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-09-08 09:39:03 +0000

    add a make_bundle script that leaves all the needed resources to use Redis with the queue system (except Redis backend)
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3309 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 94ac65ffe9002d34a6eb8b877b6d7c04efd25551

---
+++ utils/storage/redisPSCO/make_bundle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script builds a bundle with all the needed stuff (aside from Redis backend)
+# to use the Redis storage API with COMPSs
+
+# Rebuild the Redis API. This should leave a directory target with an uber-JAR that contains
+# all the needed stuff
+mvn clean package
+
+# Create the bundle directory. This will be the path that we will need to pass to enqueue_compss as
+# storage_home
+mkdir -p bundle
+
+cp target/compss-redisPSCO.jar bundle/
+# Move the scripts folder to the bundle. The scripts "storage_init" and "storage_stop" will be executed
+# by COMPSs at the beggining and the end of the execution respectively. They build (destroy) a Redis cluster
+# They come with some examples
+cp -rf scripts bundle/

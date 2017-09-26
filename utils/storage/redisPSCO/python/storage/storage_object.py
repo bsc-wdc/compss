e85e4a12e720a3e8013dcdff733efb533dbf57be
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-09-26 12:44:59 +0000

    storageobject works
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3334 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 36b3f72a71673722f990080c7bd9950a56a5f343

--- utils/storage/redisPSCO/python/storage/storage_object.py
+++ utils/storage/redisPSCO/python/storage/storage_object.py
@@ -6,34 +6,34 @@ import uuid
 import api
 
 class storage_object(object):
-  '''Storage Object
-  '''
-
-  def __init__(self):
-    '''Constructor method
+    '''Storage Object
     '''
-    # Id will be None until persisted
-    self.pycompss_psco_identifier = None
 
-  def makePersistent(self):
-    '''Stores the object in the Redis database
-    '''
-    api.makePersistent(self)
+    def __init__(self):
+        '''Constructor method
+        '''
+        # Id will be None until persisted
+        self.pycompss_psco_identifier = None
 
-  def make_persistent(self):
-    '''Lowercase wrapper for makePersistent
-    '''
-    self.makePersistent()
+    def makePersistent(self):
+        '''Stores the object in the Redis database
+        '''
+        api.makePersistent(self)
 
-  def delete(self):
-    '''Deletes the object from the Redis database
-    '''
-    api.deletePersistent(self)
+    def make_persistent(self):
+        '''Support for underscore notation
+        '''
+        self.makePersistent()
 
-  def getID(self):
-    '''Gets the ID of the object
-    '''
-    return self.pycompss_psco_identifier
+    def delete(self):
+        '''Deletes the object from the Redis database
+        '''
+        api.deletePersistent(self)
+
+    def getID(self):
+        '''Gets the ID of the object
+        '''
+        return self.pycompss_psco_identifier
 
 
 '''Add support for camelCase

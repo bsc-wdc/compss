6ecdbc0e11f1d460ba3ce364c14061ed306d61bd
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-09-29 09:09:55 +0000

    modified test73
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3358 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: bd858865ef19c57df719ed70e9e421e9ee07e93f

--- tests/sources/basic/73-pscosRedis_python/src/modules/psco.py
+++ tests/sources/basic/73-pscosRedis_python/src/modules/psco.py
@@ -2,7 +2,7 @@ import storage.api
 from storage.storage_object import StorageObject
 
 class PSCO(StorageObject):
-    def __init__(self, content):
+    def __init__(self, content = "Content"):
         super(PSCO, self).__init__()
         self.content = content
 

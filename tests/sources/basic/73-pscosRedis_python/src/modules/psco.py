39965123ceaad987c500622ee9853b1eb4767e84
srodrig1 <srodrig1@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-09-29 08:04:43 +0000

    add test for Redis PSCO
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3351 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 87b6b9167ce33f0f9bcfe93226a32c9e6c59c4d2

---
+++ tests/sources/basic/73-pscosRedis_python/src/modules/psco.py
@@ -0,0 +1,13 @@
+import storage.api
+from storage.storage_object import StorageObject
+
+class PSCO(StorageObject):
+    def __init__(self, content):
+        super(PSCO, self).__init__()
+        self.content = content
+
+    def get_content(self):
+        return self.content
+
+    def set_content(self, content):
+        self.content = content

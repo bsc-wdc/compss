b7676390ea129b9bee1ca0fbb787e99d3d3215d6
aaguila <aaguila@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-10-04 10:00:51 +0000

    C persistent worker implementation and bug fixes (all tests passing), changed boost serialization protocol to binary
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3369 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: 12834dad77eab20dd7eb34346867f9cc07d45ff5

--- compss/programming_model/bindings/c/src/bindinglib/GS_templates.h
+++ compss/programming_model/bindings/c/src/bindinglib/GS_templates.h
@@ -27,6 +27,9 @@
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
+
+//#include <executor.h>
+
 using namespace std;
 using namespace boost;
 
@@ -49,7 +52,9 @@ void compss_delete_file(char * filename);
 void compss_barrier();
 FILE* compss_fopen(char * filename, char * mode);
 template <class T> void compss_wait_on(T &obj);
+template <class T> void persistent_compss_wait_on(T &obj);
 template <> inline void compss_wait_on<char *>(char * &obj);
+
 
 #ifndef COMPSS_WORKER
 
@@ -67,14 +72,21 @@ void compss_wait_on(T &obj) {
   debug_printf("[C-BINDING]  -  @compss_wait_on  -  template class\n");  
   debug_printf("[C-BINDING]  -  @compss_wait_on  -  Runtime filename: %s\n", runtime_filename);
 
-  /*ifstream ifs(runtime_filename);
-  archive::text_iarchive ia(ifs);
-  */
-  ifstream ifs(runtime_filename, std::ios::binary);
+
+
+//  ifstream ifs(runtime_filename);
+//  archive::text_iarchive ia(ifs);
+  
+
+  ifstream ifs(runtime_filename, ios::binary );  
   archive::binary_iarchive ia(ifs);
+
+
 
   ia >> obj;
   ifs.close();
+
+  cout << "file serialised in compss wait" << endl;
   
   // No longer needed, the current version of the object is in memory now
   GS_Close_File(entry.filename, in_dir);
@@ -83,11 +95,66 @@ void compss_wait_on(T &obj) {
   remove(runtime_filename);
   objectMap.erase(&obj);
 }
+/*
+template <class T>
+void persistent_compss_wait_on(T &obj) {
+  debug_printf("[C-BINDING]  -  @compss_wait_on  -  Ref: %p\n", &obj);
+  Entry entry = objectMap[&obj];
+  char *runtime_filename;
+
+  debug_printf("[C-BINDING]  -  @compss_wait_on  -  Entry.type: %d\n", entry.type);
+  debug_printf("[C-BINDING]  -  @compss_wait_on  -  Entry.classname: %s\n", entry.classname);
+  debug_printf("[C-BINDING]  -  @compss_wait_on  -  Entry.filename: %s\n", entry.filename);
+
+  GS_Get_File(entry.filename, in_dir, &runtime_filename);
+  debug_printf("[C-BINDING]  -  @compss_wait_on  -  template class\n");
+  debug_printf("[C-BINDING]  -  @compss_wait_on  -  Runtime filename: %s\n", runtime_filename);
+
+  cout << "before checking if file exists in compss wait" << endl;
+
+//  while( !filesystem::exists(runtime_filename));
+
+//  cout << "file exists? " << filesystem::exists(runtime_filename) << endl;
+
+//  ifstream ifs(runtime_filename);
+
+  ifstream ifs(runtime_filename);
+
+  bool exists = false;
+
+  while (false){
+    exists = ifs.good();
+    cout << "file exists? " << exists << endl;
+  }
+
+  cout << "about to serialize file in compss wait" << endl;
+
+  archive::text_iarchive ia(ifs);
+
+  cout << "archive created" << endl;
+
+  ia >> obj;
+  ifs.close();
+
+  cout << "file serialised in compss wait" << endl;
+
+  // No longer needed, the current version of the object is in memory now
+  GS_Close_File(entry.filename, in_dir);
+  compss_delete_file(entry.filename);
+  remove(entry.filename);
+  remove(runtime_filename);
+  objectMap.erase(&obj);
+}
+*/
+
+
 
 #else
 
 template <class T> void compss_wait_on(T &obj) { }
+template <class T> void persistent_compss_wait_on(T &obj) { }
 template <> void compss_wait_on<char *>(char * &obj) { }
+
 
 #endif /* COMPSS_WORKER */
 

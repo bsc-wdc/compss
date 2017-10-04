ba660181a5b1e3aa2c89cf19591fa21e36775a8e
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-10-04 06:57:52 +0000

    Added exceptions test for PyCOMPSs. In skip mode due to it is not finished yet. Currently evaluates the exceptions raised in the master code.
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3367 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: c3b24c8ef16fcfdb331aecde5d340cd2e01fbb55

---
+++ tests/sources/basic/74-exceptions_python/test/excInMasterBeforeTasks.py
@@ -0,0 +1,24 @@
+from pycompss.api.task import task
+
+@task(returns=int)
+def increment(v):
+    return v+1
+
+def main():
+    from pycompss.api.api import compss_wait_on
+    raise Exception('GENERAL EXCEPTION RAISED - HAPPENED BEFORE SUBMITTING TASKS AT MASTER.')
+    values = [0, 100, 200, 300]
+    for i in range(4):
+        for j in range(100):
+            values[i] = increment(values[i])
+    result = compss_wait_on(values)
+
+    if result[0] == 100 and result[1] == 200 and result[2] == 300 and result[3] == 400:
+        print "- Result value: OK"
+    else:
+        print "- Result value: ERROR"
+        print "- This error is a root error. Please fix error at test 19."
+
+
+if __name__=='__main__':
+    main()

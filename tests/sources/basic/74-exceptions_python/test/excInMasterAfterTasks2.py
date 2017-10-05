a07a8f0df8685a70d308a9d80f88ccd112281e77
fconejer <fconejer@9ab3861e-6c05-4e1b-b5ef-99af60850597>
2017-10-05 13:43:20 +0000

    Updated test 74 - Added check exceptions thrown in tasks.
    git-svn-id: http://compss.bsc.es/svn/compss/framework/trunk@3375 9ab3861e-6c05-4e1b-b5ef-99af60850597
    
    Former-commit-id: e2d907484b94b1785d8d548d3d6d3abc1b3e6200

--- tests/sources/basic/74-exceptions_python/test/excInMasterAfterTasks2.py
+++ tests/sources/basic/74-exceptions_python/test/excInMasterAfterTasks2.py
@@ -8,11 +8,11 @@ def main():
     from pycompss.api.api import compss_wait_on
     values = [0, 100, 200, 300]
     for i in range(4):
-        for j in range(100):
+        for j in range(10):
             values[i] = increment(values[i])
     result = compss_wait_on(values)
 
-    if result[0] == 100 and result[1] == 200 and result[2] == 300 and result[3] == 400:
+    if result[0] == 10 and result[1] == 110 and result[2] == 210 and result[3] == 310:
         print "- Result value: OK"
     else:
         print "- Result value: ERROR"

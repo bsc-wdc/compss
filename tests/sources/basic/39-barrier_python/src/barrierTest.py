9d2b338a3608aaf6b0e7b31656ea754cc0f5311b
CristianRCV <cristianrcv@gmail.com>
2018-05-07 16:01:10 +0200

    Test all barrier API calls in PYTHON

--- tests/sources/basic/39-barrier_python/src/barrierTest.py
+++ tests/sources/basic/39-barrier_python/src/barrierTest.py
@@ -1,30 +1,67 @@
 """
-PyCOMPSs Constraints test
+PyCOMPSs Barrier test
 =========================
     This file represents PyCOMPSs Testbench.
-    Checks the constraints, and constraints with environment variables.
+    Checks the barrier API call
 """
 
-from pycompss.api.api import compss_wait_on, compss_barrier
+from pycompss.api.api import compss_barrier
 from tasks import get_hero
 import time
 
-def main_program():
-    results = []
-    for i in range(20):
-        results.append(get_hero())
 
-    print("All tasks submitted --> waiting.")
+def launch_tasks(num_tasks):
+    results = []
+    for _ in range(num_tasks):
+        r = get_hero()
+        results.append(r)
+
+    return results
+
+
+def main_program():
+    num_tasks = 20
+
+    print ("Launching set of tasks with barrier")
+    results = launch_tasks(num_tasks)
+
     start_time = time.time()
     compss_barrier()
-    elapsed_time = time.time() - start_time
-    print("Finished waiting")
-    print("Elapsed time: ", elapsed_time)
+    end_time = time.time()
+    et1 = end_time - start_time
 
-    if elapsed_time >= 1:
-        print("- Test barrier: OK")
+    print ("Launching set of tasks with barrier and noMoreTasks false")
+    results = launch_tasks(num_tasks)
+
+    start_time = time.time()
+    compss_barrier(False)
+    end_time = time.time()
+    et2 = end_time - start_time
+
+    print ("Launching set of tasks with barrier and noMoreTasks true")
+    results = launch_tasks(num_tasks)
+
+    start_time = time.time()
+    compss_barrier(True)
+    end_time = time.time()
+    et3 = end_time - start_time
+
+    print ("Results:")
+    if et1 >= 1:
+        print("- Test barrier(): OK")
     else:
-        print("- Test barrier: ERROR")
+        print("- Test barrier(): ERROR")
+
+    if et2 >= 1:
+        print("- Test barrier(false): OK")
+    else:
+        print("- Test barrier(false): ERROR")
+
+    if et3 >= 1:
+        print("- Test barrier(true): OK")
+    else:
+        print("- Test barrier(true): ERROR")
+
 
 if __name__ == "__main__":
     main_program()
--- tests/sources/basic/39-barrier_python/src/tasks.py
+++ tests/sources/basic/39-barrier_python/src/tasks.py
@@ -7,7 +7,8 @@ from pycompss.api.task import task
 from pycompss.api.parameter import *
 import time
 
-@task(returns = int)
+
+@task(returns=int)
 def get_hero():
     print("Hero working.")
     time.sleep(1)

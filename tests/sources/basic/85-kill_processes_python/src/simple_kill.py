fe1f3f4f3be845ce65fc913dfb24fd5df0eeec7e
Marc43 <marcdominguezr@gmail.com>
2018-05-23 12:49:36 +0200

    Number of test changed

---
+++ tests/sources/basic/85-kill_processes_python/src/simple_kill.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+"""
+PyCOMPSs Testbench Simple
+========================
+"""
+
+# Imports
+rom pycompss.api.parameter import * # This error is intended, do not modify!
+from pycompss.api.task import task
+
+
+def main_program():
+    from pycompss.api.api import compss_open
+
+    # Check and get parameters
+    if len(sys.argv) != 2:
+        usage()
+        exit(-1)
+    initialValue = sys.argv[1]
+    fileName = "counter"
+
+    # Write value
+    fos = open(fileName, 'w')
+    fos.write(initialValue)
+    fos.close()
+    print("Initial counter value is " + initialValue)
+
+    # Execute increment
+    increment(fileName)
+
+    # Write new value
+    fis = compss_open(fileName, 'r+')
+    finalValue = fis.read()
+    fis.close()
+    print("Final counter value is " + finalValue)
+
+
+@task(filePath=FILE_INOUT)
+def increment(filePath):
+    print("Init task user code")
+    # Read value
+    fis = open(filePath, 'r')
+    value = fis.read()
+    print("Received " + value)
+    fis.close()
+
+    # Write value
+    fos = open(filePath, 'w')
+    newValue = str(int(value) + 1)
+    print("Computed " + newValue)
+    fos.write(newValue)
+    fos.close()
+
+
+def usage():
+    print("[ERROR] Bad number of parameters")
+    print("    Usage: simple <counterValue>")
+
+
+if __name__ == "__main__":
+    main_program()

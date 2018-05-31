827cf7fc959bb345389a5ef173120852c0690513
Marc43 <marcdominguezr@gmail.com>
2018-05-31 12:00:23 +0200

    jvmti profiling added to create a more robust test

--- tests/sources/basic/86-objectDeregisterJava/src/main/java/objectDeregister/ObjectDeregister.java
+++ tests/sources/basic/86-objectDeregisterJava/src/main/java/objectDeregister/ObjectDeregister.java
@@ -4,30 +4,41 @@ import es.bsc.compss.api.COMPSs;
 
 public class ObjectDeregister {
 
-    public static void main(String[] args) {
-    
+    public static void main(String[] args) throws Exception {
+    	    
     	/* This test provides a Dummy object used to provoke a situation where
     	 * useless objects remain in memory until the end of execution, this is
     	 * creating a loop using the same object all over. */
-    	
-    	final int ITERATIONS = 2;
+    	int k;
+    	final int ITERATIONS = 10;
     	
     	System.out.println("[LOG] Starting to execute the tasks");
     	for (int i = 0; i < ITERATIONS; ++i) {
-        	Dummy d = new Dummy(0);
+        	Dummy d = new Dummy(i);
 
         	ObjectDeregisterImpl.task1(i, d); 
-    		//ObjectDeregisterImpl.task2(i+1, d); 
+    		ObjectDeregisterImpl.task2(i+1, d); 
     		ObjectDeregisterImpl.task3(d); 
     		//Allows garbage collector to delete the object from memory
     		COMPSs.deregisterObject((Object) d);
     	}
     	System.out.println("[LOG] Finished executing tasks");
+    	COMPSs.barrier();
+    	ObjectDeregisterImpl.task4();
+    	COMPSs.barrier();
+    	System.gc();
+    	Thread.sleep(10000);
+    	System.out.println("[LOG] Garbage collector called");
+    	k = ClassInstanceTest.countInstances(Dummy.class);
+    	if (k > 0) {
+    		System.out.println("[ERROR] At the end in the MASTER " + String.valueOf(k) + " instances were found of the Dummy object");
+    		System.exit(-1);
+    	}
     	
     	/* task1 & task2 write into the object so a copy 
     	 * of the object will be created, the task3
     	 * will just read the object task2 used */
     	
     }
-
+    
 }

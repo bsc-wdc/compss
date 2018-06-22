36ce8167ea8e70fc1f3485d4c0c6f622142c630d
Francesc-Josep Lordan Gomis <francesc.lordan@gmail.com>
2018-06-22 13:09:44 +0200

    C Worker handles AppDir and InstallDir

--- compss/runtime/adaptors/gat/scripts/worker_c.sh
+++ compss/runtime/adaptors/gat/scripts/worker_c.sh
@@ -13,6 +13,12 @@
   taskTracing=false # Only available with NIO
   taskId=0 # Not used with GAT
 
+  #Removing  AppDir and Installation dir from params
+  param0=`echo ${params} | awk '{print \$1}'`
+  params=`echo ${params} | awk '{\$1=\$2=\$3="";print \$0}'`
+  params=${params:3}
+  params="$param0 $params"
+
   echo "[WORKER_C.SH] EXEC CMD: ${app_dir}/worker/worker_c $taskTracing $taskId $params"
   "${app_dir}"/worker/worker_c $taskTracing $taskId $params
   ev=$?

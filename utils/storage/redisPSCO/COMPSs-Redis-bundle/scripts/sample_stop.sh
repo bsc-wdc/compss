9e052f9ba8e86e32fa977dc1bc63deea4f0717ee
sergiorg <sergio.r.guasch@gmail.com>
2018-09-25 12:12:26 +0200

    Removed unnecessary scripts

--- utils/storage/redisPSCO/COMPSs-Redis-bundle/scripts/sample_stop.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-./storage_stop.sh 1233354 linux-rk0z linux-rk0z "" esta sample_props.cfg
--- utils/storage/redisPSCO/scripts/sample_launch.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-./storage_init.sh  123 algo algo "" esta sample_props.cfg

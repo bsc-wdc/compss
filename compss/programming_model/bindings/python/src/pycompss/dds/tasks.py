512f5a887d93153ae01f9f015c3e05fc65f76caf
nmammadl <nihad.mammadli@bsc.es>
2018-10-03 17:08:48 +0200

    Polish.

--- compss/programming_model/bindings/python/src/pycompss/dds/tasks.py
+++ compss/programming_model/bindings/python/src/pycompss/dds/tasks.py
@@ -146,7 +146,7 @@ def task_combine(iterator, creator_func, combiner_func):
     return r
 
 
-@task(a=INOUT, returns=1)
+@task(returns=1)
 def task_merge(a, b, merger_function):
     """
     Merge two combined values and update the value in the first incoming dict.
@@ -159,6 +159,8 @@ def task_merge(a, b, merger_function):
     for k, v in b.items():
         temp = merger_function(a[k], v) if k in a else v
         a[k] = temp
+
+    return a
 
 
 

30c5988f662aab7c5d1dc3091a6e250d50861688
CristianRCV <cristianrcv@gmail.com>
2018-10-18 12:54:17 +0200

    Fix runtime.log parser retrieving OK task status. Fix EI column sizes

--- compss/tools/monitor/src/main/java/monitoringParsers/RuntimeLogParser.java
+++ compss/tools/monitor/src/main/java/monitoringParsers/RuntimeLogParser.java
@@ -43,7 +43,7 @@ public class RuntimeLogParser {
     private static String runtimeLogPath = "";
     private static int lastParsedLine = -1;
 
-    private static final Logger logger = LogManager.getLogger(Loggers.RUNTIME_LOG_PARSER);
+    private static final Logger LOGGER = LogManager.getLogger(Loggers.RUNTIME_LOG_PARSER);
 
 
     public static Vector<ExecutionInformationTask> getTasks() {
@@ -63,7 +63,7 @@ public class RuntimeLogParser {
     }
 
     public static void parse() {
-        logger.debug("Parsing runtime.log file...");
+        LOGGER.debug("Parsing runtime.log file...");
         if (!Properties.getBasePath().equals("")) {
             // Check if application has changed
             String newPath = Properties.getBasePath() + File.separator + Constants.RUNTIME_LOG;
@@ -74,17 +74,17 @@ public class RuntimeLogParser {
             }
 
             // Parse
-            try ( FileReader fr = new FileReader(runtimeLogPath);
-                    BufferedReader br = new BufferedReader(fr)) {
+            try (FileReader fr = new FileReader(runtimeLogPath); BufferedReader br = new BufferedReader(fr)) {
 
                 String line = br.readLine(); // Parsed line
                 int i = 0; // Line counter
                 String lastNewJobId = new String(""); // Last new job ID entry
-                while (line != null) { // TODO add transfer files status
+                while (line != null) {
+                    // TODO add transfer files status
                     if (i > lastParsedLine) {
                         // Check line information and add to structures
                         if (line.contains("@processTask") && (line.contains("New method task") || line.contains("New service task"))) {
-                            logger.debug("* New task");
+                            LOGGER.debug("* New task");
                             String[] str = line.split(" ");
                             String taskId = str[str.length - 1];
                             String taskName = line.substring(line.lastIndexOf("task(") + 5, line.lastIndexOf(")"));
@@ -96,12 +96,12 @@ public class RuntimeLogParser {
                                 tasksCurrent.add(tasks.get(Integer.valueOf(taskId)));
                             }
                         } else if ((line.contains("@doError")) && (line.contains("rescheduling task execution"))) {
-                            logger.debug(" * Rescheduled task");
+                            LOGGER.debug(" * Rescheduled task");
                             String[] info = line.split(" ");
                             String taskId = info[info.length - 12];
                             tasks.get(Integer.valueOf(taskId)).setTaskStatus(Constants.STATUS_TASK_CREATING);
                         } else if (line.contains("@endTask") && line.contains(" with end status ")) {
-                            logger.debug("* Task End");
+                            LOGGER.debug("* Task End");
                             String taskId = line.substring(line.lastIndexOf("task ") + 5, line.lastIndexOf(" with end status"));
                             String state = line.substring(line.lastIndexOf(" with end status ") + 17);
                             if (state.equals("FINISHED")) {
@@ -114,7 +114,7 @@ public class RuntimeLogParser {
                             }
                             tasksCurrent.remove(tasks.get(Integer.valueOf(taskId)));
                         } else if ((line.contains("@errorOnAction")) && (line.contains("Blocked Action"))) {
-                            logger.debug("* Blocked Action");
+                            LOGGER.debug("* Blocked Action");
                             // Task is Blocked (we mark it as failed)
                             String taskId = line.substring(line.lastIndexOf("Task ") + 5, line.lastIndexOf(", CE name"));
                             tasks.get(Integer.valueOf(taskId)).setTaskStatus(Constants.STATUS_TASK_FAILED);
@@ -123,7 +123,7 @@ public class RuntimeLogParser {
                             }
                             tasksCurrent.remove(tasks.get(Integer.valueOf(taskId)));
                         } else if ((line.contains("@doSubmit")) && (line.contains("New Job"))) {
-                            logger.debug("* New job");
+                            LOGGER.debug("* New job");
                             String[] str = line.split(" ");
                             lastNewJobId = str[str.length - 3];
                             String taskId = str[str.length - 1].substring(0, str[str.length - 1].indexOf(")"));
@@ -139,7 +139,7 @@ public class RuntimeLogParser {
                                 tasksCurrent.add(tasks.get(Integer.valueOf(taskId)));
                             }
                         } else if ((line.contains("@doSubmit")) && (line.contains("Rescheduled Job"))) {
-                            logger.debug("* Rescheduled job");
+                            LOGGER.debug("* Rescheduled job");
                             String[] str = line.split(" ");
                             lastNewJobId = str[str.length - 3];
                             String taskId = str[str.length - 1].substring(0, str[str.length - 1].indexOf(")"));
@@ -155,11 +155,11 @@ public class RuntimeLogParser {
                                 tasksCurrent.add(tasks.get(Integer.valueOf(taskId)));
                             }
                         } else if ((line.contains("@doSubmit")) && (line.contains("* Target host"))) {
-                            logger.debug("* Add target for last new job");
+                            LOGGER.debug("* Add target for last new job");
                             String host = line.substring(line.lastIndexOf(": ") + 1);
                             tasks.get(jobsToTasks.get(Integer.valueOf(lastNewJobId))).setJobHost(lastNewJobId, false, host);
                         } else if ((line.contains("@failedJob")) && (line.contains("with state FAILED"))) {
-                            logger.debug("* Failed job");
+                            LOGGER.debug("* Failed job");
                             String[] info = line.split(" ");
                             String jobId = info[info.length - 4];
                             tasks.get(jobsToTasks.get(Integer.valueOf(jobId))).setJobStatus(Constants.STATUS_TASK_FAILED);
@@ -167,7 +167,7 @@ public class RuntimeLogParser {
                                 tasksWithFailedJobs.add(tasks.get(jobsToTasks.get(Integer.valueOf(jobId))));
                             }
                         } else if ((line.contains("@failedJob")) && (line.contains("resubmitting task to the same worker"))) {
-                            logger.debug("* Job Resubmited");
+                            LOGGER.debug("* Job Resubmited");
                             String[] info = line.split(" ");
                             String jobId = info[info.length - 16];
                             String taskId = info[info.length - 12];
@@ -178,9 +178,9 @@ public class RuntimeLogParser {
                             }
                             resubmitedJobs.add(jobId);
                         } else if ((line.contains("@completedJob")) && (line.contains("with state OK"))) {
-                            logger.debug("* Job completed");
+                            LOGGER.debug("* Job completed");
                             String[] info = line.split(" ");
-                            String jobId = info[info.length - 4];
+                            String jobId = info[info.length - 7];
                             tasks.get(jobsToTasks.get(Integer.valueOf(jobId))).setJobStatus(Constants.STATUS_TASK_DONE);
                         }
                     }
@@ -189,15 +189,15 @@ public class RuntimeLogParser {
                 }
                 lastParsedLine = i - 1;
             } catch (Exception e) {
-                logger.error("Cannot parse runtime.log file: " + runtimeLogPath);
-                logger.debug("Error ", e);
+                LOGGER.error("Cannot parse runtime.log file: " + runtimeLogPath);
+                LOGGER.debug("Error ", e);
                 clear();
             }
         } else {
             // Load default value
             clear();
         }
-        logger.debug("runtime.log file parsed");
+        LOGGER.debug("runtime.log file parsed");
     }
 
     private static void clear() {

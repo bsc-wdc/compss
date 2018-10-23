45b1843af1bd2be01737b16b2b606d9b325e3124
fconejer <francisco.conejero@bsc.es>
2018-10-23 17:59:54 +0200

    Improved test 58 results check and homogeneized with tests 59 and 60.

--- tests/sources/basic/58-binaryDecorator_python/src/modules/testBinaryDecorator.py
+++ tests/sources/basic/58-binaryDecorator_python/src/modules/testBinaryDecorator.py
@@ -44,9 +44,9 @@ def mySedIN(expression, file):
     pass
 
 
-@binary(binary="sed", workingDir=".")
-@task(returns=int, file=FILE_IN)
-def mySedReturn(expression, file):
+@binary(binary="date", workingDir=".")
+@task(returns=int)
+def myReturn():
     pass
 
 
@@ -102,15 +102,14 @@ class testBinaryDecorator(unittest.TestCase):
         compss_barrier()
 
     def testReturn(self):
-        infile = "src/infile"
-        ev = mySedReturn('s/Hi/HELLO/g', infile)
+        ev = myReturn()
         ev = compss_wait_on(ev)
         self.assertEqual(ev, 0)
 
     def testFailedBinaryExitValue(self):
         ev = failedBinary(123)
         ev = compss_wait_on(ev)
-        self.assertEqual(ev, 123)
+        self.assertEqual(ev, 123)  # own exit code for failed execution
 
     @unittest.skip("UNSUPPORTED WITH GAT")
     def testFileManagementINOUT(self):

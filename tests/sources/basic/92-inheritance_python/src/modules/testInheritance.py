42210065aa68db2e69c8d54ccb56f1d929ac2b43
fconejer <javier.conejero@bsc.es>
2018-12-28 13:02:16 +0100

    Extended inheritance test - check same class name from different file.

--- tests/sources/basic/92-inheritance_python/src/modules/testInheritance.py
+++ tests/sources/basic/92-inheritance_python/src/modules/testInheritance.py
@@ -79,54 +79,54 @@ class testInheritance(unittest.TestCase):
         self.assertEqual(obj.get_value(), 2 * (1234 + 4) / 4)
 
     '''
-    INHERITANCE FROM A DIFFERENT FILE
+    INHERITANCE FROM A DIFFERENT FILE WITH SAME CLASS NAME
     '''
 
     def testSimpleInheritance_non_modifier_othermodule(self):
-        from .otherclasses import inheritedClass2
-        obj = inheritedClass2()
+        from .otherclasses_same_name import inheritedClass
+        obj = inheritedClass()
         o = obj.increment_non_modifier(1)
         o = compss_wait_on(o)
         self.assertEqual(o, 1235)
 
     def testSimpleInheritance_modifier_othermodule(self):
-        from .otherclasses import inheritedClass2
-        obj = inheritedClass2()
+        from .otherclasses_same_name import inheritedClass
+        obj = inheritedClass()
         obj.increment_modifier(1)
         obj = compss_wait_on(obj)
         self.assertEqual(obj.get_value(), 1235)
 
     def testInheritance_with_overriding_non_modifier_othermodule(self):
-        from .otherclasses import inheritedClassWithOverride2
-        obj = inheritedClassWithOverride2()
+        from .otherclasses_same_name import inheritedClassWithOverride
+        obj = inheritedClassWithOverride()
         o = obj.increment_non_modifier(1)
         o = compss_wait_on(o)
         self.assertEqual(o, 1235 * 2)
 
     def testInheritance_with_overriding_modifier_othermodule(self):
-        from .otherclasses import inheritedClassWithOverride2
-        obj = inheritedClassWithOverride2()
+        from .otherclasses_same_name import inheritedClassWithOverride
+        obj = inheritedClassWithOverride()
         obj.increment_modifier(1)
         obj = compss_wait_on(obj)
         self.assertEqual(obj.get_value(), 1235 * 2)
 
     def testInheritance_extended_non_modifier_othermodule(self):
-        from .otherclasses import inheritedClassExtended2
-        obj = inheritedClassExtended2()
+        from .otherclasses_same_name import inheritedClassExtended
+        obj = inheritedClassExtended()
         o = obj.multiplier_non_modifier(4)
         o = compss_wait_on(o)
         self.assertEqual(o, 1234 * 4)
 
     def testInheritance_extended_modifier_othermodule(self):
-        from .otherclasses import inheritedClassExtended2
-        obj = inheritedClassExtended2()
+        from .otherclasses_same_name import inheritedClassExtended
+        obj = inheritedClassExtended()
         obj.multiplier_modifier(4)
         obj = compss_wait_on(obj)
         self.assertEqual(obj.get_value(), 1234 * 4)
 
     def testInheritance_multilevel_non_modifier_othermodule(self):
-        from .otherclasses import inheritedClassMultilevelOverridedExtended2
-        obj = inheritedClassMultilevelOverridedExtended2()
+        from .otherclasses_same_name import inheritedClassMultilevelOverridedExtended
+        obj = inheritedClassMultilevelOverridedExtended()
         o = obj.increment_non_modifier(4)
         o = compss_wait_on(o)
         p = obj.divider_non_modifier(4)
@@ -135,7 +135,71 @@ class testInheritance(unittest.TestCase):
         self.assertEqual(p, 1234 / 4)
 
     def testInheritance_multilevel_modifier_othermodule(self):
-        from .otherclasses import inheritedClassMultilevelOverridedExtended2
+        from .otherclasses_same_name import inheritedClassMultilevelOverridedExtended
+        obj = inheritedClassMultilevelOverridedExtended()
+        obj.increment_modifier(4)
+        obj.divider_modifier(4)
+        obj = compss_wait_on(obj)
+        self.assertEqual(obj.get_value(), 2 * (1234 + 4) / 4)
+
+    '''
+    INHERITANCE FROM A DIFFERENT FILE WITH SAME CLASS NAME
+    '''
+
+    def testSimpleInheritance_non_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClass2
+        obj = inheritedClass2()
+        o = obj.increment_non_modifier(1)
+        o = compss_wait_on(o)
+        self.assertEqual(o, 1235)
+
+    def testSimpleInheritance_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClass2
+        obj = inheritedClass2()
+        obj.increment_modifier(1)
+        obj = compss_wait_on(obj)
+        self.assertEqual(obj.get_value(), 1235)
+
+    def testInheritance_with_overriding_non_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClassWithOverride2
+        obj = inheritedClassWithOverride2()
+        o = obj.increment_non_modifier(1)
+        o = compss_wait_on(o)
+        self.assertEqual(o, 1235 * 2)
+
+    def testInheritance_with_overriding_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClassWithOverride2
+        obj = inheritedClassWithOverride2()
+        obj.increment_modifier(1)
+        obj = compss_wait_on(obj)
+        self.assertEqual(obj.get_value(), 1235 * 2)
+
+    def testInheritance_extended_non_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClassExtended2
+        obj = inheritedClassExtended2()
+        o = obj.multiplier_non_modifier(4)
+        o = compss_wait_on(o)
+        self.assertEqual(o, 1234 * 4)
+
+    def testInheritance_extended_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClassExtended2
+        obj = inheritedClassExtended2()
+        obj.multiplier_modifier(4)
+        obj = compss_wait_on(obj)
+        self.assertEqual(obj.get_value(), 1234 * 4)
+
+    def testInheritance_multilevel_non_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClassMultilevelOverridedExtended2
+        obj = inheritedClassMultilevelOverridedExtended2()
+        o = obj.increment_non_modifier(4)
+        o = compss_wait_on(o)
+        p = obj.divider_non_modifier(4)
+        p = compss_wait_on(p)
+        self.assertEqual(o, 2 * (1234 + 4))
+        self.assertEqual(p, 1234 / 4)
+
+    def testInheritance_multilevel_modifier_othermodule2(self):
+        from .otherclasses_diff_name import inheritedClassMultilevelOverridedExtended2
         obj = inheritedClassMultilevelOverridedExtended2()
         obj.increment_modifier(4)
         obj.divider_modifier(4)

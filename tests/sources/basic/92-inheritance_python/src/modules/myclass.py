5a868c3f8b830fade1789429da79edacd44b425b
fconejer <javier.conejero@bsc.es>
2019-01-08 12:14:57 +0100

    Added nested and multiple inheritance checks in test 92.

--- tests/sources/basic/92-inheritance_python/src/modules/myclass.py
+++ tests/sources/basic/92-inheritance_python/src/modules/myclass.py
@@ -33,7 +33,6 @@ class myClass(object):
     def get_value(self):
         return self.value
 
-
 class inheritedClass(myClass):
     pass
 
@@ -90,3 +89,51 @@ class inheritedClassMultilevelOverridedExtended(inheritedClassWithOverride):
     @task(returns = int)
     def divider(cls, v):
         return cls.static_value / v
+
+class nestedInheritance(object):
+    def __init__(self):
+        self.mc = myClass()
+
+class myOtherClass(object):
+    other_static_value = 5432
+
+    def __init__(self, other_value):
+        self.other_value = other_value
+
+    @task(returns = int)
+    def decrement_non_modifier(self, v):
+        print("self.other_value: ", self.other_value)
+        print("V: ", v)
+        return self.other_value - v
+
+    @task()
+    def decrement_modifier(self, v):
+        print("self.other_value: ", self.other_value)
+        print("V: ", v)
+        self.other_value = self.other_value - v
+
+    @classmethod
+    @task(returns = int)
+    def decrement(cls, v):
+        return cls.other_static_value - v
+
+    def get_other_value(self):
+        return self.other_value
+
+class joinerClass(object):
+    @task()
+    def multiplier_modifier(self):
+        print("self.value: ", self.value)
+        print("self.other_value: ", self.other_value)
+        self.joined_value = self.value * self.other_value
+
+    @task(returns = int)
+    def multiplier_non_modifier(self):
+        print("self.value: ", self.value)
+        print("self.other_value: ", self.other_value)
+        return self.value * self.other_value
+
+class multipleInheritanceClass(myClass, myOtherClass, joinerClass):
+    def __init__(self, value):
+        myClass.__init__(self)
+        myOtherClass.__init__(self, value)

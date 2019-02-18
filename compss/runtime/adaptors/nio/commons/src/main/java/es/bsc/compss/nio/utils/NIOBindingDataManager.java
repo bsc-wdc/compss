9be063734413af72bf0e5a39664d02208c31f214
CristianRCV <cristianrcv@gmail.com>
2019-02-18 12:18:03 +0100

    Remove unnecessary sync on ConcurrentHashMap in NIOBindingDataManager

--- compss/runtime/adaptors/nio/commons/src/main/java/es/bsc/compss/nio/utils/NIOBindingDataManager.java
+++ compss/runtime/adaptors/nio/commons/src/main/java/es/bsc/compss/nio/utils/NIOBindingDataManager.java
@@ -35,7 +35,7 @@ public class NIOBindingDataManager extends BindingDataManager {
     protected static final Logger LOGGER = LogManager.getLogger(Loggers.COMM);
     private static final boolean DEBUG = LOGGER.isDebugEnabled();
 
-    private static Map<NIOConnection, NIOBindingObjectTransferListener> listeners = new ConcurrentHashMap<NIOConnection, NIOBindingObjectTransferListener>();
+    private static final Map<NIOConnection, NIOBindingObjectTransferListener> LISTENERS = new ConcurrentHashMap<NIOConnection, NIOBindingObjectTransferListener>();
 
     static {
         System.loadLibrary("bindings_common");
@@ -49,25 +49,23 @@ public class NIOBindingDataManager extends BindingDataManager {
     public static void receiveBindingObject(NIOAgent agent, NIOConnection c, String sourceId, int type) {
         Semaphore sem = new Semaphore(0);
         NIOBindingObjectTransferListener nbol = new NIOBindingObjectTransferListener(agent, sem);
-        synchronized (listeners) {
-            listeners.put(c, nbol);
-        }
+        LISTENERS.put(c, nbol);
+
         NIOBindingObjectStream nbos = new NIOBindingObjectStream((NIOConnection) c, nbol);
         NIOBindingObjectReceiver receiver = new NIOBindingObjectReceiver(c, sourceId, type, nbos);
         Thread t = new Thread(receiver);
         t.setName("BindingObjectReceiver_" + sourceId);
         t.start();
-
     }
 
     public static void receivedPartialBindingObject(NIOConnection c, Transfer t) {
-        NIOBindingObjectTransferListener nbol = listeners.get(c);
+        NIOBindingObjectTransferListener nbol = LISTENERS.get(c);
         nbol.setTransfer(t);
         nbol.notifyEnd();
     }
 
     public static void objectReceived(NIOConnection c) {
-        NIOBindingObjectTransferListener nbol = listeners.remove(c);
+        NIOBindingObjectTransferListener nbol = LISTENERS.remove(c);
         nbol.getAgent().receivedData(c, nbol.getTransfer());
     }
 

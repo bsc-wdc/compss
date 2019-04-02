8bdc7734d8b473178cc32886b9e1f71d09a3a16a
mbertran <marta.bertran@bsc.es>
2019-04-02 13:29:43 +0200

    Test for deletion of file in sharedDisk

---
+++ tests/sources/java/2_delete_file_shared_disk/src/main/java/deleteFileSharedDisk/DeleteFileSharedDiskItf.java
@@ -0,0 +1,18 @@
+package deleteFileSharedDisk;
+
+import es.bsc.compss.types.annotations.Constraints;
+import es.bsc.compss.types.annotations.Parameter;
+import es.bsc.compss.types.annotations.parameter.Type;
+import es.bsc.compss.types.annotations.parameter.Direction;
+import es.bsc.compss.types.annotations.task.Method;
+
+
+public interface DeleteFileSharedDiskItf {
+
+    @Constraints(computingUnits = "1")
+    @Method(declaringClass = "deleteFileSharedDisk.DeleteFileSharedDiskImpl")
+    void readFromFile(
+        @Parameter(type = Type.FILE, direction = Direction.IN) String file
+    );
+
+}

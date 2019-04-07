8f11ad74aad6d089d3dca838a2e078afec4f3af4
ramonamela <ramon.amela@gmail.com>
2019-04-07 17:49:39 +0200

    FIFO and LIFO scheduler with queue and stack

--- compss/runtime/scheduler/fifoScheduler/src/main/java/es/bsc/compss/scheduler/fifoScheduler/FIFOScheduler.java
+++ compss/runtime/scheduler/fifoScheduler/src/main/java/es/bsc/compss/scheduler/fifoScheduler/FIFOScheduler.java
@@ -16,55 +16,257 @@
  */
 package es.bsc.compss.scheduler.fifoScheduler;
 
+import es.bsc.compss.components.impl.ResourceScheduler;
+import es.bsc.compss.components.impl.TaskScheduler;
+import es.bsc.compss.scheduler.exceptions.BlockedActionException;
+import es.bsc.compss.scheduler.exceptions.UnassignedActionException;
 import es.bsc.compss.scheduler.readyScheduler.ReadyScheduler;
 import es.bsc.compss.scheduler.types.AllocatableAction;
+import es.bsc.compss.scheduler.types.ObjectValue;
 import es.bsc.compss.scheduler.types.Score;
 import es.bsc.compss.types.resources.Worker;
 import es.bsc.compss.types.resources.WorkerResourceDescription;
+import es.bsc.compss.util.ErrorManager;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.json.JSONObject;
 
-
 /**
- * Representation of a Scheduler that considers only ready tasks and sorts them in FIFO mode
+ * Representation of a Scheduler that considers only ready tasks and sorts them
+ * in FIFO mode
  *
  */
-public class FIFOScheduler extends ReadyScheduler {
+public class FIFOScheduler extends TaskScheduler {
 
-    /**
-     * Constructs a new Ready Scheduler instance
-     *
-     */
-    public FIFOScheduler() {
-        super();
-    }
+	protected LinkedList<AllocatableAction> unassignedReadyActions;
+	protected final HashSet<ResourceScheduler<?>> availableWorkers;
+	protected int amountOfWorkers;
 
-    /*
-     * *********************************************************************************************************
-     * *********************************************************************************************************
-     * ***************************** UPDATE STRUCTURES OPERATIONS **********************************************
-     * *********************************************************************************************************
-     * *********************************************************************************************************
-     */   
-    @Override
-    public <T extends WorkerResourceDescription> FIFOResourceScheduler<T> generateSchedulerForResource(Worker<T> w, JSONObject resJSON,
-            JSONObject implJSON) {
-        // LOGGER.debug("[FIFOScheduler] Generate scheduler for resource " + w.getName());
-        return new FIFOResourceScheduler<>(w, resJSON, implJSON);
-    }
+	/**
+	 * Constructs a new Ready Scheduler instance
+	 *
+	 */
+	public FIFOScheduler() {
+		super();
+		this.unassignedReadyActions = new LinkedList<AllocatableAction>();
+		this.availableWorkers = new HashSet<ResourceScheduler<?>>();
+		this.amountOfWorkers = 0;
+	}
 
-    @Override
-    public Score generateActionScore(AllocatableAction action) {
-        // LOGGER.debug("[FIFOScheduler] Generate Action Score for " + action);
-        return new Score(action.getPriority(), -action.getId(), 0, 0);
-    }
-    
-    /*
-     * *********************************************************************************************************
-     * *********************************************************************************************************
-     * ********************************* SCHEDULING OPERATIONS *************************************************
-     * *********************************************************************************************************
-     * *********************************************************************************************************
-     */
+	/*
+	 * *****************************************************************************
+	 * ****************************
+	 * *****************************************************************************
+	 * **************************** ***************************** UPDATE STRUCTURES
+	 * OPERATIONS **********************************************
+	 * *****************************************************************************
+	 * ****************************
+	 * *****************************************************************************
+	 * ****************************
+	 */
+	@Override
+	public <T extends WorkerResourceDescription> void workerLoadUpdate(ResourceScheduler<T> resource) {
 
+	}
+
+	@Override
+	public <T extends WorkerResourceDescription> FIFOResourceScheduler<T> generateSchedulerForResource(Worker<T> w,
+			JSONObject resJSON, JSONObject implJSON) {
+		// LOGGER.debug("[FIFOScheduler] Generate scheduler for resource " +
+		// w.getName());
+		return new FIFOResourceScheduler<>(w, resJSON, implJSON);
+	}
+
+	@Override
+	protected <T extends WorkerResourceDescription> void workerDetected(ResourceScheduler<T> resource) {
+		super.workerDetected(resource);
+		this.availableWorkers.add(resource);
+		this.amountOfWorkers += 1;
+	}
+
+	@Override
+	protected <T extends WorkerResourceDescription> void workerRemoved(ResourceScheduler<T> resource) {
+		super.workerRemoved(resource);
+		this.availableWorkers.remove(resource);
+		this.amountOfWorkers -= 1;
+		if (this.amountOfWorkers == 0) {
+			for (AllocatableAction action : this.unassignedReadyActions) {
+				addToBlocked(action);
+			}
+			this.unassignedReadyActions.clear();
+		}
+	}
+
+	@Override
+	public Score generateActionScore(AllocatableAction action) {
+		// LOGGER.debug("[FIFOScheduler] Generate Action Score for " + action);
+		return new Score(action.getPriority(), -action.getId(), 0, 0);
+	}
+
+	/*
+	 * *****************************************************************************
+	 * ****************************
+	 * *****************************************************************************
+	 * **************************** ********************************* SCHEDULING
+	 * OPERATIONS *************************************************
+	 * *****************************************************************************
+	 * ****************************
+	 * *****************************************************************************
+	 * ****************************
+	 */
+	@Override
+	protected void scheduleAction(AllocatableAction action, Score actionScore) throws BlockedActionException {
+		if (!action.hasDataPredecessors()) {
+			try {
+				if (DEBUG) {
+					LOGGER.debug("[ReadyScheduler] Trying to schedule action " + action);
+				}
+				action.tryToSchedule(actionScore, this.availableWorkers);
+				ResourceScheduler<? extends WorkerResourceDescription> resource = action.getAssignedResource();
+				if (!resource.canRunSomething()) {
+					this.availableWorkers.remove(resource);
+				}
+				removeActionFromSchedulerStructures(action);
+				if (DEBUG) {
+					LOGGER.debug("[ReadyScheduler] Remove action " + action + " from scheduler structures");
+				}
+			} catch (UnassignedActionException ex) {
+				if (DEBUG) {
+					LOGGER.debug("[ReadyScheduler] Introducing action " + action
+							+ " into the scheduler from try to schedule action");
+				}
+				addActionToSchedulerStructures(action);
+			}
+		}
+	}
+
+	protected <T extends WorkerResourceDescription> void scheduleAction(AllocatableAction action,
+			ResourceScheduler<T> targetWorker, Score actionScore)
+			throws BlockedActionException, UnassignedActionException {
+		// This if should be eraseble since we only handle dependency free actions
+		if (!action.hasDataPredecessors()) {
+			action.schedule(targetWorker, actionScore);
+			removeActionFromSchedulerStructures(action);
+		}
+	}
+
+	@Override
+	public List<AllocatableAction> getUnassignedActions() {
+		return this.unassignedReadyActions;
+	}
+
+	@Override
+	public final <T extends WorkerResourceDescription> void handleDependencyFreeActions(
+			List<AllocatableAction> dataFreeActions, List<AllocatableAction> resourceFreeActions,
+			List<AllocatableAction> blockedCandidates, ResourceScheduler<T> resource) {
+		if (DEBUG) {
+			LOGGER.debug("[ReadyScheduler] Treating dependency free actions on resource " + resource.getName());
+		}
+		this.availableWorkers.add(resource);
+		purgeFreeActions(dataFreeActions, resourceFreeActions, blockedCandidates, resource);
+		tryToLaunchFreeActions(dataFreeActions, resourceFreeActions, blockedCandidates, resource);
+	}
+
+	public <T extends WorkerResourceDescription> void purgeFreeActions(List<AllocatableAction> dataFreeActions,
+			List<AllocatableAction> resourceFreeActions, List<AllocatableAction> blockedCandidates,
+			ResourceScheduler<T> resource) {
+	}
+
+	private void addActionToSchedulerStructures(AllocatableAction action) {
+		if (this.unassignedReadyActions.isEmpty()) {
+			if (DEBUG) {
+				LOGGER.debug(
+						"[ReadyScheduler] Cannot add action " + action + " because there are not available resources");
+			}
+			addToBlocked(action);
+		}else {
+			if(!this.unassignedReadyActions.contains(action)) {
+				if (DEBUG) {
+					LOGGER.debug("[ReadyScheduler] Add action to scheduler structures " + action);
+				}
+				this.unassignedReadyActions.addLast(action);
+			}
+		}
+	}
+
+	private void removeActionFromSchedulerStructures(AllocatableAction action) {
+		this.unassignedReadyActions.remove(action);
+	}
+
+	protected <T extends WorkerResourceDescription> void tryToLaunchFreeActions(List<AllocatableAction> dataFreeActions,
+			List<AllocatableAction> resourceFreeActions, List<AllocatableAction> blockedCandidates,
+			ResourceScheduler<T> resource) {
+		if (DEBUG) {
+			LOGGER.debug("[ReadyScheduler] Try to launch free actions on resource " + resource.getName() + " with "
+					+ this.unassignedReadyActions.size() + " candidates in this worker");
+		}
+
+		// Actions that have been freeded by the action that just finished
+		for (AllocatableAction freeAction : dataFreeActions) {
+			if (DEBUG) {
+				LOGGER.debug(
+						"[ReadyScheduler] Introducing action " + freeAction + " into the scheduler from data free");
+			}
+			addActionToSchedulerStructures(freeAction);
+		}
+		dataFreeActions = new LinkedList<AllocatableAction>();
+
+		// Resource free actions should always be empty in this scheduler
+		for (AllocatableAction freeAction : resourceFreeActions) {
+			if (DEBUG) {
+				LOGGER.debug(
+						"[ReadyScheduler] Introducing action " + freeAction + " into the scheduler from resource free");
+			}
+			addActionToSchedulerStructures(freeAction);
+		}
+		resourceFreeActions = new LinkedList<AllocatableAction>();
+
+		// Only in case there are actions that have entered the scheduler without having
+		// available resources -> They were in the blocked list
+		for (AllocatableAction freeAction : blockedCandidates) {
+			if (DEBUG) {
+				LOGGER.debug("[ReadyScheduler] Introducing action " + freeAction + " into the scheduler from blocked");
+			}
+			addActionToSchedulerStructures(freeAction);
+		}
+		blockedCandidates = new LinkedList<AllocatableAction>();
+
+		Iterator<AllocatableAction> executableActionsIterator = this.unassignedReadyActions.iterator();
+		HashSet<AllocatableAction> actionsToErase = new HashSet<AllocatableAction>();
+		while (executableActionsIterator.hasNext() && !this.availableWorkers.isEmpty()) {
+			AllocatableAction freeAction = executableActionsIterator.next();
+			try {
+				freeAction.tryToSchedule(generateActionScore(freeAction), this.availableWorkers);
+				ResourceScheduler<? extends WorkerResourceDescription> assignedResource = freeAction
+						.getAssignedResource();
+				tryToLaunch(freeAction);
+				if (!assignedResource.canRunSomething()) {
+					this.availableWorkers.remove(assignedResource);
+				}
+				actionsToErase.add(freeAction);
+			} catch (BlockedActionException e) {
+				actionsToErase.add(freeAction);
+				addToBlocked(freeAction);
+			} catch (UnassignedActionException e) {
+				// Nothing to be done here since the action was already in the scheduler
+				// structures. If there is an exception, the freeAction will not be added
+				// to the objectValueToErase list.
+				// Hence, this is not an ignored Exception but an expected behavior.
+			}
+		}
+		for (AllocatableAction action : actionsToErase) {
+			removeActionFromSchedulerStructures(action);
+		}
+	}
 }

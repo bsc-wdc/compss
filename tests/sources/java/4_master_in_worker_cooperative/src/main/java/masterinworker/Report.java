89f9d94ccbb4a67e83e3ed4b821191aba4c9a2de
Francesc-Josep Lordan Gomis <francesc.lordan@gmail.com>
2019-04-29 11:27:47 +0200

    Master in worker test cooperative

---
+++ tests/sources/java/4_master_in_worker_cooperative/src/main/java/masterinworker/Report.java
@@ -0,0 +1,43 @@
+
+package masterinworker;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+
+public class Report implements Externalizable {
+
+    private long startTime;
+    private long endTime;
+
+    public Report() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void completedExecution() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput oo) throws IOException {
+        oo.writeLong(startTime);
+        oo.writeLong(endTime);
+    }
+
+    @Override
+    public void readExternal(ObjectInput oi) throws IOException, ClassNotFoundException {
+        this.startTime = oi.readLong();
+        this.endTime = oi.readLong();
+    }
+
+}

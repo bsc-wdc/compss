7e6a02fd730148d7635b2d9b1a5ea7ff0e2a896d
Francesc-Josep Lordan Gomis <francesc.lordan@gmail.com>
2019-04-29 14:29:49 +0200

    Checkstyle tests worker_in_master

--- tests/sources/java/4_master_in_worker_cooperative/src/main/java/masterinworker/Tasks.java
+++ tests/sources/java/4_master_in_worker_cooperative/src/main/java/masterinworker/Tasks.java
@@ -138,7 +138,7 @@ public class Tasks {
     }
 
     /**
-     * Verifies that the content of the file matches the one passed in as a parameter on COMPSsWorker02
+     * Verifies that the content of the file matches the one passed in as a parameter on COMPSsWorker02.
      *
      * @param content text expected to be found on the file
      * @param fileName path of the file to analyze

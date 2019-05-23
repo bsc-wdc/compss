e74be066ffce9f901b14bc8dbc7671dea2adf54a
Jorge Ejarque <jorge.ejarque@bsc.es>
2019-05-23 19:43:30 +0200

    docker deployment updates for new docker interface

--- compss/runtime/scripts/system/docker/generate_docker_resources.sh
+++ compss/runtime/scripts/system/docker/generate_docker_resources.sh
@@ -2,12 +2,13 @@
 
   # Retrieve script arguments
   resources_file=$1
-  num_workers=$1
-  cus=$2
-  memory=$3
-  swarm_manager_ip=$4
-  image_name=$5
-  creation_time=$6
+  num_workers=$2
+  cus=$3
+  memory=$4
+  swarm_manager_ip=$5
+  image_name=$6
+  cloud=$7
+  creation_time=$8
 
   # Define script constants
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -24,9 +25,11 @@
   for (( i=1; i<=num_workers; i++ )); do
     add_compute_node "worker$i" "$cus" "0" "0" "$memory" "$((40000 + 2*(i-1) + 1))" "$((40000 + 2*(i-1) + 2))" "" ""
   done
-  # Add cloud
-  its="small:1:0:0:2.0:1:0.85 medium:2:0:0:3.0:1:0.95 large:4:0:0:4.0:1:1.25 extra_large:8:0:0:8.0:1:2.25"
-  add_cloud "Docker" "tcp://$swarm_manager_ip" "docker-conn.jar" "es.bsc.conn.docker.Docker" "$image_name" "" "$creation_time" "43000" "43003" "" "${its}"
+  if [ "${cloud}" == "True" ]; then
+    echo "Adding cloud ..."
+    its="small:1:0:0:2.0:1:0.85 medium:2:0:0:3.0:1:0.95 large:4:0:0:4.0:1:1.25 extra_large:8:0:0:8.0:1:2.25"
+    add_cloud "Docker" "tcp://$swarm_manager_ip" "docker-conn.jar" "es.bsc.conn.docker.Docker" "$image_name" "" "$creation_time" "43000" "43003" "" "${its}"
+  fi
   # Close resources (from generate_resources.sh)
   add_footer
 

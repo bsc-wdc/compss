da0dd927bf6e2d0c08988fb77d77279508cfcb47
CristianRCV <cristianrcv@gmail.com>
2019-05-24 15:22:58 +0200

    Add distro-stream-lib implementation in python (cloned from original repo)

---
+++ compss/programming_model/bindings/python/src/pycompss/streams/components/distro_stream_client.py
@@ -0,0 +1,307 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+# For better print formatting
+from __future__ import print_function
+
+# Imports
+import unittest
+import logging
+import socket
+from threading import Thread
+
+try:
+    # Python 3
+    import queue
+except ImportError:
+    # Python 2
+    import Queue as queue
+
+# Project imports
+from pycompss.streams.types.requests import STOP
+from pycompss.streams.types.requests import StopRequest
+
+#
+# Logger definition
+#
+
+logger = logging.getLogger("pycompss.streams.distro_stream_client")
+
+
+#
+# Client Handler
+#
+
+class DistroStreamClientHandler:
+    """
+    Handler to use the DistroStreamClient. This is a static class.
+
+    Attributes:
+        - CLIENT: Distributed Stream Client to handle.
+            + type: DistroStreamClient
+    """
+
+    CLIENT = None
+
+    def __init__(self):
+        """
+        Creates a new handler instance. Should never be called directly since all attributes are static.
+        """
+        # Nothing to do since this is a static handler
+        pass
+
+    @staticmethod
+    def init_and_start(master_ip=None, master_port=None):
+        """
+        Initializes and starts the client.
+
+        :param master_ip: Master IP.
+            + type: string
+        :param master_port: Master port.
+            + type: int
+        :return: None.
+        """
+        DistroStreamClientHandler.CLIENT = DistroStreamClient(master_ip=master_ip, master_port=master_port)
+        DistroStreamClientHandler.CLIENT.start()
+
+    @staticmethod
+    def set_stop():
+        """
+        Marks the client to stop.
+
+        :return: None.
+        """
+        req = StopRequest()
+        DistroStreamClientHandler.CLIENT.add_request(req)
+
+    @staticmethod
+    def request(req):
+        """
+        Adds a new request to the client.
+
+        :param req: Client request.
+            + type: Subclass of Request
+        :return: None.
+        """
+        DistroStreamClientHandler.CLIENT.add_request(req)
+
+
+#
+# Client definition
+#
+class DistroStreamClient(Thread):
+    """
+    Distro Stream Client definition.
+
+    Attributes:
+        - master_ip: Master IP address.
+            + type: string
+        - master_port: Master port.
+            + type: int
+        - running: Whether the client thread is running or not
+            + type: boolean
+        - requests: Queue of pending client requests
+            + type: Queue.Queue
+
+    """
+
+    def __init__(self, master_ip=None, master_port=None):
+        """
+        Creates a new Client associated to the given master properties.
+
+        :param master_ip: Master IP address.
+            + type: string
+        :param master_port: Master port.
+            + type: int
+        """
+        super(DistroStreamClient, self).__init__()
+
+        logger.info("Initializing DS Client on " + str(master_ip) + ":" + str(master_port))
+
+        # Register information
+        self.master_ip = master_ip
+        self.master_port = master_port
+
+        # Initialize internal structures
+        self.running = True
+        self.requests = queue.Queue()
+
+    def run(self):
+        """
+        Running method of the internal thread.
+
+        :return: None.
+        """
+        logger.info("DS Client started")
+
+        while self.running:
+            # Process requests
+            req = self.requests.get(block=True)
+
+            if __debug__:
+                logger.debug("Processing request: " + str(req.get_type()))
+
+            if req.get_type() == STOP:
+                logger.info("DS Client asked to stop")
+                self.running = False
+                req.set_response("DONE")
+            else:
+                self._process_request(req)
+
+            # Mark the request as processed
+            req.set_processed()
+            self.requests.task_done()
+
+        logger.info("DS Client stopped")
+
+    def _process_request(self, req):
+        # Process requests to the server
+
+        # Open socket connection
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.master_ip, self.master_port))
+
+            # Send request message
+            req_msg = req.get_request_msg()
+            if __debug__:
+                logger.debug("Sending request to server: " + str(req_msg))
+            req_msg = req_msg + "\n"
+            s.sendall(req_msg)
+
+            # Receive answer
+            answer = s.recv(1024)
+            answer = answer.decode(encoding='UTF-8').strip()
+            if __debug__:
+                logger.debug("Received answer from server: " + str(answer))
+            req.set_response(answer)
+        except Exception as e:
+            print(e)
+            # Some error occurred, mark request as failed and keep going
+            req.set_error(1, str(e))
+
+    def add_request(self, req):
+        """
+        Adds a new request to the client.
+
+        :param req: Request to add.
+            + type: Request subclass.
+        :return: None.
+        """
+        if __debug__:
+            logger.debug("Adding new request to client queue: " + str(req.get_type()))
+
+        self.requests.put(req, block=True)
+
+
+#
+# UNIT TEST CASES
+#
+
+class TestDistroStreamClient(unittest.TestCase):
+
+    def disabled_test_client(self):
+        """
+        Tests the client. Requires a valid server into the given master_port.
+
+        :return: None.
+        """
+        # Master port
+        master_port = 49049
+
+        # Imports
+        import uuid
+
+        from pycompss.streams.types.requests import RegisterStreamRequest
+        from pycompss.streams.types.requests import StreamStatusRequest
+        from pycompss.streams.types.requests import CloseStreamRequest
+        from pycompss.streams.distro_stream import FILE
+        from pycompss.streams.distro_stream import AT_MOST_ONCE
+
+        try:
+            # Start
+            print("Start")
+            DistroStreamClientHandler.init_and_start(master_ip="localhost", master_port=master_port)
+
+            # Send request
+            print("Send request")
+            req = RegisterStreamRequest(None, FILE, AT_MOST_ONCE, ["/tmp/file_stream_python/"])
+            DistroStreamClientHandler.request(req)
+
+            # Wait response
+            print("Wait response")
+            req.wait_processed()
+
+            # Process response
+            print("Received: ")
+            print(" - Error Code    : " + str(req.get_error_code()))
+            print(" - Error Message : " + str(req.get_error_msg()))
+            print(" - Response      : " + str(req.get_response_msg()))
+            stream_id = uuid.UUID(req.get_response_msg())
+
+            # Send request
+            print("Send request")
+            req = StreamStatusRequest(stream_id)
+            DistroStreamClientHandler.request(req)
+
+            # Wait response
+            print("Wait response")
+            req.wait_processed()
+
+            # Process response
+            print("Received: ")
+            print(" - Error Code    : " + str(req.get_error_code()))
+            print(" - Error Message : " + str(req.get_error_msg()))
+            print(" - Response      : " + str(req.get_response_msg()))
+
+            # Send request
+            print("Send request")
+            req = CloseStreamRequest(stream_id)
+            DistroStreamClientHandler.request(req)
+
+            # Wait response
+            print("Wait response")
+            req.wait_processed()
+
+            # Process response
+            print("Received: ")
+            print(" - Error Code    : " + str(req.get_error_code()))
+            print(" - Error Message : " + str(req.get_error_msg()))
+            print(" - Response      : " + str(req.get_response_msg()))
+        finally:
+            DistroStreamClientHandler.set_stop()
+
+    def test_client_handler(self):
+        """
+        Tests the client handler with two different processes.
+
+        :return:
+        """
+
+        def runner():
+            print("Starting process")
+            print("Init Client Handler")
+            DistroStreamClientHandler.init_and_start()
+            print("Stop Client Handler")
+            DistroStreamClientHandler.set_stop()
+            print("End process")
+
+        from multiprocessing import Process
+        p1 = Process(target=runner)
+        p2 = Process(target=runner)
+
+        p1.start()
+        p2.start()
+
+        p1.join()
+        p2.join()
+
+
+#
+# MAIN FOR UNIT TEST
+#
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(name)s - %(message)s')
+    unittest.main()
---
+++ compss/programming_model/bindings/python/src/pycompss/streams/distro_stream.py
@@ -0,0 +1,346 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+# For better print formatting
+from __future__ import print_function
+
+# Imports
+import unittest
+import logging
+from abc import abstractmethod
+
+# Project imports
+from pycompss.streams.types.requests import RegisterStreamRequest
+from pycompss.streams.types.requests import CloseStreamRequest
+from pycompss.streams.types.requests import StreamStatusRequest
+from pycompss.streams.types.requests import PollRequest
+from pycompss.streams.components.distro_stream_client import DistroStreamClientHandler
+
+#
+# Logger definition
+#
+
+logger = logging.getLogger("pycompss.streams.distro_stream")
+
+#
+# Type Enums
+#
+# class StreamType(Enum):
+FILE = "FILE"
+OBJECT = "OBJECT"
+PSCO = "PSCO"
+
+# class ConsumerMode(Enum):
+AT_MOST_ONCE = "AT_MOST_ONCE"
+AT_LEAST_ONCE = "AT_LEAST_ONCE"
+
+
+def str2bool(val):
+    return val.lower() in ("yes", "true", "t", "1")
+
+
+#
+# Interface definition
+#
+class DistroStream(object):
+    """
+    Interface for File and Object Distributed Streams.
+
+    Attributes:
+        - alias: Stream Alias.
+            + type: string
+        - id: Stream Id.
+            + type: string containing UUID
+        - stream_type: Internal stream type.
+            + type: StreamType
+        - access_mode: Stream consumer access mode.
+            + type: ConsumerMode
+
+    """
+
+    def __init__(self, alias=None, stream_type=None, access_mode=AT_MOST_ONCE, internal_stream_info=None):
+        """
+        Creates a new DistroStream instance.
+
+        :param alias: Stream alias.
+            + type: string
+        :param stream_type: Internal stream type.
+            + type: StreamType
+        :param access_mode: Stream access mode.
+            + type: ConsumerMode
+        :param internal_stream_info: Implementation specific information.
+            + type: List<T>
+        :raise RegistrationException: When client cannot register the stream into the server.
+        """
+        logger.debug("Registering new stream...")
+
+        self.alias = alias
+        self.stream_type = stream_type
+        self.access_mode = access_mode
+
+        # Retrieve registration id
+        req = RegisterStreamRequest(self.alias, self.stream_type, self.access_mode, internal_stream_info)
+        DistroStreamClientHandler.request(req)
+
+        req.wait_processed()
+        error = req.get_error_code()
+        if error != 0:
+            raise RegistrationException(error, req.get_error_msg())
+        self.id = req.get_response_msg()
+
+    def get_stream_type(self):
+        """
+        Returns the internal stream type.
+
+        :return: The internal stream type
+            + type: StreamType
+        """
+        return self.stream_type
+
+    def close(self):
+        """
+        Closes the current stream.
+
+        :return: None
+        """
+        if __debug__:
+            logger.debug("Closing stream " + str(self.id))
+
+        # Ask for stream closure
+        req = CloseStreamRequest(self.id)
+        DistroStreamClientHandler.request(req)
+
+        req.wait_processed()
+        error = req.get_error_code()
+        if error != 0:
+            logger.error("ERROR: Cannot close stream")
+            logger.error(" - Internal Error Code: " + str(error))
+            logger.error(" - Internal Error Msg: " + str(req.get_error_msg()))
+
+        # No need to process the answer message, checking the error is enough.
+
+    def is_closed(self):
+        """
+        Returns whether the stream is closed or not.
+
+        :return: True if the stream is closed, False otherwise.
+            + type: boolean
+        """
+        if __debug__:
+            logger.debug("Checking if stream " + str(self.id) + " is closed")
+
+        # Ask for stream status
+        req = StreamStatusRequest(self.id)
+        DistroStreamClientHandler.request(req)
+
+        req.wait_processed()
+        error = req.get_error_code()
+        if error != 0:
+            logger.error("ERROR: Cannot retrieve stream status")
+            logger.error(" - Internal Error Code: " + str(error))
+            logger.error(" - Internal Error Msg: " + str(req.get_error_msg()))
+
+        return str2bool(req.get_response_msg())
+
+    @abstractmethod
+    def publish(self, message):
+        """
+        Publishes the given message on the stream.
+
+        :param message: Message to publish.
+        :return: None
+        """
+        pass
+
+    @abstractmethod
+    def publish_list(self, messages):
+        """
+        Publishes the given list of messages on the stream.
+
+        :param messages: List of messages to publish.
+        :return: None
+        """
+        pass
+
+    @abstractmethod
+    def poll(self, timeout=None):
+        """
+        Polls the produced messages. If there are registered messages, returns immediately. Otherwise,
+         waits until a record is produced or the timeout is exceeded.
+
+        :param timeout: Maximum request time to poll new messages.
+        :return: List of polled messages.
+            + type: List<T>
+        """
+        pass
+
+
+#
+# FileDistroStream definition
+#
+class FileDistroStream(DistroStream):
+    """
+    File Distributed Stream implementation.
+
+    Attributes:
+        - base_dir: Base directory path for the streaming.
+            + type: string
+
+    """
+
+    def __init__(self, alias=None, access_mode=AT_MOST_ONCE, base_dir=None):
+        """
+        Creates a new FileDistroStream instance.
+
+        :param alias: Stream alias.
+            + type: string
+        :param access_mode: Stream access mode.
+            + type: ConsumerMode
+        :param base_dir: Base directory for the file stream.
+            + type: string
+        :raise RegistrationException: When client cannot register the stream into the server.
+        """
+        super(FileDistroStream, self).__init__(alias=alias,
+                                               stream_type=FILE,
+                                               access_mode=access_mode,
+                                               internal_stream_info=[base_dir])
+        self.base_dir = base_dir
+
+    def publish(self, message):
+        # Nothing to do since server automatically publishes the written files
+        logger.warn("WARN: Unnecessary call on publish on FileDistroStream")
+
+    def publish_list(self, messages):
+        # Nothing to do since server automatically publishes the written files
+        logger.warn("WARN: Unnecessary call on publish on FileDistroStream")
+
+    def poll(self, timeout=None):
+        logger.info("Polling new stream items...")
+
+        # Send request to server
+        req = PollRequest(self.id)
+        DistroStreamClientHandler.request(req)
+
+        # Retrieve answer
+        req.wait_processed()
+        error = req.get_error_code()
+        if error != 0:
+            raise BackendException(error, req.get_error_msg())
+
+        # Parse answer
+        info = req.get_response_msg()
+        if __debug__:
+            logger.debug("Retrieved stream items: " + str(info))
+        if info is not None and info and info != "null":
+            return info.split()
+        else:
+            return []
+
+
+#
+# ObjectDistroStream definition
+#
+class ObjectDistroStream(DistroStream):
+    """
+    Object Distributed Stream implementation.
+
+    Attributes:
+    """
+
+    def __init__(self, alias=None, access_mode=AT_MOST_ONCE):
+        """
+        Creates a new ObjectDistroStream instance.
+
+        :param alias: Stream alias.
+            + type: string
+        :param access_mode: Stream access mode.
+            + type: ConsumerMode
+        :raise RegistrationException: When client cannot register the stream into the server.
+        """
+        super(ObjectDistroStream, self).__init__(alias=alias,
+                                                 stream_type=FILE,
+                                                 access_mode=access_mode,
+                                                 internal_stream_info=[])
+        raise Exception("ERROR: ObjectDistroStream is not supported in Python")
+
+    def publish(self, message):
+        pass
+
+    def publish_list(self, messages):
+        pass
+
+    def poll(self, timeout=None):
+        pass
+
+
+#
+# Exception Class
+#
+
+class RegistrationException(Exception):
+
+    def __init__(self, code=None, message=None):
+        """
+        Creates a new RegistrationException instance.
+
+        :param code: Internal request error code.
+            + type: int
+        :param message: Internal request error message.
+            + type: string
+        """
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        s = "ERROR: Registration Exception.\n"
+        s = s + " - Internal error code: " + str(self.code)
+        s = s + " - Internal error message:" + str(self.message)
+
+        return s
+
+
+class BackendException(Exception):
+
+    def __init__(self, code=None, message=None):
+        """
+        Creates a new BackendException instance.
+
+        :param code: Internal request error code.
+            + type: int
+        :param message: Internal request error message.
+            + type: string
+        """
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        s = "ERROR: Backend Exception.\n"
+        s = s + " - Internal error code: " + str(self.code)
+        s = s + " - Internal error message:" + str(self.message)
+
+        return s
+
+
+#
+# UNIT TEST CASES
+#
+
+class TestDistroStream(unittest.TestCase):
+
+    def test_file_distro_stream(self):
+        # TODO: Add test
+        pass
+
+    def test_object_distro_stream(self):
+        # TODO: Add test
+        pass
+
+
+#
+# MAIN FOR UNIT TEST
+#
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(name)s - %(message)s')
+    unittest.main()

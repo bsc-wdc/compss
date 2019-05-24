f5468105e7d249f832b6c981e0d7e0eb590d0ac8
ramonamela <ramon.amela@gmail.com>
2019-05-24 15:25:38 +0200

    Add function to treat args

--- compss/programming_model/bindings/python/src/exaqute/ExaquteTask.py
+++ compss/programming_model/bindings/python/src/exaqute/ExaquteTask.py
@@ -26,3 +26,28 @@ def delete_object(obj):
 
 def compute(obj):
     raise Exception("Compute not implemented in the current scheduler")
+
+def from_args_to_vector(obj):
+    print("FROM ARGS TO VECTOR")
+    index = [i for i, x in enumerate(obj) if x == "#"]
+    print(index)
+    print(list(enumerate(obj)))
+    if len(index) == 0:
+        return obj
+    new_vector = []
+    new_vector.append(list(obj[0:index[0]]))
+    for i in range(len(index) - 1):
+        new_vector.append(list(obj[(index[i] + 1):index[i + 1]]))
+    new_vector.append(list(obj[(index[-1] + 1):len(obj)]))
+    print("Return:")
+    print(new_vector)
+    return new_vector
+
+
+def from_vector_to_args(obj):
+    new_vector = []
+    new_vector.extend(obj[0])
+    for i in range(1, len(obj)):
+        new_vector.append("#")
+        new_vector.extend(obj[1])
+    return new_vector

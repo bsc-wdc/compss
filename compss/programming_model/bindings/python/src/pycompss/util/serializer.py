ca655bdbe58c0d67309a4009617300aed5a30ceb
fconejer <javier.conejero@bsc.es>
2019-05-29 18:07:18 +0200

    Fixed unnecessary serialization stacktraces in logs.

--- compss/programming_model/bindings/python/src/pycompss/util/serializer.py
+++ compss/programming_model/bindings/python/src/pycompss/util/serializer.py
@@ -20,24 +20,34 @@
 """
 PyCOMPSs Utils: Data serializer/deserializer
 This file implements the main serialization/deserialization functions.
-All serialization/deserialization calls should be made using one of the following functions:
+All serialization/deserialization calls should be made using one of the
+following functions:
 
-- serialize_to_file(obj, file_name) -> dumps the object "obj" to the file "file_name"
+- serialize_to_file(obj, file_name) -> dumps the object "obj" to the file
+                                       "file_name"
 - serialize_to_string(obj) -> dumps the object "obj" to a string
-- serialize_to_handler(obj, handler) -> writes the serialized object using the specified handler
-                                        it also moves the handler's pointer to the end of the dump
+- serialize_to_handler(obj, handler) -> writes the serialized object using the
+                                        specified handler it also moves the
+                                        handler's pointer to the end of the
+                                        dump
 
-- deserialize_from_file(file_name) -> loads the first object from the tile "file_name"
-- deserialize_from_string(serialized_content) -> loads the first object from the given string
-- deserialize_from_handler(handler) -> deserializes an object using the given handler, it also leaves the
-                                       handler's pointer pointing to the end of the serialized object
+- deserialize_from_file(file_name) -> loads the first object from the tile
+                                      "file_name"
+- deserialize_from_string(serialized_content) -> loads the first object from
+                                                 the given string
+- deserialize_from_handler(handler) -> deserializes an object using the given
+                                       handler, it also leaves the handler's
+                                       pointer pointing to the end of the
+                                       serialized object
 """
 
 import types
 import traceback
 
 from pycompss.runtime.commons import IS_PYTHON3
-from .serialization.extended_support import pickle_generator, convert_to_generator, GeneratorIndicator
+from pycompss.util.serialization.extended_support import pickle_generator
+from pycompss.util.serialization.extended_support import convert_to_generator
+from pycompss.util.serialization.extended_support import GeneratorIndicator
 from .object_properties import object_belongs_to_module
 
 from io import BytesIO
@@ -111,7 +121,8 @@ def serialize_to_handler(obj, handler):
     Serialize an object to a handler.
 
     :param obj: Object to be serialized.
-    :param handler: A handler object. It must implement methods like write, writeline and similar stuff
+    :param handler: A handler object. It must implement methods like write,
+                    writeline and similar stuff
     """
 
     # Get the serializer priority
@@ -140,12 +151,16 @@ def serialize_to_handler(obj, handler):
                 if serializer is numpy and NUMPY_AVAILABLE:
                     serializer.save(handler, obj)
                 else:
-                    serializer.dump(obj, handler, protocol=serializer.HIGHEST_PROTOCOL)
+                    serializer.dump(obj, 
+                                    handler, 
+                                    protocol=serializer.HIGHEST_PROTOCOL)
                 success = True
             except Exception:
-                if __debug__:
-                    traceback.print_exc()  # No need to print all serializer exceptions
-                    print('WARNING! Serialization with %s failed.' % str(serializer))
+                success = False
+                # if __debug__:
+                #    traceback.print_exc()  # No need to show all stacktraces
+                #    print('WARNING! Serialization with %s failed.' %
+                #          str(serializer))
         i += 1
 
     # if ret_value is None then all the serializers have failed
@@ -187,26 +202,38 @@ def serialize_to_string(obj):
     handler.close()
     return ret
 
+
 def serialize_named_to_file(name, obj, file_name):
-    '''Serialize a named object to a file
+    """
+    Serialize a named object to a file
+
     :param name: Name of the object
     :param obj: Name of the object
     :param file_name: Name of the file name
     :return: Nothing, it just serializes the named object
-    '''
+    """
+
     serialize_to_file((name, obj), file_name)
 
+
 def serialize_named_to_string(name, obj):
-    '''Serialize a named object to a string
+    """
+    Serialize a named object to a string
+
     :param name: Name of the object
     :param obj: Object to serialize
     :return: <String> the serialized content
-    '''
+    """
+
     return serialize_to_string((name, obj))
 
+
 def get_numpy_dummy_obj():
-    '''Return a numpy object or an empty list if numpy is not available
-    '''
+    """
+    Return a numpy object or an empty list if numpy is not available
+
+    :return: numpy zeros list or empty list
+    """
     try:
         return numpy.zeros(1)
     except:
@@ -214,43 +241,49 @@ def get_numpy_dummy_obj():
 
 
 def deserialize_from_handler(handler):
-    '''Deserialize an object from a file.
-    :param handler: File name from where the object is going to be deserialized.
+    """
+    Deserialize an object from a file.
+
+    :param handler: File name from where the object is going to be
+                    deserialized.
     :return: The object deserialized.
-    '''
+    """
+
     # Retrieve the used library (if possible)
     try:
         original_position = handler.tell()
         serializer = idx2lib[int(handler.read(4))]
     except:
         handler.seek(original_position)
-        raise SerializerException('Handler does not refer to a valid PyCOMPSs object')
+        error_message = 'Handler does not refer to a valid PyCOMPSs object'
+        raise SerializerException(error_message)
 
     try:
         ret = serializer.load(handler)
         # Special case: deserialized obj wraps a generator
-        if isinstance(ret, tuple) and ret and isinstance(ret[0], GeneratorIndicator):
+        if isinstance(ret, tuple) and \
+                ret and \
+                isinstance(ret[0], GeneratorIndicator):
             ret = convert_to_generator(ret[1])
         return ret
     except Exception:
         if __debug__:
-            print('WARNING! Deserialization with %s failed.' % str(serializer))
-            traceback.print_exc()  # No need to print all deserialize exceptions
-    # We are not able to deserialize the contents from file_name with any of our
-    # serializers
-    try:
-        traceback.print_exc()
-    except AttributeError:
-        # Bug fixed in 3.5 - issue10805
-        pass
-    raise SerializerException('Cannot deserialize object')
+            print('ERROR! Deserialization with %s failed.' % str(serializer))
+            try:
+                traceback.print_exc()
+            except AttributeError:
+                # Bug fixed in 3.5 - issue10805
+                pass
+        raise SerializerException('Cannot deserialize object')
 
 
 def deserialize_from_file(file_name):
-    '''Deserialize the contents in a given file.
+    """
+    Deserialize the contents in a given file.
+
     :param file_name: Name of the file with the contents to be deserialized
     :return: A deserialized object
-    '''
+    """
 
     handler = open(file_name, 'rb')
     ret = deserialize_from_handler(handler)
@@ -259,23 +292,30 @@ def deserialize_from_file(file_name):
 
 
 def deserialize_from_string(serialized_content):
-    '''
+    """
     Deserialize the contents in a given string.
+
     :param serialized_content: A string with serialized contents
     :return: A deserialized object
-    '''
+    """
+
     handler = BytesIO(serialized_content)
     ret = deserialize_from_handler(handler)
     handler.close()
     return ret
 
+
 def serialize_objects(to_serialize):
-    '''
-    Serialize a list of objects to file. If a single object fails to be serialized, then an Exception by
-    serialize_to_file will be thrown (and not caught)
-    The structure of the parameter is [(object1, file_name1), ... , (objectN, file_nameN)].
-    :param to_serialize: List of lists to be serialized. Each sublist is a pair of the form ['object','file name']
-    :return:
-    '''
+    """
+    Serialize a list of objects to file. If a single object fails to be
+    serialized, then an Exception by serialize_to_file will be thrown
+    (and not caught).
+    The structure of the parameter is:
+         [(object1, file_name1), ... , (objectN, file_nameN)].
+    :param to_serialize: List of lists to be serialized. Each sublist is a
+                         pair of the form ['object','file name']
+    :return: None
+    """
+
     for obj_and_file in to_serialize:
         serialize_to_file(*obj_and_file)

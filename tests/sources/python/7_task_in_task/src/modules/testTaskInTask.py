70e1c412eccd87aedcd424e2b7059095139ecb29
fconejer <javier.conejero@bsc.es>
2019-05-31 18:01:17 +0200

    Enhanced task in task test with external tasks and class task. Pep8 format.

--- tests/sources/python/7_task_in_task/src/modules/testTaskInTask.py
+++ tests/sources/python/7_task_in_task/src/modules/testTaskInTask.py
@@ -13,39 +13,46 @@ import unittest
 from pycompss.api.api import compss_barrier
 from pycompss.api.api import compss_wait_on
 from pycompss.api.task import task
-from pycompss.api.parameter import *
-
 from pycompss.api.constraint import constraint
 from pycompss.api.implement import implement
 from pycompss.api.multinode import multinode
+from .external import external
+from .external import externalc
+from .external import example
 
 
 @task(returns=1)
 def level1(value):
     return value + level2(value)
 
+
 @task(returns=1)
 def level2(value):
     return value + level3(value)
 
+
 @task(returns=1)
 def level3(value):
     return value * 2
+
 
 @constraint(computing_units="2")
 @task(returns=1)
 def level1c(value):
     return level2c(value) / value
 
+
 @constraint(computing_units="2")
 @task(returns=1)
 def level2c(value):
     return value + level3c(value)
 
+
 @constraint(computing_units="2")
 @task(returns=1)
 def level3c(value):
     return value * 2
+
 
 # force not to run this task, just the implementation
 @constraint(computing_units="100")
@@ -54,6 +61,7 @@ def slow_task(value):
     # 80 + 60 - 40 = 100
     return level1(value) + level2c(value) - level3(value)
 
+
 @implement(source_class="modules.testTaskInTask", method="slow_task")
 @constraint(computing_units="1")
 @task(returns=list)
@@ -61,12 +69,23 @@ def fast_task(value):
     # 3 + 60 + 40 = 103
     return level1c(value) + level2(value) + level3c(value)
 
+
 @constraint(computing_units="2")
 @multinode(computing_nodes="2")
 @task(returns=1)
 def multi_node_task(value):
     # 80 * 60 * 40 = 192000
     return level1(value) * level2c(value) * level3(value)
+
+
+@task(returns=1)
+def ext_task(value):
+    return value / external(value)
+
+
+@task(returns=1)
+def ext_task_c(value):
+    return value / externalc(value)
 
 
 class testTaskInTask(unittest.TestCase):
@@ -81,12 +100,28 @@ class testTaskInTask(unittest.TestCase):
         result = compss_wait_on(result)
         self.assertEqual(result, 3)
 
-    def testImplementArgWarn(self):
+    def testImplementTaskInTask(self):
         result = slow_task(20)
         result = compss_wait_on(result)
         self.assertEqual(result, 3 + 60 + 40)
 
-    def testMultinodeArgWarn(self):
+    def testMultinodeTaskInTask(self):
         result = multi_node_task(20)
         result = compss_wait_on(result)
         self.assertEqual(result, 80 * 60 * 40)
+
+    def testTaskInTaskExt(self):
+        result = ext_task(20)
+        result = compss_wait_on(result)
+        self.assertEqual(result, 2)
+
+    def testTaskInTaskExtC(self):
+        result = ext_task_c(20)
+        result = compss_wait_on(result)
+        self.assertEqual(result, 4)
+
+    def testTaskInTaskClass(self):
+        o = example(4)
+        o.increment(20)
+        o = compss_wait_on(o)
+        self.assertEqual(o.get_v(), 15)

a01dc65f46e9f77342207111d3dc0deb0e875e8e
nmammadl <nihad.mammadli@bsc.es>
2019-06-05 15:52:15 +0200

    [DDS] Introduction to tests.

---
+++ tests/sources/python/0_dds/src/test_dds.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+"""
+PyCOMPSs DDS test
+=========================
+    This file represents PyCOMPSs Testbench.
+    Tests of DDS Library
+"""
+
+# Imports
+
+
+def main_program():
+
+    print("____________TEST DDS______________")
+
+    print("- Test DDS: OK")
+
+
+if __name__ == "__main__":
+    main_program()

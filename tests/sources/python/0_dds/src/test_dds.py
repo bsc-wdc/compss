3dcb70d7f605eba8949272ebcea0cf4f641a159f
nmammadl <nihad.mammadli@bsc.es>
2019-06-06 11:40:39 +0200

    [DDS] Test of multiple files.

--- tests/sources/python/0_dds/src/test_dds.py
+++ tests/sources/python/0_dds/src/test_dds.py
@@ -11,6 +11,7 @@ PyCOMPSs DDS test
 
 # Imports
 import tempfile
+import os, shutil
 
 from pycompss.dds import DDS
 
@@ -28,13 +29,36 @@ def test_loader_functions():
         tmp_f.write(bytearray(content, "utf8"))
     dds = DDS().load_file(filename).collect()
     assert dds[0] == content
+    os.remove(filename)
 
+    # Multiple files
+    _dir = os.path.join(tempfile.gettempdir(), "multi")
+    if not os.path.exists(_dir):
+        os.mkdir(_dir)
+
+    for _i in range(10):
+        desc, filename = tempfile.mkstemp(dir=_dir)
+        file_path = os.path.join(_dir, filename)
+        with open(file_path, "wb") as tmp_f:
+            tmp_f.write(bytearray(str(_i), "utf8"))
+
+    dds = DDS().load_files_from_dir(_dir).collect()
+    dds = sorted(dds, key=lambda x: x[1])
+    for _i in range(10):
+        assert int(dds[_i][1]) == _i
+    shutil.rmtree(_dir)
+
+    return True
 
 
 def main_program():
 
     print("____________TEST DDS______________")
-    test_loader_functions()
+    if not test_loader_functions():
+        print("- Test DDS Loader Functions: FAILED")
+        # If loader functions fail, quit the tests..
+        return
+
     print("- Test DDS: OK")
 
 

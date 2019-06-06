7064f95043c53aaf3713c443b59b011b19dad23b
nmammadl <nihad.mammadli@bsc.es>
2019-06-06 10:43:58 +0200

    [DDS] Iterator and Single File tests.

--- tests/sources/python/0_dds/src/test_dds.py
+++ tests/sources/python/0_dds/src/test_dds.py
@@ -10,12 +10,31 @@ PyCOMPSs DDS test
 """
 
 # Imports
+import tempfile
+
+from pycompss.dds import DDS
+
+
+def test_loader_functions():
+
+    # Iterator loader
+    dds = DDS().load(range(10)).collect()
+    assert len(dds) == 10
+
+    # Single file
+    content = "Holala World!"
+    desc, filename = tempfile.mkstemp()
+    with open(filename, "wb") as tmp_f:
+        tmp_f.write(bytearray(content, "utf8"))
+    dds = DDS().load_file(filename).collect()
+    assert dds[0] == content
+
 
 
 def main_program():
 
     print("____________TEST DDS______________")
-
+    test_loader_functions()
     print("- Test DDS: OK")
 
 

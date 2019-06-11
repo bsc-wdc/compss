3a34a51a0d6623a29d8f9a204c81eaafecaa8f16
nmammadl <nihad.mammadli@bsc.es>
2019-06-11 15:14:59 +0200

    [DDS] Word Count tests.

--- tests/sources/python/0_dds/src/test_dds.py
+++ tests/sources/python/0_dds/src/test_dds.py
@@ -51,6 +51,31 @@ def test_loader_functions():
     return True
 
 
+def word_count():
+    # Word Count
+    vocabulary = ["Holala ", "World ", "test ", "COMPSs ", "Lorem ", "Ipsum "]
+
+    _dir = os.path.join(tempfile.gettempdir(), "wc")
+    if not os.path.exists(_dir):
+        os.mkdir(_dir)
+    for _i in range(5):
+        desc, filename = tempfile.mkstemp(dir=_dir, suffix=".txt")
+        file_path = os.path.join(_dir, filename)
+        tmp_f = open(file_path, "w")
+        for word in vocabulary:
+            tmp_f.write(word)
+        tmp_f.close()
+
+    dds = DDS().load_files_from_dir(_dir) \
+        .map_and_flatten(lambda x: x[1].split())\
+        .count_by_value(as_dict=True)
+
+    for value in dds.values():
+        assert value == 5
+
+    shutil.rmtree(_dir)
+
+
 def main_program():
 
     print("____________TEST DDS______________")
@@ -58,7 +83,7 @@ def main_program():
         print("- Test DDS Loader Functions: FAILED")
         # If loader functions fail, quit the tests..
         return
-
+    word_count()
     print("- Test DDS: OK")
 
 

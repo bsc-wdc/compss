a38d584799783ef8fb087cf7928d83ff486f1155
CristianRCV <cristianrcv@gmail.com>
2019-06-20 13:22:16 +0200

    Update DistroStreamLibrary version for python3

--- compss/programming_model/bindings/python/src/pycompss/streams/components/distro_stream_client.py
+++ compss/programming_model/bindings/python/src/pycompss/streams/components/distro_stream_client.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python
-#
-#  Copyright 2002-2019 Barcelona Supercomputing Center (www.bsc.es)
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
 
 # -*- coding: utf-8 -*-
 
@@ -183,6 +168,7 @@ class DistroStreamClient(Thread):
             if __debug__:
                 logger.debug("Sending request to server: " + str(req_msg))
             req_msg = req_msg + "\n"
+            req_msg = req_msg.encode()
             s.sendall(req_msg)
 
             # Receive answer

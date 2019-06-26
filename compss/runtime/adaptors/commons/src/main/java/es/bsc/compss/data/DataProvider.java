2009e14630f1d542d7e99608acd996daabedee1f
Jorge Ejarque <jorge.ejarque@bsc.es>
2019-06-26 10:22:43 +0200

    changes to fix problems of transfers problems in GPFS

--- compss/runtime/adaptors/commons/src/main/java/es/bsc/compss/data/DataProvider.java
+++ compss/runtime/adaptors/commons/src/main/java/es/bsc/compss/data/DataProvider.java
@@ -37,5 +37,11 @@ public interface DataProvider {
      * @param tt Transfer listener.
      */
     public void askForTransfer(InvocationParam param, int index, FetchDataListener tt);
+    
+    /** Check if data in the parameter is being transferred.
+     * @param param Invocation Parameter to check
+     * @return {@code true} if data is being transferred, {@code false} otherwise.
+     */
+    public boolean isTransferingData(InvocationParam param);
 
 }

40ea4285cd20432bcb03d2d1a30c14ee508a717a
CristianRCV <cristianrcv@gmail.com>
2019-07-10 14:08:30 +0200

    Update DistroStreamLibrary to 1.0-19

--- compss/programming_model/bindings/python/src/pycompss/streams/types/__init__.py
+++ compss/programming_model/bindings/python/src/pycompss/streams/types/__init__.py
@@ -14,4 +14,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+
+# For * imports                                                                                                                                                                                                                             
 __all__ = ['requests']

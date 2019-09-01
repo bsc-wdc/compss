39f6551e5f84051086ec7b3bf197f3ff66dfbf3d
Leroy Jenkins <jenkins@bsc.es>
2019-09-01 22:37:39 +0200

    Update to version 2.5.rc1909

--- compss/programming_model/bindings/python/setup.py
+++ compss/programming_model/bindings/python/setup.py
@@ -89,7 +89,7 @@ def find_packages(path='./src'):
 setup(
     # Metadata
     name='pycompss',
-    version='2.5.rc1908',
+    version='2.5.rc1909',
     description='Python Binding for COMP Superscalar Runtime',
     long_description=open('README.txt').read(),
     author='Workflows and Distributed Computing Group (WDC) - Barcelona Supercomputing Center (BSC)',

64dfbd7522d083599ca9e6f91e6d27c210c2f301
Clément Foyer <cfoyer@cray.com>
2019-10-14 12:25:58 +0200

    Add unit tests
    Add assert check on serialization checks.
    Change serialization test to a tuple. Check returned values.

--- tests/sources/python/1_serialization/src/modules/testSerialization.py
+++ tests/sources/python/1_serialization/src/modules/testSerialization.py
@@ -14,7 +14,6 @@ import numpy as np
 
 from pycompss.util.serialization.serializer import deserialize_from_file, serialize_to_file
 
-
 class testSerialization(unittest.TestCase):
 
     def testObjectArray(self):
@@ -24,5 +23,8 @@ class testSerialization(unittest.TestCase):
 
         tmp_file = tempfile.NamedTemporaryFile()
 
-        serialize_to_file(arr, tmp_file.name)
-        deserialize_from_file(tmp_file.name)
+        serialize_to_file(((50,50), arr), tmp_file.name) # Serialise tuple instead of array
+        dim, arr_test = deserialize_from_file(tmp_file.name)
+
+        self.assertTrue((50, 50) == dim)
+        self.assertTrue(np.array_equal(arr, arr_test))
---
+++ tests/sources/python/1_sharedarray/src/modules/testSharedArray.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+"""
+PyCOMPSs Testbench Tasks
+========================
+"""
+
+# Imports
+import unittest
+import tempfile
+import numpy as np
+
+from os.path import basename
+
+from pycompss.util.sharedmemory.shma import serialize_to_shm,       \
+                                            deserialize_from_shm,   \
+                                            delete_shma,            \
+                                            clear_shma
+
+class testSharedArray(unittest.TestCase):
+
+    SHMA_TEST_ARRAY_SHAPE = (50, 50)
+
+    def testSharedArray(self):
+        """
+        Tests de-/serialization of np.ndarrays of double using shared memory.
+        """
+
+        serialize_to_shm(testSharedArray.arr, testSharedArray.tmp_file)
+
+        arr_test1 = deserialize_from_shm(testSharedArray.tmp_file)
+        self.assertTrue(testSharedArray.SHMA_TEST_ARRAY_SHAPE == arr_test1.shape)
+        self.assertTrue(np.array_equal(testSharedArray.arr, arr_test1))
+        self.assertEqual(type(testSharedArray.arr), type(arr_test1),
+                         msg='Bad returned array type.')
+
+        arr_test2 = deserialize_from_shm(testSharedArray.tmp_file)
+        arr_test2 += 1
+        self.assertTrue(np.array_equal(arr_test1, arr_test2))
+
+        delete_shma(testSharedArray.tmp_file.name)
+        clear_shma(False)
+
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Generate an 50x50 numpy array of 0 (ints) and set the temporary file
+        for array serialization.
+        """
+        testSharedArray.arr = np.zeros(testSharedArray.SHMA_TEST_ARRAY_SHAPE)
+        testSharedArray.tmp_file = tempfile.NamedTemporaryFile()
+
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Free the space used for the shared array. """
+        testSharedArray.tmp_file.close()

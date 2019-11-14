ff1b68f6a77013a3ec8317fd241e4b5ad37de946
CristianRCV <cristianrcv@gmail.com>
2019-11-14 11:42:53 +0100

    Fix pom versions to 2.6.rc1911

--- tests/scripts/arguments.py
+++ tests/scripts/arguments.py
@@ -83,7 +83,7 @@ def get_args():
                                      add_help=True)
 
     # Add version
-    parser.add_argument('--version', action='version', version='2.5.rc1906')
+    parser.add_argument('--version', action='version', version='2.6.rc1911')
 
     # Add skip options
     parser.add_argument("-s", "--skip",

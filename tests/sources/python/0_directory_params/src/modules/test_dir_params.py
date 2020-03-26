058771a4b71567b54e7f95391455188b22d2a20d
nmammadl <nihad.mammadli@bsc.es>
2020-03-26 18:04:42 +0100

    DIRECTORY_T workflow tests.

--- tests/sources/python/0_directory_params/src/modules/test_dir_params.py
+++ tests/sources/python/0_directory_params/src/modules/test_dir_params.py
@@ -48,6 +48,108 @@ class TestDirParams(unittest.TestCase):
             with(open(_fp, 'a')) as nm:
                 nm.write(" #inout_task# ")
 
+    @task(dir_in=DIRECTORY_IN)
+    def dir_in_task_i(self, dir_in):
+        res = list()
+        for _ in os.listdir(dir_in):
+            _fp = dir_in + os.sep + _
+            with(open(_fp, 'r')) as nm:
+                res .append(nm.read())
+
+        return res
+
+    @task(dir_out=DIRECTORY_OUT)
+    def dir_out_task_i(self, dir_out, i):
+
+        if os.path.exists(dir_out):
+            shutil.rmtree(dir_out)
+
+        os.mkdir(dir_out)
+
+        f_out = "{}{}{}".format(dir_out, os.sep, i)
+
+        with(open(f_out, 'w')) as nm:
+            nm.write("written in dir out #{}".format(i))
+
+    @task(dir_inout=DIRECTORY_INOUT, returs=list)
+    def dir_inout_task_i(self, dir_inout, i):
+
+        res = list()
+        for _ in os.listdir(dir_inout):
+            with(open("{}{}{}".format(dir_inout, os.sep, _), 'r')) as nm:
+                res.append(nm.read())
+
+        f_inout = "{}{}{}".format(dir_inout, os.sep, i)
+
+        with(open(f_inout, 'w')) as nm:
+            nm.write("written by inout task #" + str(i))
+
+        return res
+
+    def test_workflow(self):
+        """
+        Test multiple tasks with directory in, out, and inout params.
+        """
+        cur_path = "{}{}".format(os.getcwd(), os.sep)
+        dir_t = "{}{}".format(cur_path, "some_dir_t")
+        os.mkdir(dir_t)
+
+        # len(phase[i] = i)
+        res_phase_0 = []
+        for i in range(0, 5, 1):
+            res_phase_0.append(self.dir_inout_task_i(dir_t, i))
+
+        # len(phase[i] = 5)
+        res_phase_1 = []
+        for i in range(0, 5, 1):
+            res_phase_1.append(self.dir_in_task_i(dir_t))
+
+        # len(phase[i] = i + 5)
+        res_phase_2 = []
+        for i in range(5, 10, 1):
+            res_phase_2.append(self.dir_inout_task_i(dir_t, i))
+
+        # len(phase[i] = 10)
+        res_phase_3 = []
+        for i in range(0, 5, 1):
+            res_phase_3.append(self.dir_in_task_i(dir_t))
+
+        # dir out should contain only the last file
+        for i in range(0, 15, 1):
+            self.dir_out_task_i(dir_t, i)
+
+        res_phase_0 = cwo(res_phase_0)
+        res_phase_1 = cwo(res_phase_1)
+        res_phase_2 = cwo(res_phase_2)
+        res_phase_3 = cwo(res_phase_3)
+        cwod(dir_t)
+
+        for i, res in enumerate(res_phase_0):
+            self.assertEqual(len(res), i,
+                             "error in task #{} of phase 0: {} != {}"
+                             .format(i, len(res), i))
+
+        for i, res in enumerate(res_phase_1):
+            self.assertEqual(len(res), 5,
+                             "error in task #{} of phase 1: {} != 5"
+                             .format(i, len(res)))
+
+        for i, res in enumerate(res_phase_2):
+            self.assertEqual(len(res), i + 5,
+                             "error in task #{} of phase 2: {} != {}"
+                             .format(i, len(res), i + 5))
+
+        for i, res in enumerate(res_phase_3):
+            self.assertEqual(len(res), 10,
+                             "error in task #{} of phase 3: {} != 10"
+                             .format(i, len(res)))
+
+        self.assertEqual(1, len(os.listdir(dir_t)),
+                         "directory has fewer or more files than 1: {}"
+                         .format(len(os.listdir(dir_t))))
+
+        shutil.rmtree(dir_t)
+
     def test_dir_inout(self):
         """
         Test DIRECTORY_INOUT

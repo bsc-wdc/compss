e77f615bb63990296bd81fbfe9db73b13fed2d96
Jorge Ejarque <jorge.ejarque@bsc.es>
2020-04-06 17:27:35 +0200

    exaqute api local parameter missing

--- compss/programming_model/bindings/python/src/exaqute/ExaquteParameter.py
+++ compss/programming_model/bindings/python/src/exaqute/ExaquteParameter.py
@@ -20,3 +20,15 @@ INOUT = None
 FILE_IN = None
 FILE_OUT = None
 FILE_INOUT = None
+
+COLLECTION_IN = None
+COLLECTION_INOUT = None
+COLLECTION_OUT = None
+
+# Aliases for parameter definition as dictionary
+Type = 'type'  # parameter type
+Direction = 'direction'  # parameter type
+StdIOStream = 'stream'  # parameter stream
+Prefix = 'prefix'  # parameter prefix
+Depth = 'depth'  # collection recursive depth
+

534ca0bcc32157101dc6ea756fcaf62110ea2cf7
CristianRCV <cristianrcv@gmail.com>
2020-04-22 17:56:54 +0200

    Fix race condition on termination of Abstract Connector

--- compss/runtime/resources/commons/src/main/java/es/bsc/compss/connectors/AbstractConnector.java
+++ compss/runtime/resources/commons/src/main/java/es/bsc/compss/connectors/AbstractConnector.java
@@ -17,6 +17,7 @@
 package es.bsc.compss.connectors;
 
 import es.bsc.compss.connectors.utils.CreationThread;
+import es.bsc.compss.connectors.utils.DeadlineThread;
 import es.bsc.compss.connectors.utils.DeletionThread;
 import es.bsc.compss.connectors.utils.Operations;
 import es.bsc.compss.log.Loggers;
@@ -26,9 +27,8 @@ import es.bsc.compss.types.resources.CloudMethodWorker;
 import es.bsc.compss.types.resources.ShutdownListener;
 import es.bsc.compss.types.resources.description.CloudMethodResourceDescription;
 
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
@@ -53,7 +53,7 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
     public static final String ADAPTOR_MAX_PORT_PROPERTY_NAME = "adaptor-max-port";
     public static final String ADAPTOR_MIN_PORT_PROPERTY_NAME = "adaptor-min-port";
 
-    // Constants
+    // Time constants
     protected static final long MIN_TO_S = 60;
     protected static final long S_TO_MS = 1_000;
     protected static final long ONE_HOUR = 3_600_000;
@@ -66,23 +66,22 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
 
     // Timer properties
     private static final long INITIAL_CREATION_TIME = ONE_MIN;
-    private static final long MINIM_DEADLINE_INTERVAL = 10_000;
-    private static final long DELETE_SAFETY_INTERVAL = HALF_MIN;
-    private static final long MAX_DEADLINE_INTERVAL = ONE_MIN;
 
+    private final CloudProvider provider;
     private float currentCostPerHour;
     private final float deletedMachinesCost;
     private long meanCreationTime; // MS
     private int createdVMs;
 
-    private final CloudProvider provider;
     private final ConcurrentHashMap<String, VM> ip2vm;
     private final ConcurrentHashMap<Object, Long> powerOnVMTimestamp;
-    private boolean terminate = false;
-    private boolean check = false;
-    private final DeadlineThread dead;
     private final TreeSet<VM> vmsToDelete;
     private final LinkedList<VM> vmsAlive;
+
+    private boolean terminate = false;
+    private boolean check = false;
+
+    private final DeadlineThread dead;
 
 
     /**
@@ -93,11 +92,6 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
      */
     public AbstractConnector(CloudProvider provider, Map<String, String> props) {
         this.provider = provider;
-        this.ip2vm = new ConcurrentHashMap<>();
-        this.powerOnVMTimestamp = new ConcurrentHashMap<>();
-        this.vmsToDelete = new TreeSet<>();
-        this.vmsAlive = new LinkedList<>();
-        // ipToConnection = Collections.synchronizedMap(new HashMap<String, Connection>());
 
         String estCreationTimeStr = props.get(PROP_ESTIMATED_CREATION_TIME);
         if (estCreationTimeStr != null) {
@@ -110,16 +104,68 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
                 this.meanCreationTime = INITIAL_CREATION_TIME;
             }
         }
-
         LOGGER.debug("[Abstract Connector] Initial mean creation time is " + this.meanCreationTime);
+
         this.createdVMs = 0;
         this.currentCostPerHour = 0.0f;
         this.deletedMachinesCost = 0.0f;
         this.terminate = false;
         this.check = false;
-        this.dead = new DeadlineThread();
+
+        this.ip2vm = new ConcurrentHashMap<>();
+        this.powerOnVMTimestamp = new ConcurrentHashMap<>();
+        this.vmsToDelete = new TreeSet<>();
+        this.vmsAlive = new LinkedList<>();
+
+        // Deadline thread
+        this.dead = new DeadlineThread(this);
         this.dead.start();
+
+        // Ender hook
         Runtime.getRuntime().addShutdownHook(new Ender(this));
+    }
+
+    /**
+     * Returns the associated Cloud Provider.
+     * 
+     * @return The associated Cloud Provider.
+     */
+    public CloudProvider getProvider() {
+        return this.provider;
+    }
+
+    /**
+     * Returns a shallow copy of the alive VMs.
+     * 
+     * @return A shallow copy of the alive VMs.
+     */
+    @SuppressWarnings("unchecked")
+    public List<VM> getCopyOfAliveVms() {
+        synchronized (this.vmsAlive) {
+            return (List<VM>) this.vmsAlive.clone();
+        }
+    }
+
+    /**
+     * Removes the given VM from the registered alive VMs. Does nothing if VM is not registered.
+     * 
+     * @param vm VM to remove from alive VMs.
+     */
+    public void removeFromAliveVms(VM vm) {
+        synchronized (this.vmsAlive) {
+            this.vmsAlive.remove(vm);
+        }
+    }
+
+    /**
+     * Removes the given VM from the registere toDelete VMs. Does nothing if VM is not registered.
+     * 
+     * @param vm VM to remove from toDelete VMs.
+     */
+    public void removeFromDeleteVms(VM vm) {
+        synchronized (this.vmsToDelete) {
+            this.vmsToDelete.remove(vm);
+        }
     }
 
     /*
@@ -151,7 +197,7 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
         return true;
     }
 
-    private synchronized VM tryToReuseVM(CloudMethodResourceDescription requested) {
+    private VM tryToReuseVM(CloudMethodResourceDescription requested) {
         String imageReq = requested.getImage().getImageName();
         VM reusedVM = null;
         synchronized (this.vmsToDelete) {
@@ -191,8 +237,18 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
 
     @Override
     public void terminateAll() {
+        // Mark the thread to terminate
         this.terminate = true;
+
+        // Ask the deadline to terminate and wait
         this.dead.terminate();
+        try {
+            Thread.sleep(DeadlineThread.getMaxDeadlineInterval());
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+
+        // Clean pending VMs to delete
         synchronized (this.vmsToDelete) {
             for (VM vm : this.vmsToDelete) {
                 try {
@@ -203,6 +259,8 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
                 this.vmsToDelete.clear();
             }
         }
+
+        // CLean ip2vm
         synchronized (this.ip2vm) {
             for (VM vm : this.ip2vm.values()) {
                 LOGGER.info("[Abstract Connector] Retrieving data from VM " + vm.getName());
@@ -227,6 +285,7 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
             this.ip2vm.clear();
         }
 
+        // Clean remaining alive VMs
         synchronized (this.vmsAlive) {
             this.vmsAlive.clear();
         }
@@ -293,7 +352,6 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
             this.meanCreationTime = totaltime / createdVMs;
             LOGGER.debug("[Abstract Connector] New mean creation time: " + this.meanCreationTime);
         }
-
     }
 
     @Override
@@ -333,7 +391,7 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
         return numSlots;
     }
 
-    private synchronized void addMachine(VM vmInfo) {
+    private void addMachine(VM vmInfo) {
         String ip = vmInfo.getName();
         synchronized (this.ip2vm) {
             this.ip2vm.put(ip, vmInfo);
@@ -343,8 +401,7 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
         }
     }
 
-    private synchronized void removeMachine(VM vmInfo) {
-
+    private void removeMachine(VM vmInfo) {
         synchronized (this.ip2vm) {
             this.ip2vm.remove(vmInfo.getName());
         }
@@ -414,112 +471,9 @@ public abstract class AbstractConnector implements Connector, Operations, Cost {
         }
         long dif = now - vmStart;
         long mod = dif % limit;
-        return mod < limit - DELETE_SAFETY_INTERVAL; // my deadline is less than 2 min away
+        return mod < limit - DeadlineThread.getDeleteSafetyInterval();
     }
 
-
-    /**
-     * Deadline thread for VM timeout.
-     */
-    private class DeadlineThread extends Thread {
-
-        private boolean keepGoing;
-
-
-        public DeadlineThread() {
-            this.keepGoing = true;
-        }
-
-        @Override
-        public void run() {
-            Thread.currentThread().setName("[Abstract Connector] Connector " + provider.getName() + " deadline");
-
-            long sleepTime = 1_000L;
-            while (this.keepGoing) {
-                try {
-                    LOGGER.debug("[Abstract Connector] Deadline thread sleeps " + sleepTime + " ms.");
-                    Thread.sleep(sleepTime);
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
-                }
-                synchronized (vmsAlive) {
-                    if (vmsAlive.isEmpty()) {
-                        // LOGGER.info("MONITOR STATUS DEAD no VMs alive");
-                        sleepTime = getSleepTime();
-                        LOGGER.debug("[Abstract Connector] No VMs alive deadline sleep set to " + sleepTime + " ms.");
-                        continue;
-                    } else {
-                        sleepTime = getSleepTime();
-                        LOGGER.debug("[Abstract Connector] VMs alive initial sleep set to " + sleepTime + " ms.");
-                        try {
-                            Iterator<VM> vms = vmsAlive.iterator();
-                            while (vms.hasNext()) {
-                                VM vmInfo = vms.next();
-                                long timeLeft = timeLeft(vmInfo.getStartTime());
-                                // LOGGER.info("MONITOR STATUS DEAD next VM " + vmInfo.ip + " @ " + vmInfo.startTime + "
-                                // --> " + timeLeft);
-                                if (timeLeft <= DELETE_SAFETY_INTERVAL) {
-                                    if (vmInfo.isToDelete()) {
-                                        LOGGER.info("[Abstract Connector] Deleting vm " + vmInfo.getName()
-                                            + " because is marked to delete"
-                                            + " and it is on the safety delete interval");
-                                        vmsAlive.pollFirst();
-                                        synchronized (vmsToDelete) {
-                                            vmsToDelete.remove(vmInfo);
-                                        }
-                                        DeletionThread dt;
-                                        dt = new DeletionThread((Operations) AbstractConnector.this, vmInfo);
-                                        dt.start();
-                                    }
-                                } else if (sleepTime > timeLeft - DELETE_SAFETY_INTERVAL) {
-                                    sleepTime = timeLeft - DELETE_SAFETY_INTERVAL;
-                                    LOGGER.debug("[Abstract Connector] Evaluating sleep time for " + vmInfo.getName()
-                                        + " because an interval near to finish " + sleepTime + " ms.");
-                                }
-                            }
-                        } catch (ConcurrentModificationException e) {
-                            LOGGER.debug("[Abstract Connector] Concurrent modification in deadline thread. Ignoring",
-                                e);
-                            sleepTime = 1_000L;
-                        }
-
-                    }
-                }
-            }
-        }
-
-        private long getSleepTime() {
-            long time = getTimeSlot();
-            if (time <= 0) {
-                return MINIM_DEADLINE_INTERVAL;
-            } else {
-                time = time - DELETE_SAFETY_INTERVAL;
-                if (time > MAX_DEADLINE_INTERVAL) {
-                    return MAX_DEADLINE_INTERVAL;
-                } else {
-                    return time;
-                }
-            }
-        }
-
-        public void terminate() {
-            this.keepGoing = false;
-            this.interrupt();
-        }
-
-        private long timeLeft(long time) {
-            long now = System.currentTimeMillis();
-            long limit = getTimeSlot();
-            if (limit <= 0) {
-                return 0;
-            }
-            long result = limit - ((now - time) % limit);
-            LOGGER.debug("Calculating sleep time at " + time + " now is " + now + ": remaining --> " + limit + " - "
-                + (now - time) + " % " + limit + " = " + result + " ms to deadline");
-            return result;
-        }
-
-    }
 
     /**
      * Ender Thread for JVM destruction.

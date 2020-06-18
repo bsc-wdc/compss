baf67ca26de7fc5a5806a738d699ae08b1615bbf
Leroy Jenkins <jenkins@bsc.es>
2020-06-18 17:27:34 +0200

    Update to version 2.7

--- compss/programming_model/bindings/python/setup.py
+++ compss/programming_model/bindings/python/setup.py
@@ -89,7 +89,7 @@ def find_packages(path='./src'):
 setup(
     # Metadata
     name='pycompss',
-    version='2.7.pr',
+    version='2.7',
     description='Python Binding for COMP Superscalar Runtime',
     long_description=open('README.txt').read(),
     author='Workflows and Distributed Computing Group (WDC) - Barcelona Supercomputing Center (BSC)',

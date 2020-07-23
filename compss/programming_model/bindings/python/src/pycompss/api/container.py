e531b89cd3bd0f6549d9199216a398b99e6d31d9
Pau <pau.recort@bsc.es>
2020-07-23 12:42:43 +0200

    Updated pycompss/api/container.py to new pycomss standard

--- compss/programming_model/bindings/python/src/pycompss/api/container.py
+++ compss/programming_model/bindings/python/src/pycompss/api/container.py
@@ -24,13 +24,15 @@ PyCOMPSs API - CONTAINER
     definition through the decorator.
 """
 
-import inspect
-import logging
-import os
 from functools import wraps
 import pycompss.util.context as context
 from pycompss.api.commons.error_msgs import not_in_pycompss
 from pycompss.util.arguments import check_arguments
+from pycompss.api.commons.decorator import PyCOMPSsDecorator
+from pycompss.api.commons.decorator import keep_arguments
+from pycompss.api.commons.decorator import CORE_ELEMENT_KEY
+from pycompss.runtime.task.core_element import CE
+
 
 if __debug__:
     logger = logging.getLogger(__name__)
@@ -46,11 +48,13 @@ SUPPORTED_ARGUMENTS = {'engine',
 DEPRECATED_ARGUMENTS = {'workingDir'}
 
 
-class Container(object):
+class Container(PyCOMPSsDecorator):
     """
     This decorator also preserves the argspec, but includes the __init__ and
     __call__ methods, useful on mpi task creation.
     """
+
+    __slots__ = []
 
     def __init__(self, *args, **kwargs):
         """
@@ -62,24 +66,17 @@ class Container(object):
         :param args: Arguments
         :param kwargs: Keyword arguments
         """
-        self.args = args
-        self.kwargs = kwargs
-        self.scope = context.in_pycompss()
-        self.registered = False
-        # This enables the decorator to get info from the caller
-        # (e.g. self.source_frame_info.filename or
-        #  self.source_frame_info.lineno)
-        # self.source_frame_info = inspect.getframeinfo(inspect.stack()[1][0])
+        decorator_name = '@' + self.__class__.__name__.lower()
+        super(self.__class__, self).__init__(decorator_name, *args, **kwargs)
         if self.scope:
             if __debug__:
                 logger.debug("Init @container decorator...")
-
             # Check the arguments
             check_arguments(MANDATORY_ARGUMENTS,
                             DEPRECATED_ARGUMENTS,
                             SUPPORTED_ARGUMENTS | DEPRECATED_ARGUMENTS,
                             list(kwargs.keys()),
-                            "@container")
+                            decorator_name)
 
     def __call__(self, func):
         """
@@ -91,102 +88,77 @@ class Container(object):
         @wraps(func)
         def container_f(*args, **kwargs):
             if not self.scope:
-                # from pycompss.api.dummy.container import container as dummy_container
-                # d_b = dummy_container(self.args, self.kwargs)
-                # return d_b.__call__(func)
                 raise Exception(not_in_pycompss("container"))
 
-            if context.in_master():
-                # master code
-                mod = inspect.getmodule(func)
-                self.module = mod.__name__  # not func.__module__
-
-                if (self.module == '__main__' or
-                        self.module == 'pycompss.runtime.launch'):
-                    # The module where the function is defined was run as
-                    # __main__, so we need to find out the real module name.
-
-                    # path = mod.__file__
-                    # dirs = mod.__file__.split(os.sep)
-                    # file_name = os.path.splitext(
-                    #                 os.path.basename(mod.__file__))[0]
-
-                    # Get the real module name from our launch.py variable
-                    path = getattr(mod, "APP_PATH")
-
-                    dirs = path.split(os.path.sep)
-                    file_name = os.path.splitext(os.path.basename(path))[0]
-                    mod_name = file_name
-
-                    i = len(dirs) - 1
-                    while i > 0:
-                        new_l = len(path) - (len(dirs[i]) + 1)
-                        path = path[0:new_l]
-                        if "__init__.py" in os.listdir(path):
-                            # directory is a package
-                            i -= 1
-                            mod_name = dirs[i] + '.' + mod_name
-                        else:
-                            break
-                    self.module = mod_name
-
-                # Include the registering info related to @binary
-                if not self.registered:
-                    # Set as registered
-                    self.registered = True
-                    # Retrieve the base core_element established at @task
-                    # decorator
-                    from pycompss.api.task import current_core_element as cce
-                    # Update the core element information with the binary
-                    # argument information
-                    cce.set_impl_type("CONTAINER")
-                    _binary = self.kwargs['binary']
-                    engine = self.kwargs['engine']
-                    image = self.kwargs['image']
-                    if 'working_dir' in self.kwargs:
-                        working_dir = self.kwargs['working_dir']
-                    elif 'workingDir' in self.kwargs:
-                        working_dir = self.kwargs['workingDir']
-                    else:
-                        working_dir = '[unassigned]'  # Empty or '[unassigned]'
-                    fail_by_ev_str = 'false'
-                    impl_signature = 'CONTAINER.' + _binary
-                    cce.set_impl_signature(impl_signature)
-                    impl_args = [_binary, working_dir, fail_by_ev_str, engine, image]
-                    cce.set_impl_type_args(impl_args)
-            else:
-                # worker code
-                pass
-            # This is executed only when called.
             if __debug__:
                 logger.debug("Executing container_f wrapper.")
 
-            if len(args) > 0:
-                # The 'self' for a method function is passed as args[0]
-                slf = args[0]
+            if context.in_master():
+                # master code
+                if not self.core_element_configured:
+                    self.__configure_core_element__(kwargs)
+            else:
+                # worker code
+                pass
 
-                # Replace and store the attributes
-                saved = {}
-                for k, v in self.kwargs.items():
-                    if hasattr(slf, k):
-                        saved[k] = getattr(slf, k)
-                        setattr(slf, k, v)
-
-            # Call the method
-            import pycompss.api.task as t
-            t.prepend_strings = False
-            ret = func(*args, **kwargs)
-            t.prepend_strings = True
-
-            if len(args) > 0:
-                # Put things back
-                for k, v in saved.items():
-                    setattr(slf, k, v)
+            with keep_arguments(args, kwargs, prepend_strings=False):
+                # Call the method
+                ret = func(*args, **kwargs)
 
             return ret
 
         container_f.__doc__ = func.__doc__
         return container_f
+
+    def __configure_core_element__(self, kwargs):
+        # type: (dict) -> None
+        """ Include the registering info related to @binary.
+
+        IMPORTANT! Updates self.kwargs[CORE_ELEMENT_KEY].
+
+        :param kwargs: Keyword arguments received from call.
+        :return: None
+        """
+        if __debug__:
+            logger.debug("Configuring @container core element.")
+
+        # Resolve @container (mandatory) specific parameters
+        engine = self.kwargs['engine']
+        image = self.kwargs['image']
+
+
+        # Resolve the working directory
+        self.__resolve_working_dir__()
+        # Resolve the fail by exit value
+        self.__resolve_fail_by_exit_value__()
+
+        impl_type = 'CONTAINER'
+        _binary = str(self.kwargs['binary'])
+        impl_signature = '.'.join((impl_type, _binary))
+        impl_args = [_binary,
+                     self.kwargs['working_dir'],
+                     self.kwargs['fail_by_exit_value'],
+                     engine,
+                     image]
+
+        if CORE_ELEMENT_KEY in kwargs:
+            # Core element has already been created in a higher level decorator
+            # (e.g. @constraint)
+            kwargs[CORE_ELEMENT_KEY].set_impl_type(impl_type)
+            kwargs[CORE_ELEMENT_KEY].set_impl_signature(impl_signature)
+            kwargs[CORE_ELEMENT_KEY].set_impl_type_args(impl_args)
+        else:
+            # @container is in the top of the decorators stack.
+            # Instantiate a new core element object, update it and include
+            # it into kwarg
+            core_element = CE()
+            core_element.set_impl_type(impl_type)
+            core_element.set_impl_signature(impl_signature)
+            core_element.set_impl_type_args(impl_args)
+            kwargs[CORE_ELEMENT_KEY] = core_element
+
+        # Set as configured
+        self.core_element_configured = True
 
 
 # ############################################################################## #

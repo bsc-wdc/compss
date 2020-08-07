11e491575b8496c457fe2e5d660ba96337d5a72f
fconejer <javier.conejero@bsc.es>
2020-08-07 09:38:51 +0200

    Added unittest for @decaf decorator.

--- compss/programming_model/bindings/python/nose_tests.py
+++ compss/programming_model/bindings/python/nose_tests.py
@@ -49,6 +49,7 @@ class ExtensionPlugin(Plugin):
         'pycompss/tests/api/test_binary.py',
         'pycompss/tests/api/test_compss.py',
         'pycompss/tests/api/test_constraint.py',
+        'pycompss/tests/api/test_decaf.py',
         'pycompss/tests/api/test_io.py',
         'pycompss/tests/api/test_ompss.py',
         'pycompss/tests/api/test_opencl.py',

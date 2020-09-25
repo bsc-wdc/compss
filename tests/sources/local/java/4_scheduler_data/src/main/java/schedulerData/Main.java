833fca9f82dd2b9251a1527f50563c39b04d65cd
Jorge Ejarque <jorge.ejarque@bsc.es>
2020-09-25 19:07:35 +0200

    change timers schedulers

--- tests/sources/local/java/4_scheduler_data/src/main/java/schedulerData/Main.java
+++ tests/sources/local/java/4_scheduler_data/src/main/java/schedulerData/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     private static final String FILE_NAME = "counterFile_";
 
-    private static final int SLEEP_WAIT_FOR_RUNTIME = 10_000; // ms
+    private static final int SLEEP_WAIT_FOR_RUNTIME = 15_000; // ms
 
 
     private static void generateTree(int taskWidth, int taskChilds, String fileInout, String fileIn) {
@@ -29,7 +29,7 @@ public class Main {
 
         MainImpl.increment(fileInout, fileIn);
         try {
-            Thread.sleep(150);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }

a857602e4bf38fe8301076db38c76fb9fc16aa6d
fconejer <javier.conejero@bsc.es>
2020-10-27 15:23:19 +0100

    Added unittest for jupyter notebook.

--- compss/programming_model/bindings/python/nose_tests.py
+++ compss/programming_model/bindings/python/nose_tests.py
@@ -96,6 +96,7 @@ INTEGRATION_WHITE_LIST = [
     'pycompss/tests/integration/test_launch_synthetic_application.py',
     'pycompss/tests/integration/test_launch_functions.py',
     'pycompss/tests/integration/test_launch_0_basic1.py',
+    'pycompss/tests/main/test_notebook.py',
 ]
 
 

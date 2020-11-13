87444276831ddc1c026a6dc2a86643cfea58f272
fconejer <javier.conejero@bsc.es>
2020-11-13 19:41:04 +0100

    Improved dds examples unittest.

--- compss/programming_model/bindings/python/src/pycompss/tests/dds/dds_examples.py
+++ compss/programming_model/bindings/python/src/pycompss/tests/dds/dds_examples.py
@@ -15,8 +15,22 @@
 #  limitations under the License.
 #
 
+import os
+import sys
+import shutil
+import tempfile
+
 from pycompss.dds.examples import pi_estimation
 from pycompss.dds.examples import transitive_closure
+from pycompss.dds.examples import word_count
+from pycompss.dds.examples import terasort
+from pycompss.dds.examples import inverted_indexing
+
+
+# The DDS examples unittest only checks functionality (not the validity of the results).
+# TODO: check that the results are OK.
+
+EXAMPLES_NAME = "examples.py"
 
 
 def pi_estimation_example():
@@ -27,6 +41,41 @@ def transitive_closure_example():
     transitive_closure(2)
 
 
+def wordcount_example():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    wordcount_dataset_path = os.path.join(current_path, "dataset", "wordcount")
+    argv_backup = sys.argv
+    sys.argv = [EXAMPLES_NAME, wordcount_dataset_path]
+    word_count()
+    sys.argv = argv_backup
+
+
+def terasort_example(result_path):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    terasort_dataset_path = os.path.join(current_path, "dataset", "terasort")
+    argv_backup = sys.argv
+    sys.argv = [EXAMPLES_NAME, terasort_dataset_path, result_path]
+    terasort()
+    sys.argv = argv_backup
+
+
+def inverted_indexing_example():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    inverted_indexing_dataset_path = os.path.join(current_path,
+                                                  "dataset", "wordcount")
+    argv_backup = sys.argv
+    sys.argv = [EXAMPLES_NAME, inverted_indexing_dataset_path]
+    inverted_indexing()
+    sys.argv = argv_backup
+
+
 def main():
+    result_path = tempfile.mkdtemp()
+    # Examples to run
     pi_estimation_example()
     transitive_closure_example()
+    wordcount_example()
+    terasort_example(result_path)
+    inverted_indexing_example()
+    # Clean the results directory
+    shutil.rmtree(result_path)

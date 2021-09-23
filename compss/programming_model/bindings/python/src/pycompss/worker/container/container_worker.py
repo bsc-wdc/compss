c6dac92c9367595e7d8326beda5437908d84a181
fconejer <francisco.conejero@bsc.es>
2021-09-23 16:31:59 +0200

    Fixed logger at @container with python code

--- compss/programming_model/bindings/python/src/pycompss/worker/container/container_worker.py
+++ compss/programming_model/bindings/python/src/pycompss/worker/container/container_worker.py
@@ -26,6 +26,7 @@ inside containers.
 import pycompss.worker.container.pythonpath_fixer  # noqa
 
 # Regular imports
+import os
 import sys
 import logging
 
@@ -33,10 +34,11 @@ import logging
 import pycompss.util.context as context
 from pycompss.worker.commons.worker import execute_task
 from pycompss.worker.commons.executor import build_return_params_message
+from pycompss.util.logger.helpers import init_logging_worker
 
 # Define static logger
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)  # NOSONAR
-LOGGER = logging.getLogger()
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)  # NOSONAR
+# logger = logging.getLogger()
 
 
 #
@@ -49,43 +51,62 @@ def main():
     :return: Exit value
     """
 
-    # Log initialisation
-    if __debug__:
-        LOGGER.debug("Initialising Python worker inside the container...")
-
     # Parse arguments
-    if __debug__:
-        LOGGER.debug("Parsing Python function and arguments...")
-
     # TODO: Enhance the received parameters from ContainerInvoker.java
     func_file_path = str(sys.argv[1])
     func_name = str(sys.argv[2])
     num_slaves = 0
     timeout = 0
     cus = 1
-    has_target = str(sys.argv[3]).lower() == "true"
-    return_type = str(sys.argv[4])
-    return_length = int(sys.argv[5])
-    num_params = int(sys.argv[6])
-    func_params = sys.argv[7:]
+    log_level = sys.argv[3]
+    tracing = sys.argv[4] == 'true'
+    has_target = str(sys.argv[5]).lower() == "true"
+    return_type = str(sys.argv[6])
+    return_length = int(sys.argv[7])
+    num_params = int(sys.argv[8])
+    func_params = sys.argv[9:]
+
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    print("XXXXX: sys.argv: " + sys.argv)
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+    # Log initialisation
+    # Load log level configuration file
+    worker_path = os.path.dirname(os.path.realpath(__file__))
+    if log_level == "true" or log_level == "debug":
+        # Debug
+        log_json = "".join((worker_path,
+                            "/../../../log/logging_container_worker_debug.json"))
+    elif log_level == "info" or log_level == "off":
+        # Info or no debug
+        log_json = "".join((worker_path,
+                            "/../../../log/logging_container_worker_off.json"))
+    else:
+        # Default
+        log_json = "".join((worker_path,
+                            "/../../../log/logging_container_worker.json"))
+    init_logging_worker(log_json, tracing)
+    if __debug__:
+        logger = logging.getLogger('pycompss.worker.container.container_worker')  # noqa: E501
+        logger.debug("Initialising Python worker inside the container...")
 
     execute_task_params = [func_file_path, func_name, num_slaves,
                            timeout, cus, has_target, return_type,
                            return_length, num_params] + func_params
 
     if __debug__:
-        LOGGER.debug("- File: " + str(func_file_path))
-        LOGGER.debug("- Function: " + str(func_name))
-        LOGGER.debug("- HasTarget: " + str(has_target))
-        LOGGER.debug("- ReturnType: " + str(return_type))
-        LOGGER.debug("- Num Returns: " + str(return_length))
-        LOGGER.debug("- Num Parameters: " + str(num_params))
-        LOGGER.debug("- Parameters: " + str(func_params))
-        LOGGER.debug("DONE Parsing Python function and arguments")
+        logger.debug("- File: " + str(func_file_path))
+        logger.debug("- Function: " + str(func_name))
+        logger.debug("- HasTarget: " + str(has_target))
+        logger.debug("- ReturnType: " + str(return_type))
+        logger.debug("- Num Returns: " + str(return_length))
+        logger.debug("- Num Parameters: " + str(num_params))
+        logger.debug("- Parameters: " + str(func_params))
+        logger.debug("DONE Parsing Python function and arguments")
 
     # Process task
     if __debug__:
-        LOGGER.debug("Processing task...")
+        logger.debug("Processing task...")
 
     process_name = "ContainerInvoker"
     storage_conf = "null"
@@ -98,7 +119,7 @@ def main():
                           storage_conf,
                           execute_task_params,
                           tracing,
-                          LOGGER,
+                          logger,
                           None,
                           log_files,           # noqa
                           python_mpi,
@@ -108,40 +129,40 @@ def main():
     exit_value, new_types, new_values, _, except_msg = result
 
     if __debug__:
-        LOGGER.debug("DONE Processing task")
+        logger.debug("DONE Processing task")
 
     # Process results
     if __debug__:
-        LOGGER.debug("Processing results...")
-        LOGGER.debug("Task exit value = " + str(exit_value))
+        logger.debug("Processing results...")
+        logger.debug("Task exit value = " + str(exit_value))
 
     if exit_value == 0:
         # Task has finished without exceptions
         if __debug__:
-            LOGGER.debug("Building return parameters...")
-            LOGGER.debug("New Types: " + str(new_types))
-            LOGGER.debug("New Values: " + str(new_values))
+            logger.debug("Building return parameters...")
+            logger.debug("New Types: " + str(new_types))
+            logger.debug("New Values: " + str(new_values))
         build_return_params_message(new_types, new_values)
         if __debug__:
-            LOGGER.debug("DONE Building return parameters")
+            logger.debug("DONE Building return parameters")
     elif exit_value == 2:
         # Task has finished with a COMPSs Exception
         if __debug__:
             except_msg = except_msg.replace(" ", "_")
-            LOGGER.debug("Registered COMPSs Exception: %s" %
+            logger.debug("Registered COMPSs Exception: %s" %
                          str(except_msg))
     else:
         # An exception has been raised in task
         if __debug__:
             except_msg = except_msg.replace(" ", "_")
-            LOGGER.debug("Registered Exception in task execution %s" %
+            logger.debug("Registered Exception in task execution %s" %
                          str(except_msg))
 
     # Return
     if exit_value != 0:
-        LOGGER.debug("ERROR: Task execution finished with non-zero exit value (%s != 0)" % str(exit_value))  # noqa: E501
+        logger.debug("ERROR: Task execution finished with non-zero exit value (%s != 0)" % str(exit_value))  # noqa: E501
     else:
-        LOGGER.debug("Task execution finished SUCCESSFULLY!")
+        logger.debug("Task execution finished SUCCESSFULLY!")
     return exit_value
 
 

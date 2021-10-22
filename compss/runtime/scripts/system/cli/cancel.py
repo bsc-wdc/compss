e0f66d856632b7636803d7075db848ce71266de8
Cristian Tatu <ctatu@bsc.es>
2021-10-22 11:31:46 +0200

    scripts for job management

---
+++ compss/runtime/scripts/system/cli/cancel.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+#
+#  Copyright 2002-2021 Barcelona Supercomputing Center (www.bsc.es)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+"""
+This script is aimed at cancelling a running PyCOMPSs job with Jupyter notebook.
+"""
+
+import os
+import sys
+from commons import VERBOSE
+from commons import SUCCESS_KEYWORD
+from commons import ERROR_KEYWORD
+from commons import setup_supercomputer_configuration
+from commons import update_command
+from commons import command_runner
+from commons import is_notebook_job
+from commons import not_a_notebook
+
+
+def cancel():
+    """
+    Main cancel function.
+    Gets the cancel command from the environment variable (defined in supercomputer cfgs)
+    and includes the job identifiers to be cancelled.
+    :return: None
+    """
+    if VERBOSE:
+        print("Cancelling PyCOMPSs interactive job...")
+
+    # Get command line arguments
+    job_ids = sys.argv[1:]
+
+    # Load the Supercomputer configuration to get the appropriate cancel command
+    setup_supercomputer_configuration()
+
+    success = True
+    # There might be more than one to cancel
+    for job_id in job_ids:
+        if VERBOSE:
+            print(" - Job: " + str(job_id))
+
+        # Get the command to cancel the job
+        raw_cancel_command = os.environ['QUEUE_JOB_CANCEL_CMD']
+        cancel_command = update_command(raw_cancel_command, job_id)
+
+        # Cancel the job
+        return_code, _, _ = command_runner(cancel_command)
+
+        if return_code != 0:
+            success = False
+
+    if VERBOSE:
+        print("Finished cancellation.")
+
+    # Print to notify the cancel result
+    if success:
+        print(SUCCESS_KEYWORD)
+    else:
+        print(ERROR_KEYWORD)
+        exit(1)
+
+
+if __name__ == '__main__':
+    cancel()
---
+++ compss/runtime/scripts/system/cli/find.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#
+#  Copyright 2002-2021 Barcelona Supercomputing Center (www.bsc.es)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+"""
+This script is aimed at getting the list of running PyCOMPSs jobs
+with Jupyter notebook.
+"""
+
+import os
+from commons import VERBOSE
+from commons import SUCCESS_KEYWORD
+from commons import ERROR_KEYWORD
+from commons import command_runner
+from commons import setup_supercomputer_configuration
+from commons import get_job_name
+from commons import get_job_status
+from commons import verify_job_name
+
+
+def find():
+    """
+    Main find function.
+    Gets the jobs list command from the environment variable (defined in supercomputer cfgs)
+    and runs it. It also gets each job identifier name and status to provide more information
+    to the users.
+    :return: None
+    """
+    if VERBOSE:
+        print("Looking for PyCOMPSs interactive jobs...")
+
+    # Load the Supercomputer configuration to get the appropriate status command
+    setup_supercomputer_configuration()
+
+    # Get the list of running job ids
+    job_list_command = os.environ['QUEUE_JOB_LIST_CMD']
+    return_code, raw_job_ids, _ = command_runner(job_list_command.split())
+    job_ids = raw_job_ids.splitlines()
+
+    # Filter the jobs (keep only the notebook related ones)
+    jobs = []
+    for job_id in job_ids:
+        name = get_job_name(job_id).strip()
+        status, _ = get_job_status(job_id)
+        jobs.append((job_id, status, name))
+
+    if VERBOSE:
+        print("Finished looking for jobs.")
+
+    # Print to notify the find result
+    if return_code != 0:
+        print(ERROR_KEYWORD)
+        exit(1)
+    else:
+        # Print to provide the list of jobs to the client
+        print(SUCCESS_KEYWORD)
+        for job_id, status, name in jobs:
+            print(str(job_id) + '\t- ' + str(status) + '\t- ' + str(name))
+
+
+if __name__ == '__main__':
+    find()

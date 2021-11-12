e595df7f989fe814949614ccf61e37eb9d8aaed3
Leroy Jenkins <compsuperscalar@gmail.com>
2021-11-12 08:45:55 +0100

    Update to version 2.10

--- compss/programming_model/bindings/python/setup.py
+++ compss/programming_model/bindings/python/setup.py
@@ -102,7 +102,7 @@ else :
 setup(
     # Metadata
     name='pycompss',
-    version='2.9.rc2111',
+    version='2.10',
     description='Python Binding for COMP Superscalar Runtime',
     long_description=open('README.txt').read(),
     author='Workflows and Distributed Computing Group (WDC) - Barcelona Supercomputing Center (BSC)',

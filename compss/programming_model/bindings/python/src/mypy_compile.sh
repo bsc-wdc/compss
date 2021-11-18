87fbbd439c0b2d3a54c1895c104c1efb3a7542ac
fconejer <francisco.conejero@bsc.es>
2021-11-18 13:16:01 +0100

    Added mypy check on buildlocal.

--- compss/programming_model/bindings/python/src/mypy_compile.sh
+++ compss/programming_model/bindings/python/src/mypy_compile.sh
@@ -4,7 +4,7 @@
 ## Compilation with mypyc script ##
 ###################################
 
-mypyc --ignore-missing-imports --exclude 'pycompss\/((tests)\/|(dds)\/|(functions\/data_tasks.py)|(functions\/elapsed_time.py)|(streams\/)|(runtime\/launch.py)|(util\/interactive\/events.py)|(interactive.py)|(__main__.py))$' ./pycompss/
+/opt/python/bin/mypyc --ignore-missing-imports --exclude 'pycompss\/((tests)\/|(dds)\/|(functions\/data_tasks.py)|(functions\/elapsed_time.py)|(streams\/)|(runtime\/launch.py)|(util\/interactive\/events.py)|(interactive.py)|(__main__.py))$' ./pycompss/
 
 # Copy the main pycompss compiled module one folder up to be found using
 # python3 -m pycompss. Alternative would be to force use only python3 or
--- compss/programming_model/bindings/python/src/mypy_deps.sh
+++ compss/programming_model/bindings/python/src/mypy_deps.sh
@@ -1,2 +1,4 @@
+python3 -m pip install mypy
+python3 -m pip install mypy-extensions
 python3 -m pip install types-tabulate
 python3 -m pip install types-decorator

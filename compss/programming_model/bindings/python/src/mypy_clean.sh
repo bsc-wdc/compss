9db8eed9e41def4a8925d08309bd0962c6e72757
fconejer <francisco.conejero@bsc.es>
2021-11-25 08:47:53 +0100

    Improved mypy clean and check scripts.

--- compss/programming_model/bindings/python/src/mypy_clean.sh
+++ compss/programming_model/bindings/python/src/mypy_clean.sh
@@ -6,8 +6,8 @@
 
 rm -rf build
 # rm -rf .mypy_cache
-find . -type f -name "*\.so" -exec rm {} \;
-find . -type d -name "*.mypy_cache" -exec rm -rf {} \;
+find .. -type f -name "*\.so" -exec rm {} \;
+find .. -type d -name "*.mypy_cache" -exec rm -rf {} \;
 rm -rf index.* mypy-html.css html/
 
 # TODO: CHANGE THE SOURCE NAMES FROM *.py_source TO *.py

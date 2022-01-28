66c0d14d8384ad60e4b98c51fb90842bbee13e19
fconejer <francisco.conejero@bsc.es>
2022-01-28 17:06:41 +0100

    Generalized mypy check command.

--- compss/programming_model/bindings/python/src/mypy_check.sh
+++ compss/programming_model/bindings/python/src/mypy_check.sh
@@ -4,5 +4,5 @@
 ## Check typing with mypy ##
 ############################
 
-/opt/python/bin/mypy --pretty --ignore-missing-imports --exclude 'pycompss\/((tests)\/|(dds)\/|(functions\/data_tasks.py)|(functions\/elapsed_time.py)|(streams\/)|(runtime\/launch.py)|(util\/translators\/code_replacer\/tests\/original.py)|(util\/interactive\/events.py)|(interactive.py)|(__main__.py))$' ./pycompss/
+mypy --pretty --ignore-missing-imports --exclude 'pycompss\/((tests)\/|(dds)\/|(functions\/data_tasks.py)|(functions\/elapsed_time.py)|(streams\/)|(runtime\/launch.py)|(util\/translators\/code_replacer\/tests\/original.py)|(util\/interactive\/events.py)|(interactive.py)|(__main__.py))$' ./pycompss/
 

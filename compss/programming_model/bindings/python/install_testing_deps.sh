d7122fb562293d2b1f3ad37abeee66db9e479315
fconejer <francisco.conejero@bsc.es>
2022-02-05 08:36:07 +0100

    Added unittest testing dependencies for PyCOMPSs binding.

--- compss/programming_model/bindings/python/install_testing_deps.sh
+++ compss/programming_model/bindings/python/install_testing_deps.sh
@@ -10,6 +10,8 @@ python3 -m pip install coverage --user
 python3 -m pip install pycodestyle --user
 python3 -m pip install mpi4py --user
 python3 -m pip install numpy dill guppy3 memory_profiler matplotlib decorator --user
+python3 -m pip install jupyter pytest nbval pytest-cov pytest-notebook ipyparallel jupyter_nbextensions_configurator jupyterlab --user
+python3 -m ipykernel install --user
 
 # Jupyter specific
 python2 -m pip install nbval --user

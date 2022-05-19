277cc1fb31915b4601e6611dabeb6c0cc55753bb
fconejer <francisco.conejero@bsc.es>
2022-05-19 15:57:35 +0200

    Removed missing unittests temporary results.

--- compss/programming_model/bindings/python/scripts/test/unittests_commons.sh
+++ compss/programming_model/bindings/python/scripts/test/unittests_commons.sh
@@ -7,14 +7,13 @@
 clean_unittests() {
   echo "[INFO] Cleaning unittests"
   # Remove temporary files
+  # Reestablish dds dataset
+  rm pycompss/tests/unittests/dds/dataset/pickles/00001
+  rm pycompss/tests/unittests/dds/dataset/tmp/00001
 }
 
 clean_integration_unittests() {
   echo "[INFO] Cleaning integration unittests"
-  # Reestablish dds dataset
-  rm pycompss/tests/unittests/dds/dataset/pickles/00001
-  rm pycompss/tests/unittests/dds/dataset/tmp/00001
-  #git co pycompss/tests/unittests/dds/dataset/pickles/00000
   # Remove temporary files
   [ -e outfile_1 ] && rm outfile_1
   [ -e outfile_2 ] && rm outfile_2

7d6b0ef000d9e70dabd81c10841f812c3f67bf56
fconejer <francisco.conejero@bsc.es>
2022-05-24 17:14:03 +0200

    Added more tests for file exists and delete.

--- tests/sources/local/python/0_api_delete/src/modules/testDeleteFile.py
+++ tests/sources/local/python/0_api_delete/src/modules/testDeleteFile.py
@@ -50,3 +50,36 @@ class testDeleteFile(unittest.TestCase):
         print(('Final counter value is %s' % final_value))
         self.assertEqual(final_value, '3')
         compss_delete_file(counter_name)
+
+    def testDeleteMultipleFiles(self):
+        # Check and get parameters
+        initial_value = '1'
+        counter_name = 'multi_counter_INOUT'  # check that this file does not exist after the execution
+        counter_name_IN = 'multi_counter_IN'  # check that this file does not exist after the execution
+        counter_name_OUT = 'multi_counter_OUT'  # check that this file does not exist after the execution
+
+        to_remove = []
+        for i in range(3):
+            # Write value
+            if i <= 1:
+                fos = open(counter_name, 'w')
+                fos.write(initial_value)
+                fos.close()
+            fos2 = open(counter_name_IN, 'w')
+            fos2.write(initial_value)
+            fos2.close()
+            print(('Initial counter value is %s' % initial_value))
+            # Execute increment
+            increment(counter_name)
+            increment2(counter_name_IN, counter_name_OUT)
+            # Read new value
+            print('After sending task')
+            partial_to_remove = []
+            if i == 0:
+                partial_to_remove.append(counter_name)
+            partial_to_remove.append(counter_name_IN)
+            partial_to_remove.append(counter_name_OUT)
+            to_remove.append(tuple(partial_to_remove))
+        to_remove.append(counter_name)
+        removed = compss_delete_file(to_remove)
+        self.assertEqual(removed, [(False, False, False), (False, False), (False, False), False])
--- tests/sources/local/python/0_api_file/src/file_calls_test.py
+++ tests/sources/local/python/0_api_file/src/file_calls_test.py
@@ -22,24 +22,24 @@ def gen_file(structure):
     f.close()
     time.sleep(5.0)
 
-def main(): 
+def main():
     hello_file="hello_world.txt"
     f= open(hello_file, 'w')
     f.write(HELLO)
     f.close()
-    
+
     #Check file_exists after creation
     if (not compss_file_exists(hello_file)):
         raise Exception("Wait on not used object not working")
     print("compss_file_exists in existing file is CORRECT.")
-    
+
     #Check wait on not used object
     a = 20
     a = compss_wait_on(a)
     if ( a != 20):
         raise Exception(" Wait on not used object not working (" +str(a)+")")
-    print("Wait on not used object is CORRECT.")    
-    
+    print("Wait on not used object is CORRECT.")
+
     #Check wait on not used file
     compss_wait_on_file(hello_file)
     f = compss_open(hello_file)
@@ -47,8 +47,8 @@ def main():
     f.close()
     if (content != HELLO):
         raise Exception(" Wait on not used file is not working (" + content + ")")
-    print("Wait on not used file is CORRECT.") 
-    
+    print("Wait on not used file is CORRECT.")
+
     # Check delete_file in not used file
     compss_delete_file(hello_file)
 
@@ -57,21 +57,21 @@ def main():
     #Check file exist before existing file
     if (compss_file_exists(struct_file)):
         raise Exception("Struct file shouldn't exists and it is existing")
-    print("compss_file_exists in no existing file is CORRECT.") 
+    print("compss_file_exists in no existing file is CORRECT.")
     gen_file(struct_file)
 
     #Check file exist after executint the task
     if (not compss_file_exists(struct_file)):
         raise Exception("Struct file should exists and it is not existing")
     print("compss_file_exists in used file is existing file is CORRECT.")
-    
+
     #Check file exist after delete but task not finished
     compss_delete_file(struct_file)
     if (compss_file_exists(struct_file)):
         raise Exception("Struct file shouldn't exists after_delete and it is existing")
 
     print("compss_exists_file after delete is CORRECT")
-    
+
     gen_file(struct_file_2)
     compss_wait_on_file(struct_file_2)
     f= open(struct_file_2)
@@ -88,6 +88,14 @@ def main():
     if (compss_file_exists(struct_file_2)):
         raise Exception("Struct file 2 shouldn't exists and it is existing")
     print("compss_exists_file after delete and wait_on is CORRECT")
+
+    base_all_files = [hello_file, (struct_file, struct_file_2)]
+    all_files = compss_wait_on_file(base_all_files)
+    if all_files != base_all_files:
+        raise Exception("compss_wait_on_file for multiple files failed.")
+    all_files_exist = compss_file_exists(all_files)
+    if all_files_exist != [False, (False, False)]:
+        raise Exception("compss_file_exist for multiple files failed.")
 
 if __name__ == '__main__':
     main()

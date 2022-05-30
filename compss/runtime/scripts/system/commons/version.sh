a208dc41795b4ebb62e71f5ca4020b594269be82
fconejer <francisco.conejero@bsc.es>
2022-05-30 10:28:50 +0200

    Updated flower, description and recipe.

--- compss/runtime/scripts/system/commons/version.sh
+++ compss/runtime/scripts/system/commons/version.sh
@@ -8,7 +8,7 @@ fi
 # Displays version
 ###############################################
 show_version() {
-  echo "COMPSs version 2.10 Kumquat"
+  echo "COMPSs version 3.0 Lavender"
   echo " "
 }
 
@@ -17,13 +17,13 @@ show_version() {
 ###############################################
 show_full_version() {
   cat << EOF
-                             
-        .-~~-.--.             _____       _   ______
-       :         )           |____ \\     / | /  __  \\
- .~ ~ -.\\       /.- ~~ .      ___) |    /_ | | |  | |
- >       \`.   .'       <     / ___/      | | | |  | |
-(         .- -.         )   | |____   _  | | | |__| | 
- \`- -.-~  \`- -'  ~-.- -'    |______| |_| |_| \\______/
+
+        .-~~-.--.           ______        ______
+       :         )         |____  \\      /  __  \\
+ .~ ~ -.\\       /.- ~~ .      __) |      | |  | |
+ >       \`.   .'       <     |__  |      | |  | |
+(         .- -.         )   ____) |   _  | |__| |
+ \`- -.-~  \`- -'  ~-.- -'   |______/  |_| \\______/
    (        :        )           _ _ .-:
     ~--.    :    .--~        .-~  .-~  }
         ~-.-^-.-~ \\       .~  .-~   .~
@@ -43,11 +43,13 @@ EOF
 ###############################################
 show_flower() {
   cat << EOF
-Kumquats are a group of small fruit-bearing trees in the flowering plant family Rutaceae. They were previously classified as forming the now-historical genus Fortunella, or placed within Citrus, sensu lato.
-They are slow-growing evergreen shrubs or short trees that stand 2.5 to 4.5 meters (8 to 15 ft) tall, with dense branches, sometimes bearing small thorns. The leaves are dark glossy green, and the flowers are white, similar to other citrus flowers, and can be borne singly or clustered within the leaves' axils. Depending on size, the kumquat tree can produce hundreds or even thousands of fruits each year.
+
+Lavandula (common name lavender) is a genus of 47 known species of flowering plants in the mint family, Lamiaceae. It is native to the Old World and is found in Cape Verde and the Canary Islands, and from Europe across to northern and eastern Africa, the Mediterranean, southwest Asia to India
+Many members of the genus are cultivated extensively in temperate climates as ornamental plants for garden and landscape use, for use as culinary herbs, and also commercially for the extraction of essential oils.
+The most widely cultivated species, Lavandula angustifolia, is often referred to as lavender, and there is a color named for the shade of the flowers of this species. Lavender has been used over centuries in traditional medicine and cosmetics, and "limited clinical trials support therapeutic use of lavender for pain, hot flushes, and postnatal perineal discomfort."
 
 Check more information:
-   https://en.wikipedia.org/wiki/Kumquat
+   https://en.wikipedia.org/wiki/Lavandula
 
 EOF
 }
@@ -58,20 +60,28 @@ EOF
 show_recipe() {
   cat << EOF
 
-Kumquat Quest - Cocktail 
+Lavender Martini - Cocktail
 
-From: https://cocktailsdistilled.com/recipe/kumquat-quest/
+From: https://sweetlifebake.com/lavender-martini/
+
+Author: Vianney Rodriguez
 
 Ingredients:
 
-    2 oz Vodka
-    1/2 oz Grand Marnier
-    1/2 oz Lime Juice
-    Sugar, Demerara Brown
-    3-4 Kumquats
+    1 ounce vodka
+    1/2 ounce fresh lemon juice
+    1/4 ounce lavender syrup recipe follows
+    1 cup water
+    1 cup sugar
+    1 tablespoon dried lavender buds
 
 Instructions:
-    Cut all the kumquats in half and put all the rest of the ingredients into a highball glass. Muddle all and add crushed ice. Float Grand Marnier.
+
+    1. To a cocktail shaker filled with ice add vodka, lemon juice and lavender syrup; shake well.
+    2. Strain into a martini glass and garnish with a lemon slice.
+    3. For Lavender syrup: Bring water and sugar to a boil, stirring until sugar dissolves.
+    4. Remove from heat, add lavender buds and allow to infuse for 20 minutes.
+    5. Strain, allow to cool completely and use to make cocktails, lemonade and mocktails.
 
 EOF
 }

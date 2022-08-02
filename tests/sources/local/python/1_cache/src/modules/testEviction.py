319a5d4b4114b4098f35972785d1227740141852
fconejer <francisco.conejero@bsc.es>
2022-08-02 11:00:43 +0200

    Improved cache test

---
+++ tests/sources/local/python/1_cache/src/modules/testEviction.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+"""
+PyCOMPSs Testbench
+==================
+"""
+
+# Imports
+import unittest
+import numpy as np
+from pycompss.api.task import task
+from pycompss.api.api import compss_wait_on
+from pycompss.api.parameter import *
+
+@task(returns=1, cache_returns=True)
+def producer():
+    obj = np.random.random(1000)
+    return obj # This will be put into cache
+
+
+class testEviction(unittest.TestCase):
+
+    def testCacheNumpyArray(self):
+        objs = []
+        # It will only fit 125 into cache
+        for i in range(200):
+            objs.append(producer())
+        objs = compss_wait_on(objs)
+        # print("Results: " + str(objs))

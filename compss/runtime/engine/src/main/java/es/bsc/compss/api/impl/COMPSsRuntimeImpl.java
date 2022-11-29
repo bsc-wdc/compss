628b0889f63a4a6450b0811aed05f43c7168e7dd
Francesc Lordan <francesc.lordan@gmail.com>
2022-11-29 10:22:53 +0100

    Clean imports runtimeImpl

--- compss/runtime/engine/src/main/java/es/bsc/compss/api/impl/COMPSsRuntimeImpl.java
+++ compss/runtime/engine/src/main/java/es/bsc/compss/api/impl/COMPSsRuntimeImpl.java
@@ -88,12 +88,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 

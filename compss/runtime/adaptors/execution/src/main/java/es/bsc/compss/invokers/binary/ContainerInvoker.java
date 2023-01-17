9778b0aff8228aa5a394113c73a208894e608b37
nihadness <nihad.mammadli@bsc.es>
2023-01-17 16:45:16 +0100

    Polish.

--- compss/runtime/adaptors/execution/src/main/java/es/bsc/compss/invokers/binary/ContainerInvoker.java
+++ compss/runtime/adaptors/execution/src/main/java/es/bsc/compss/invokers/binary/ContainerInvoker.java
@@ -315,7 +315,7 @@ public class ContainerInvoker extends Invoker {
                 cmd[cmdIndex++] = "-i";
                 cmd[cmdIndex++] = "--rm";
                 cmd[cmdIndex++] = "--mount";
-                cmd[cmdIndex++] = "source=pycompss_path,destination=\"/opt/COMPSs/Bindings/python/3/pycompss/\"";
+                cmd[cmdIndex++] = "source=pycompss_path,destination=/opt/COMPSs/Bindings/python/3/pycompss/";
                 cmd[cmdIndex++] = "-v";
                 // todo: nm: if the env variable is defined, use that
                 // nm: for the app dir, we need exactly the same as this, but it goes inside case CET_PYTHON..

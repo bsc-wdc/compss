a295bf8d4b1424a165ed716a097fa7c92fe9f2d2
nihadness <nihad.mammadli@bsc.es>
2023-01-24 15:42:24 +0100

    Add ENV variables to the env_script.sh.

--- tests/sources/local/python/1_decorator_container/env_script.sh
+++ tests/sources/local/python/1_decorator_container/env_script.sh
@@ -1,3 +1,5 @@
 export COMPSS_MPIRUN_TYPE=openmpi
 export DOCKER_WORKING_DIR_MOUNT="/tmp/COMPSsWorker01/"
 export DOCKER_WORKING_DIR_VOLUME="working_dir" -e
+export DOCKER_APP_DIR_MOUNT="/home/jenkins/tests_execution_sandbox/apps/"
+export DOCKER_APP_DIR_VOLUME="app_dir"

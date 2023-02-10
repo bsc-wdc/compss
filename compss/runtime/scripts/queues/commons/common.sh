824ed9ad5e3dec8cb02a0b5f45aa61cb1da1a8d5
jorgee <jorge.ejarque@bsc.es>
2023-02-10 14:57:55 +0100

    fix container execution problems in 3.x versions

--- compss/runtime/scripts/queues/commons/common.sh
+++ compss/runtime/scripts/queues/commons/common.sh
@@ -1024,8 +1024,8 @@ add_master_and_worker_nodes(){
     source "${COMPSS_HOME}/Runtime/scripts/system/${HOSTLIST_CMD}"
   else
     host_list=\$(${HOSTLIST_CMD} \$${ENV_VAR_NODE_LIST} ${HOSTLIST_TREATMENT})
-    master_node=\$(${MASTER_NAME_CMD})
-    worker_nodes=\$(echo \${host_list} | sed -e "s/\${master_node}//g")
+    export COMPSS_MASTER_NODE=\$(${MASTER_NAME_CMD})
+    export COMPSS_WORKER_NODES=\$(echo \${host_list} | sed -e "s/\${COMPSS_MASTER_NODE}//g")
   fi
 
 EOT
@@ -1040,8 +1040,8 @@ add_only_master_node(){
     source "${COMPSS_HOME}/Runtime/scripts/system/${HOSTLIST_CMD}"
   else
     host_list=\$(${HOSTLIST_CMD} \$${ENV_VAR_NODE_LIST} ${HOSTLIST_TREATMENT})
-    master_node=\$(${MASTER_NAME_CMD})
-    worker_nodes=""
+    export COMPSS_MASTER_NODE=\$(${MASTER_NAME_CMD})
+    export COMPSS_WORKER_NODES=""
   fi
 
 EOT
@@ -1055,7 +1055,7 @@ add_only_worker_nodes(){
     source "${COMPSS_HOME}/Runtime/scripts/system/${HOSTLIST_CMD}"
   else
     host_list=\$(${HOSTLIST_CMD} \$${ENV_VAR_NODE_LIST}${env_var_suffix} ${HOSTLIST_TREATMENT})
-    worker_nodes=\$(echo \${host_list})
+    export COMPSS_WORKER_NODES=\$(echo \${host_list})
   fi
 
 EOT
@@ -1072,27 +1072,27 @@ add_launch(){
     # ADD STORAGE_INIT, STORAGE_FINISH AND NODES PARSING
     cat >> "${TMP_SUBMIT_SCRIPT}" << EOT
 storage_conf=$HOME/.COMPSs/\$${ENV_VAR_JOB_ID}/storage/cfgfiles/storage.properties
-storage_master_node="\${master_node}"
+storage_master_node="\${COMPSS_MASTER_NODE}"
 
 # The storage_init.sh can put environment variables in the temporary file which will be sourced afterwards
 variables_to_be_sourced=$(mktemp)
-${storage_home}/scripts/storage_init.sh \$${ENV_VAR_JOB_ID} "\${master_node}" "\${storage_master_node}" "\${worker_nodes}" "${network}" "${storage_props}" "\${variables_to_be_sourced}" "${storage_container_image}" "${storage_cpu_affinity}"
+${storage_home}/scripts/storage_init.sh \$${ENV_VAR_JOB_ID} "\${COMPSS_MASTER_NODE}" "\${storage_master_node}" "\${COMPSS_WORKER_NODES}" "${network}" "${storage_props}" "\${variables_to_be_sourced}" "${storage_container_image}" "${storage_cpu_affinity}"
 
 if [ -f "\${variables_to_be_sourced}" ]; then
     source "\${variables_to_be_sourced}"
     rm "\${variables_to_be_sourced}"
 fi
 
-${COMPSS_HOME}/Runtime/scripts/user/launch_compss${AGENTS_SUFFIX} ${AGENTS_HIERARCHY} --master_node="\${master_node}" --worker_nodes="\${worker_nodes}" --node_memory=${node_memory} --node_storage_bandwidth=${node_storage_bandwidth} --storage_conf=\${storage_conf} ${args_pass}
+${COMPSS_HOME}/Runtime/scripts/user/launch_compss${AGENTS_SUFFIX} ${AGENTS_HIERARCHY} --master_node="\${COMPSS_MASTER_NODE}" --worker_nodes="\${COMPSS_WORKER_NODES}" --node_memory=${node_memory} --node_storage_bandwidth=${node_storage_bandwidth} --storage_conf=\${storage_conf} ${args_pass}
 
-${storage_home}/scripts/storage_stop.sh \$${ENV_VAR_JOB_ID} "\${master_node}" "\${storage_master_node}" "\${worker_nodes}" ${network} ${storage_props}
+${storage_home}/scripts/storage_stop.sh \$${ENV_VAR_JOB_ID} "\${COMPSS_MASTER_NODE}" "\${storage_master_node}" "\${COMPSS_WORKER_NODES}" ${network} ${storage_props}
 
 EOT
   else
     # ONLY ADD EXECUTE COMMAND
     cat >> "${TMP_SUBMIT_SCRIPT}" << EOT
 
-${COMPSS_HOME}/Runtime/scripts/user/launch_compss${AGENTS_SUFFIX} ${AGENTS_HIERARCHY} --master_node="\${master_node}" --worker_nodes="\${worker_nodes}" --node_memory=${node_memory} --node_storage_bandwidth=${node_storage_bandwidth} ${args_pass}
+${COMPSS_HOME}/Runtime/scripts/user/launch_compss${AGENTS_SUFFIX} ${AGENTS_HIERARCHY} --master_node="\${COMPSS_MASTER_NODE}" --worker_nodes="\${COMPSS_WORKER_NODES}" --node_memory=${node_memory} --node_storage_bandwidth=${node_storage_bandwidth} ${args_pass}
 EOT
   fi
 }

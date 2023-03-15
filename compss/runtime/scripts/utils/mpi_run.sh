657e5354f58e4c1d6396dbe597b13039392818ee
nihadness <nihad.mammadli@bsc.es>
2023-03-15 09:18:19 +0100

    Set Slurm env vars when running with container.

--- compss/runtime/scripts/utils/mpi_run.sh
+++ compss/runtime/scripts/utils/mpi_run.sh
@@ -3,8 +3,15 @@
 echo "Running mpi_run.sh"
 echo "$ENV_LOAD_MODULES_SCRIPT"
 
+env | grep "SLURM_" | sed 's/SLURM/export SLURM/g' | sed 's/=/="/g' | sed -e 's/$/"/' > $COMPSS_MASTER_WORKING_DIR/slurm_vars
+
+env
+
 
 ssh -o StrictHostKeyChecking=no $(hostname) /bin/bash <<EOF
+
+source $COMPSS_MASTER_WORKING_DIR/slurm_vars
+
 cd $COMPSS_MASTER_WORKING_DIR
 export COMPSS_MPIRUN_TYPE=$COMPSS_MPIRUN_TYPE
 

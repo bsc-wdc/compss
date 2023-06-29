a444d00e4ac77dd8f2d89d6b12e5020ec8be2433
nihadness <nihad.mammadli@bsc.es>
2023-06-29 13:44:31 +0200

    Change working dir after ssh in mpi executions with container.

--- compss/runtime/scripts/utils/mpi_run.sh
+++ compss/runtime/scripts/utils/mpi_run.sh
@@ -2,7 +2,7 @@
 
 echo "Running mpi_run.sh"
 echo "$ENV_LOAD_MODULES_SCRIPT"
-
+WORKING_DIR=$PWD
 env | grep "SLURM_" | sed 's/SLURM/export SLURM/g' | sed 's/=/="/g' | sed -e 's/$/"/' > $COMPSS_MASTER_WORKING_DIR/slurm_vars
 
 env
@@ -12,7 +12,7 @@ ssh -o StrictHostKeyChecking=no $(hostname) /bin/bash <<EOF
 
 source $COMPSS_MASTER_WORKING_DIR/slurm_vars
 
-cd $COMPSS_MASTER_WORKING_DIR
+cd $WORKING_DIR
 export COMPSS_MPIRUN_TYPE=$COMPSS_MPIRUN_TYPE
 
 source $ENV_LOAD_MODULES_SCRIPT

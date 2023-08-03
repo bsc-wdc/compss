503ad69e1765b47cbcff8ad061af5c9c51c9571d
fconejer <francisco.conejero@bsc.es>
2023-08-03 15:41:48 +0200

    Fixed compilation scripts (added missing dependency for metrics).

--- compss/programming_model/bindings/python/scripts/compilation/metrics.sh
+++ compss/programming_model/bindings/python/scripts/compilation/metrics.sh
@@ -9,7 +9,7 @@ cd "${SCRIPT_DIR}/../../src/"
 ## Check typing (with metics) with mypy ##
 ##########################################
 
-mypy --pretty --html-report . --txt-report . --check-untyped-defs --warn-redundant-casts --ignore-missing-imports --exclude 'pycompss\/((tests\/)|(dds\/)|(streams\/)||(interactive.py)|(__main__.py))$' ./pycompss/
+mypy --pretty --html-report . --txt-report . --check-untyped-defs --warn-redundant-casts --ignore-missing-imports --exclude 'pycompss\/((tests\/)|(dds\/)|(util\/objects\/replace.py)|(api\/local.py)|(api\/commons\/private_tasks.py)|(api\/software.py)|(functions\/data_tasks.py)|(streams\/)|(interactive.py)|(util\/interactive\/events.py)||(util\/interactive\/state.py)|(__main__.py))$' ./pycompss/
 ev=$?
 if [ "$ev" -ne 0 ]; then
   echo "[ERROR] Mypy metrics failed with exit value: $ev"
--- compss/programming_model/bindings/python/scripts/install_compilation_deps.sh
+++ compss/programming_model/bindings/python/scripts/install_compilation_deps.sh
@@ -4,3 +4,4 @@ python3 -m pip install mypy --user
 python3 -m pip install mypy-extensions --user
 python3 -m pip install types-tabulate --user
 python3 -m pip install types-decorator --user
+python3 -m pip install lxml --user

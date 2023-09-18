97d3ea8c0a241acff168f40f71f61c194b3d18ba
rcutal <roc.cutal@bsc.es>
2023-09-18 12:52:32 +0200

    Fixed issues with int constraints

--- compss/programming_model/bindings/python/src/pycompss/runtime/task/master.py
+++ compss/programming_model/bindings/python/src/pycompss/runtime/task/master.py
@@ -515,10 +515,11 @@ class TaskMaster:
             constraints = kwargs[CORE_ELEMENT_KEY].get_impl_constraints()
             for a in constraints:
                 self.constraint_args[a] = ConstraintDescription(constraints[a])
-                if constraints[a].isdigit() or (
+                if (isinstance(constraints[a], int) or
+                    constraints[a].isdigit() or (
                     isinstance(constraints[a], str)
                     and constraints[a].startswith("$")
-                ):
+                )):
                     if __debug__:
                         logger.debug(
                             "Detected constraint as static value or env var"

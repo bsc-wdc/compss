fa46892d5012af11195289478b7b1516f806549a
Jorge Ejarque <jorge.ejarque@bsc.es>
2024-04-17 09:41:46 +0200

    fixes on the generation of R processes

--- compss/runtime/adaptors/nio/scripts/persistent_worker_clean.sh
+++ compss/runtime/adaptors/nio/scripts/persistent_worker_clean.sh
@@ -11,18 +11,20 @@
 #
 
 kill_recursive() {
-  # Active processes
-  to_kill=$(ps --ppid "$1" -o pid= | awk '{ print $1 }' | tr '\n' ' ')
+  if [ $1 -gt 0 ]; then 
+     # Active processes
+     to_kill=$(ps --ppid "$1" -o pid= | awk '{ print $1 }' | tr '\n' ' ')
  
-  # Kill each child process of the parent
-  for fn in $to_kill; do
-    kill_recursive "$fn"
-  done
+     # Kill each child process of the parent
+     for fn in $to_kill; do
+        kill_recursive "$fn"
+     done
 
-  # Kill the parent process
-  if [ -z "$to_kill" ]; then
-    # echo "Killing $1"
-    kill -15 "$1"
+     # Kill the parent process
+     if [ -z "$to_kill" ]; then
+        echo "Killing $1"
+        kill -15 "$1"
+     fi
   fi
 }
 
--- compss/runtime/adaptors/nio/scripts/pipers/executor.R
+++ compss/runtime/adaptors/nio/scripts/pipers/executor.R
@@ -23,7 +23,7 @@ while (TRUE) {
   if (length(data) == 0) {
     break
   }
-  
+
   # Check if the received data is "QUIT" and exit the loop
   if (data == "QUIT") {
     break
@@ -52,27 +52,108 @@ while (TRUE) {
     # TODO: Add here the process of the task and write end_task message
     # Writing "END_TASK" message at this m  failed task
     # Load the module
-    tryCatch(
-	{
-		source(module)
-    		# Call the function using get()
-    		if (exists(func)) {
-       			result <- get(func)()
-       			print(result)
-			# TODO: Manage returns
-			cat("END_TASK", task_id, 0, file = output_fifo, "\n")
-    		} else {
-       			cat("Function", function_name, "does not exist")
-			cat("END_TASK", task_id, 1, file = output_fifo, "\n")
-		}
-	}, error = function(e) {
-    		# Handle the error
-    		print(paste("Error:", e$message))
-		traceback()	
-		cat("END_TASK", task_id, 1, file = output_fifo, "\n")
-	})
 
-    cat("END_TASK", task_id, 1, file = output_fifo, "\n")
+
+    cat("The module is:", module, "\n")
+    tryCatch(
+             {
+               source(module)
+               cat("Finished     source(module)", file = job_out)
+               # Call the function using get()
+               if (exists(func)) {
+
+                 num_of_nodes <- as.integer(params[1])
+                 cat("num_of_nodes is: ", num_of_nodes)
+                 num_of_threads <- as.integer(params[1+num_of_nodes+1])
+                 has_target <- as.logical(params[1+num_of_nodes+2]) #!!!
+                 return_type <- params[1+num_of_nodes+3]
+                 num_of_returns <- as.integer(params[1+num_of_nodes+4])
+                 num_of_args <- as.integer(params[1+num_of_nodes+5])
+                 cat("num_of_args is", num_of_args)
+                 #result <- get(func)(3, 4)
+                 leng_params_func <- length(formalArgs(func))
+                 # print(formalArgs(func))
+                 cat("leng_params_func is", leng_params_func)
+                 params_func_list <- list(leng_params_func)
+
+                 first_arg_ind <- num_of_nodes + 7 
+                 for(i in 0:(leng_params_func-1)){
+                   if(params[first_arg_ind] == "0"){
+                     params_func_list[[i+1]] <- as.integer(params[first_arg_ind + 5])
+                     names(params_func_list)[i+1] <- params[first_arg_ind + 3]
+                     first_arg_ind <- first_arg_ind + 6
+                   }else if(params[first_arg_ind] == "7"){
+                     params_func_list[[i+1]] <- as.numeric(params[first_arg_ind + 5])
+                     names(params_func_list)[i+1] <- params[first_arg_ind + 3]
+                     first_arg_ind <- first_arg_ind + 6
+                   }else if(params[first_arg_ind] == "8"){
+                     num_of_words <- as.integer(params[first_arg_ind + 5])
+                     vec_words <- character(num_of_words)
+                     for(j in 1:num_of_words){
+                       vec_words[j] <- as.character(params[first_arg_ind + 5 + j])
+                     }
+                     params_func_list[[i+1]] <- paste0(vec_words, collapse = " ")
+                     names(params_func_list)[i+1] <- params[first_arg_ind + 3]
+                     first_arg_ind <- first_arg_ind + 6 + num_of_words
+                   }else if(params[first_arg_ind] == "10"){
+                     content_type <- as.character(params[first_arg_ind + 4])
+                     # print(paste0("The content type is: ", content_type, "\n"))
+                     path_value <- as.character(params[first_arg_ind + 5])
+                     path_value <- strsplit(path_value, ":")[[1]]
+                     path_value <- path_value[length(path_value)]
+                     if(content_type != "null"){
+                       # print(paste0("The path value is: ", path_value, "\n"))
+                       #params_func_list[[i+1]] <- compss_unserialize(filepath = path_value)
+                       #params_func_list[[i+1]] <- readRDS(file = path_value)
+                       ext <- strsplit(path_value, "[.]")[[1]]
+                       ext <- ext[length(ext)]
+                       con <- file(description = path_value, open = "rb")
+                       par_raw <- readBin(con, what = raw(), n = file.info(path_value)$size)
+                       params_func_list[[i+1]] <- unserialize(connection = par_raw)
+                       close(con)
+                       # print("params_func_list\n")
+                       # print(params_func_list[i+1])
+                     }else{
+                       params_func_list[[i+1]] <- path_value
+                     }
+                     names(params_func_list)[i+1] <- params[first_arg_ind + 3]
+                     first_arg_ind <- first_arg_ind + 6
+                   }else{
+                     cat("i = ", i)
+                     cat("Non-supported type: ", params[first_arg_ind + 6*i])
+                   }
+                 }
+                 # print("params_func_list:\n")
+                 # print(params_func_list)
+                 result <- do.call(func, params_func_list)
+                 cat("num_of_returns:", num_of_returns, "\n")
+                 if(num_of_returns > 0){
+                   path_return_value <- as.character(params[first_arg_ind + 5])
+                   path_return_value <- strsplit(path_return_value, ":")[[1]]
+                   path_return_value <- path_return_value[length(path_return_value)]
+                   # compss_serialize(object = result, filepath = path_return_value)
+                   con <- file(description = path_return_value, open = "wb")
+                   x <- serialize(object = result, connection = NULL)
+                   writeBin(x, con)
+                   close(con)
+                   cat("The path is: ", path_return_value)
+                 }
+                 # print("The results is:\n")
+                 # print(result)
+                 cat("END_TASK", task_id, 0, file = output_fifo, "\n")
+               } else {
+                 cat("Function", function_name, "does not exist")
+                 cat("END_TASK", task_id, 1, file = output_fifo, "\n")
+               }
+             }, error = function(e) {
+               # Handle the error
+               cat("Error:", e$message, file = job_err)
+               print(paste("Error:", e$message))
+               traceback()	
+               cat("END_TASK", task_id, 1, file = output_fifo, "\n")
+             })
+
+    # cat("END_TASK", task_id, 1, file = output_fifo, "\n")
   } else {
     cat("Received:", data, "\n")
   }
@@ -81,4 +162,4 @@ while (TRUE) {
 # Close the FIFOs
 close(input_fifo)
 close(output_fifo)
-
+#

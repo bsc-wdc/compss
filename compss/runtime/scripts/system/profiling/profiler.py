04d45da0515d8fc404cf70c598b9f0bbcb47d681
nicologiacomini <nicolo.giacomini@bsc.es>
2024-12-02 17:15:55 +0100

    add profiling python files

--- compss/runtime/scripts/system/profiling/profiler.py
+++ compss/runtime/scripts/system/profiling/profiler.py
@@ -1,151 +1,97 @@
-#!/usr/bin/env python3
-#
-#  Copyright 2002-2024 Barcelona Supercomputing Center (www.bsc.es)
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
-import subprocess, os, sys
-import time
-import socket
+import subprocess, psutil, os, sys
+import xml.etree.ElementTree as ET
 from datetime import datetime
 
-try:
-    import psutil
-except ImportError:
-    print(
-        "psutil is not installed. Install it, if you want to monitor the resources status during the execution."
-    )
-    exit(1)
+def get_status_profiling(prof_status):
+    with open(prof_status, 'r') as prof:
+        return 'true' in prof.readline()
 
+def profiling_function(interval, computing_units, byte_read, byte_write, time_read, time_write):
+    net_start = psutil.net_io_counters()
+    cpus = psutil.cpu_percent(interval=interval, percpu=True)
+    if computing_units is None:
+        computing_units = len(cpus)
+    cpu_avg = round(sum(cpus[:computing_units]) / computing_units, 2)
+    mem = psutil.virtual_memory().percent
 
-def get_cpu_top() -> list:
-    """
-    Execute the command to get the values of cpu and memory usage, by calling a bash command
+    net_end = psutil.net_io_counters()
+    byte_sent = net_end.bytes_sent - net_start.bytes_sent
+    byte_recv = net_end.bytes_recv - net_start.bytes_recv
 
-    :return: list containing the as first value the cpu percentage and second value the memory percentage
-    """
-    COMMAND = "export LC_NUMERIC=C && top -b -n 1 | awk '/^%Cpu/ { cpu_usage = 100 - $8; cpu_usage = cpu_usage * 2; cpu_usage = (cpu_usage > 100) ? 100.0 : cpu_usage } /^MiB Mem/ { mem_usage = ($8 / $4) * 100 } END { printf \"%.2f,%.2f,\", cpu_usage, mem_usage }'"
-
-    result = subprocess.check_output(COMMAND, shell=True, text=True).strip().split(",")
-    return result
-
-
-def profiling_function(
-    byte_read: int,
-    byte_write: int,
-    time_read: int,
-    time_write: int,
-    prev_bytes_sent: int,
-    prev_bytes_recv: int,
-) -> tuple:
-    """
-    Function to profile and monitor system resource usage, including CPU, memory, and network I/O.
-
-    :param byte_read: The total number of bytes read during the profiling period.
-    :param byte_write: The total number of bytes written during the profiling period.
-    :param time_read: The time taken (in seconds) for read operations.
-    :param time_write: The time taken (in seconds) for write operations.
-    :param prev_bytes_sent: The total number of bytes sent before this profiling period.
-    :param prev_bytes_recv: The total number of bytes received before this profiling period.
-
-    :return: A tuple containing three elements:
-        - A formatted string with the following comma-separated values:
-            * CPU usage (average percentage)
-            * Memory usage (percentage)
-            * Bytes sent during the interval
-            * Bytes received during the interval
-            * Bytes read during the interval
-            * Bytes written during the interval
-            * Time taken for read operations
-            * Time taken for write operations
-            * Timestamp of the profiling event
-        - The updated total number of bytes sent.
-        - The updated total number of bytes received.
-    """
-    cpu_mem = get_cpu_top()
-    cpu = cpu_mem[0]
-    mem = cpu_mem[1]
-
-    net = psutil.net_io_counters()
-    byte_sent = net.bytes_sent - prev_bytes_sent
-    byte_recv = net.bytes_recv - prev_bytes_recv
-
-    new_entry = f"{cpu},{mem},{byte_sent},{byte_recv},{byte_read},{byte_write},{time_read},{time_write},{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
-    return new_entry, net.bytes_sent, net.bytes_recv
+    new_entry = f"{cpu_avg},{mem},{byte_sent},{byte_recv},{byte_read},{byte_write},{time_read},{time_write},{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
+    return new_entry
 
 
 def main():
-    log_dir = sys.argv[1]
-    print(f"LOG DIRECTORY RECEIVED: {log_dir}")
+    profiling_status_file = sys.argv[1]
+    log_dir = sys.argv[2]
+    # config_file = sys.argv[3]
 
-    profiling_interval = int(os.getenv("COMPSS_PROFILING_INTERVAL"))
     compss_home = os.getenv("COMPSS_HOME")
 
     computing_units = None
-    # hostname = "localhost"
+    hostname = 'localhost'
+    #hostname = str(subprocess.check_output("hostname", shell=True, universal_newlines=True)).strip()
 
-    is_local = os.getenv("IS_LOCAL")
-    hostname = "localhost" if is_local else socket.gethostname()
+    # is_bsc = os.getenv('BSC_MACHINE')
+    # hostname = hostname if is_bsc else 'localhost'
+    # if hostname == 'localhost':
+    #     to_parse = True
+    #     while to_parse:
+    #         try:
+    #             root = ET.parse(config_file)
+    #             computing_units = int(root.find('.//ComputingUnits').text)
+    #             hostname = "localhost"
+    #             print("ComputingUnits: "+str(computing_units))
+    #             to_parse = False
+    #         except:
+    #             print(f'Wrong file {config_file}, using the default configuration.')
+    #             config_file = compss_home + "/Runtime/configuration/xml/resources/default_resources.xml"
+    #             to_parse = True
 
-    to_write = "CPU,MEM,BYTE_SENT,BYTE_RECV,BYTE_READ_DISK,BYTE_WRITE_DISK,TIME_READ_DISK,TIME_WRITE_DISK,TIME\n"
+    to_write = 'CPU,MEM,BYTE_SENT,BYTE_RECV,BYTE_READ_DISK,BYTE_WRITE_DISK,TIME_READ_DISK,TIME_WRITE_DISK,TIME\n'
 
-    io_initial = psutil.disk_io_counters()
-    ref_read, ref_write, ref_time_read, ref_time_write = (
-        io_initial.read_bytes,
-        io_initial.write_bytes,
-        io_initial.read_time,
-        io_initial.write_time,
-    )
-    net = psutil.net_io_counters()
-    ref_byte_sent, ref_byte_recv = net.bytes_sent, net.bytes_recv
+    check = get_status_profiling(profiling_status_file)
 
-    new_entry, ref_byte_sent, ref_byte_recv = profiling_function(
-        0, 0, 0, 0, ref_byte_sent, ref_byte_recv
-    )
-    to_write += new_entry
+    io = psutil.disk_io_counters()
+    ref_read = io.read_bytes # 10
+    ref_write = io.write_bytes
+    ref_time_read = io.read_time
+    ref_time_write = io.write_time
 
-    with open(f"{log_dir}/resource_profiling_{hostname}.csv", "w") as resource:
-        resource.write(to_write)
-        resource.flush()
+    to_write += profiling_function(1, computing_units, 0, 0,0,0)
 
-        while True:
-            time.sleep(profiling_interval)
-            io_current = psutil.disk_io_counters()
-            byte_read = io_current.read_bytes - ref_read
-            byte_write = io_current.write_bytes - ref_write
-            time_read = io_current.read_time - ref_time_read
-            time_write = io_current.write_time - ref_time_write
+    io = psutil.disk_io_counters()
+    new_read = io.read_bytes
+    new_write = io.write_bytes
+    new_time_read = io.read_time
+    new_time_write = io.write_time
 
-            # Update references
-            ref_read, ref_write, ref_time_read, ref_time_write = (
-                io_current.read_bytes,
-                io_current.write_bytes,
-                io_current.read_time,
-                io_current.write_time,
-            )
+    while check:
+        byte_read = new_read - ref_read
+        byte_write = new_write - ref_write
+        time_read = new_time_read - ref_time_read
+        time_write = new_time_write - ref_time_write
 
-            new_entry, ref_byte_sent, ref_byte_recv = profiling_function(
-                byte_read,
-                byte_write,
-                time_read,
-                time_write,
-                ref_byte_sent,
-                ref_byte_recv,
-            )
+        ref_read += byte_read
+        ref_write += byte_write
+        ref_time_read += time_read
+        ref_time_write += time_write
 
-            resource.write(new_entry)
-            resource.flush()
+        to_write += profiling_function(5, computing_units, byte_read, byte_write, time_read, time_write)
+        io = psutil.disk_io_counters()
+        new_read = io.read_bytes
+        new_write = io.write_bytes
+        new_time_read = io.read_time
+        new_time_write = io.write_time
+
+        check = get_status_profiling(profiling_status_file)
+
+    stats_path = f'{log_dir}/stats'
+    os.makedirs(stats_path, exist_ok=True)
+    with open(f'{stats_path}/resource_profiling_{hostname}.csv', 'w') as f:
+        f.write(to_write)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ compss/runtime/scripts/system/profiling/profiler_path.py
@@ -0,0 +1,62 @@
+import subprocess, psutil, time, sys
+from datetime import datetime
+
+def get_status_profiling(prof_status):
+    with open(prof_status, 'r') as prof:
+        return 'true' in prof.readline()
+
+
+def get_processes_by_pathname(pathname):
+    """Get PIDs of processes that contain the specified pathname in their command."""
+    matching_processes = []
+
+    for process in psutil.process_iter(['pid', 'cmdline']):
+        try:
+            # Get the command line of the process
+            cmdline = process.info['cmdline']
+            # Check if the command line is not empty and contains the specified pathname
+            if cmdline and any(pathname in arg for arg in cmdline):
+                matching_processes.append({
+                    'pid': process.info['pid'],
+                    'cmdline': ' '.join(cmdline)  # Join the cmdline list into a single string for display
+                })
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            # Skip processes that are no longer available or can't be accessed
+            continue
+
+    return matching_processes
+
+profiling_status_file = sys.argv[1]
+log_dir = sys.argv[2]
+
+to_write = 'PID,CPU,MEM,TIME\n'
+pathname = "/opt/COMPSs/"
+
+check = get_status_profiling(profiling_status_file)
+
+while check:
+    children = get_processes_by_pathname(pathname)
+    for process in children:
+        pid = process['pid']
+        print(pid)
+        try:
+            p = psutil.Process(pid)
+            if p.status() == psutil.STATUS_RUNNING or p.status() == psutil.STATUS_SLEEPING:
+                command = f'top -b -n 1 | grep "^ *{pid}"'
+                result = subprocess.check_output(command, shell=True, universal_newlines=True)
+                r = list(filter(None, result.split(' ')))
+                cpu = r[8].strip().replace(',', '.')
+                mem = r[9].strip().replace(',', '.')
+                to_write += f'{pid},{cpu},{mem},{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n'
+
+        except (psutil.NoSuchProcess, subprocess.CalledProcessError, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+
+    time.sleep(1)
+    check = get_status_profiling(profiling_status_file)
+
+# print(to_write)
+
+with open(f'{log_dir}/PROFILING_TOP.csv', 'w') as f:
+    f.write(to_write)
+

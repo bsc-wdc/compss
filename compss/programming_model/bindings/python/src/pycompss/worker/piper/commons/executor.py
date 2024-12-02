476ec6f5c9dde3cf7a295f6c0caee8d68e374d6f
nicologiacomini <nicolo.giacomini@bsc.es>
2024-12-02 17:01:01 +0100

    add dp_enabled parameter

--- compss/programming_model/bindings/python/src/pycompss/worker/piper/commons/executor.py
+++ compss/programming_model/bindings/python/src/pycompss/worker/piper/commons/executor.py
@@ -215,6 +215,7 @@ class ExecutorConf:
         "out_cache_queue",
         "cache_profiler",
         "ear",
+        "dp_enabled",
     ]
 
     def __init__(
@@ -234,6 +235,7 @@ class ExecutorConf:
         out_cache_queue: typing.Optional[Queue] = None,
         cache_profiler: bool = False,
         ear: bool = False,
+        dp_enabled: bool = False,
     ) -> None:
         """Construct a new executor configuration.
 
@@ -253,6 +255,7 @@ class ExecutorConf:
                                to cache_ids.
         :param out_cache_queue: Cache queue where to the cache returns info.
         :param ear: Ear energy metering.
+        :param dp_enabled: Check if Provenance is enabled or not.
         """
         self.debug = debug
         self.tmp_dir = tmp_dir
@@ -269,6 +272,7 @@ class ExecutorConf:
         self.out_cache_queue = out_cache_queue
         self.cache_profiler = cache_profiler
         self.ear = ear
+        self.dp_enabled = dp_enabled
 
 
 ######################

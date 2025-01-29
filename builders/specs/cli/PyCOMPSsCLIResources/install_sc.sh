076ab2c306467f56576a6927bf0668a5626c3eb2
Cristian Tatu <ctatu@bsc.es>
2025-01-29 14:22:14 +0100

    change way of CLI install in cluster

--- builders/specs/cli/PyCOMPSsCLIResources/install_sc.sh
+++ builders/specs/cli/PyCOMPSsCLIResources/install_sc.sh
@@ -10,6 +10,6 @@ path="$1/lib/python${version}/site-packages/"
 mkdir -p ${path}
 
 export PYTHONPATH=${path}:$PYTHONPATH
-python3 setup.py install --prefix=$1 --record installed_files.txt
+python3 -m pip install . --target $1 --no-deps
 
 echo "----- Installation finished -----"

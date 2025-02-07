df32844185e677a8931eb742f5c5c02331b5dbcd
nicologiacomini <nicolo.giacomini@bsc.es>
2025-02-07 19:37:58 +0100

    fix conflict

--- builders/specs/cli/PyCOMPSsCLIResources/pycompss_cli/core/local/cmd.py
+++ builders/specs/cli/PyCOMPSsCLIResources/pycompss_cli/core/local/cmd.py
@@ -27,10 +27,41 @@ from pycompss_cli.core.cmd_helpers import command_runner
 # GLOBAL VARIABLES #
 # ################ #
 
+pointers = ["├── ", "└── "]
+follow_prefix = "│   "
+empty_prefix = "    "
+
+METRICS_UNITS = {
+    'maxTime': 'ms',
+    'minTime': 'ms',
+    'avgTime': 'ms',
+    'executionTime': 'ms',
+    'cpuAvg': '%',
+    'cpuMax': '%',
+    'memAvg': '%',
+    'memMax': '%',
+    'memMin': '%',
+    'byteSent': 'byte',
+    'byteRecv': 'byte',
+}
+
 # ############# #
 # API FUNCTIONS #
 # ############# #
 
+def resources_tree(jsonData, name='', file=None, prefix=empty_prefix, last=False, isfirst=True):
+    if isinstance(jsonData, dict):
+        if not isfirst:
+            print(prefix, pointers[1] if last else pointers[0], name, sep="", file=file)
+        prefix += empty_prefix if last else follow_prefix
+        length = len(jsonData)
+        for i, key in enumerate(jsonData.keys()):
+            last = i == (length - 1)
+            resources_tree(jsonData[key], key, file, prefix, last, isfirst=False)
+    else:
+        unit = METRICS_UNITS[name] if name in METRICS_UNITS.keys() else ''
+        name = name + f' = {jsonData} {unit}'
+        print(prefix, pointers[1] if last else pointers[0], name, sep="", file=file)
 
 def local_deploy_compss(working_dir: str = "") -> None:
     """Starts the main COMPSs image in Docker.
@@ -162,10 +193,6 @@ def local_inspect(ro_crate_list: list):
             print(f"Error loading the RO-Crate from {ro_crate_zip_or_dir} : {e}")
             continue
 
-        pointers = ["├── ", "└── "]
-        follow_prefix = "│   "
-        empty_prefix = "    "
-
         print(f"{ro_crate_zip_or_dir}")
 
         prefix = follow_prefix
@@ -292,15 +319,47 @@ def local_inspect(ro_crate_list: list):
                     i_pointer = 1 if i == (len(env_list) - 1) else 0
                     print(f"{prefix}{pointers[i_pointer]}{env_item[0]} = {env_item[1]}")
 
-            usage_e = e_create_action.get("resourceUsage")
-            usage_list = []
-            if usage_e:
-                for usage in usage_e:
-                    usage_list.append((usage.get("@id", ""), usage.get("value", "")))
-                print(f"{empty_prefix}{pointers[0]}Resource Usage")
-                for i, ru_item in enumerate(usage_list):
-                    i_pointer = 1 if i == (len(usage_list) - 1) else 0
-                    print(f"{prefix}{pointers[i_pointer]}{ru_item[0]} = {ru_item[1]}")
+        usage_e = e_create_action.get("resourceUsage")
+        usage_list = []
+        if usage_e:
+            for usage in usage_e:
+                usage_list.append((usage.get("@id", ""), usage.get("value", "")))
+            print(f"{empty_prefix}{pointers[0]}Resource Usage")
+
+            final_list = []
+
+            for i, ru_item in enumerate(usage_list):
+                ru_list = ru_item[0].split(".")
+                ru_list[0] = ru_list[0][1:]
+                ru_list.append(ru_item[1])
+                final_list.append(ru_list)
+
+            overall_list = []
+            for entry in final_list:
+                entry[0] = entry[0].split("-")[0] if '-' in entry[0] else entry[0]
+                if entry[0] == 'overall':
+                    overall_list.append(entry)
+
+            final_list = [x for x in final_list if x not in overall_list]
+
+            result = {}
+            for row in final_list:
+                current_level = result
+                for i, key in enumerate(row[:-2]):
+                    if key not in current_level:
+                        current_level[key] = {}
+                    current_level = current_level[key]
+                current_level[row[-2]] = row[-1]
+
+            for row in overall_list:
+                current_level = result
+                for i, key in enumerate(row[:-2]):
+                    if key not in current_level:
+                        current_level[key] = {}
+                    current_level = current_level[key]
+                current_level[row[-2]] = row[-1]
+
+            resources_tree(result)
 
             # Times
             e_start_time = e_create_action.get("startTime")

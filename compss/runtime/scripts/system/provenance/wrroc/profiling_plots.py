fa59ead2d6cd054ba07e4b621cc88eac42cc8845
nicologiacomini <nicolo.giacomini@bsc.es>
2025-02-18 12:09:47 +0100

    add generation of plots for all nodes and add average for cpu and memory plots

--- compss/runtime/scripts/system/provenance/wrroc/profiling_plots.py
+++ compss/runtime/scripts/system/provenance/wrroc/profiling_plots.py
@@ -14,15 +14,36 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+import typing
+import time
 import os
+import json
+import sys
 
+from pathlib import Path
+from hashlib import sha256
+from mmap import mmap, ACCESS_READ
+
+from rocrate.rocrate import ROCrate
+from rocrate.model.contextentity import ContextEntity
+
+from provenance.processing.entities import get_manually_defined_software_requirements
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import pandas as pd
 
+
+def timestamp_axis(num_entries, time_list):
+    step = int(num_entries / 60) + 1
+    labels = [f"{time_list[i]}" for i in range(0, len(time_list), step)]
+    time_list = [i for i in range(0, len(time_list), step)]
+    plt.xticks(time_list, labels=labels, rotation=80)
+    plt.subplots_adjust(top=0.95, bottom=0.25)
 
 def build_plot(title, time_list, value_list, name_dataset, measure, num_entries):
     plt.style.use('ggplot')
     fig, ax = plt.subplots(figsize=(12, 8))
+    avg_perc = round(sum(value_list) / num_entries, 2)
 
     if max(value_list) > 10**6:
         value_list = [value / 10 ** 6 for value in value_list]
@@ -30,18 +51,63 @@ def build_plot(title, time_list, value_list, name_dataset, measure, num_entries)
 
     ax.plot(time_list, value_list, marker='.', linestyle='-', label=name_dataset)
 
-    step = int(num_entries / 60) + 1
-    labels = [f"{time_list[i]}" for i in range(0, len(time_list), step)]
-    time_list = [i for i in range(0, len(time_list), step)]
+    timestamp_axis(num_entries, time_list)
 
-    plt.xticks(time_list, labels=labels, rotation=80)
-    plt.subplots_adjust(top=0.95, bottom=0.25)
+    if avg_perc < 100:
+        plt.axhline(avg_perc, color='b', linestyle='--', label=f'Average = {avg_perc}%')
+
+    # step = int(num_entries / 60) + 1
+    # labels = [f"{time_list[i]}" for i in range(0, len(time_list), step)]
+    # time_list = [i for i in range(0, len(time_list), step)]
+
+    # plt.xticks(time_list, labels=labels, rotation=80)
+    # plt.subplots_adjust(top=0.95, bottom=0.25)
 
     ax.set_title(title, fontsize=20)
     ax.set_xlabel('Timestamp')
     ax.set_ylabel(measure)
     ax.grid(True)
     ax.legend()
+
+def plot_bytes(time_list, first_df, first_df_name, second_df, second_df_name, num_entries, title):
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    first_df = [value / 10 ** 6 for value in first_df]
+    second_df = [value / 10 ** 6 for value in second_df]
+
+    for i in range(num_entries):
+        first_value = 0 if i == 0 else first_df[i-1]
+        second_value = 0 if i == 0 else second_df[i-1]
+
+        first_df[i] = first_value + first_df[i]
+        second_df[i] = second_value + second_df[i]
+
+    ax.plot(time_list, first_df, c='r', marker='.', linestyle='-', label=first_df_name)
+    ax.plot(time_list, second_df, c='b', marker='.', linestyle='-', label=second_df_name)
+
+    timestamp_axis(num_entries, time_list)
+
+    ax.set_title(title, fontsize=20)
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Megabyte (MB)')
+    ax.grid(True)
+    ax.legend()
+
+
+def build_plot_nodes(time_list, df_list, num_entries, colors, metric_name):
+    plt.figure(figsize=(12, 8))
+
+    for (label, values), i in zip(df_list.items(), range(len(df_list))):
+        index = i % len(colors)
+        plt.plot(values[:num_entries], label=label, color=colors[index], marker='.', linestyle='-')
+
+    timestamp_axis(num_entries, time_list)
+
+    plt.legend()
+    plt.xlabel('Timestamp')
+    plt.ylabel('Percentage')
+    plt.title(f'Percentage of {metric_name} for each node')
 
 
 def plot_results(folder_pathname):
@@ -59,6 +125,9 @@ def plot_results(folder_pathname):
 
     plots_pathname = folder_pathname + '/plots/'
 
+    list_of_cpus = {}
+    list_of_mems = {}
+
     # iterate on every file in the directory
     for csv_resources in os.listdir(folder_pathname):
         master_node = not 'static_' in csv_resources
@@ -71,7 +140,7 @@ def plot_results(folder_pathname):
         # if machine_name.strip().upper() == master_node:
         #     machine_name += '(MASTER)'
         if master_node:
-            machine_name += "-MASTER"
+            machine_name += '-MASTER'
 
         df = pd.read_csv(csv_resources)
         df_lenght = len(df)
@@ -85,7 +154,9 @@ def plot_results(folder_pathname):
         time_read_disk = df["TIME_READ_DISK"]
         time_write_disk = df["TIME_WRITE_DISK"]
         timestamps = df["TIME"]
-        # timestamps = [i for i in range(0, len(timestamps))]
+
+        list_of_cpus[machine_name] = list(cpu_usage)
+        list_of_mems[machine_name] = list(mem_usage)
 
         output_path = plots_pathname + machine_name
         os.makedirs(output_path, exist_ok=True)
@@ -98,12 +169,20 @@ def plot_results(folder_pathname):
         plt.savefig(output_path + '/mem.png')
         plt.close()
 
+        plot_bytes(time_list=timestamps, first_df=byte_sent, first_df_name="Bytes sent", second_df=byte_recv, second_df_name="Bytes received", num_entries=df_lenght, title="Network usage")
+        plt.savefig(output_path + '/network_usage.png')
+        plt.close()
+
         build_plot('Data transferred: bytes sent', timestamps, byte_sent, name_dataset='BYTE_SENT', measure='Byte (B)', num_entries=df_lenght)
         plt.savefig(output_path + '/bytes_sent.png')
         plt.close()
 
         build_plot('Data transferred: bytes received', timestamps, byte_recv, name_dataset='BYTE_RECV', measure='Byte (B)', num_entries=df_lenght)
         plt.savefig(output_path + '/bytes_received.png')
+        plt.close()
+
+        plot_bytes(time_list=timestamps, first_df=byte_write_disk, first_df_name="Bytes written", second_df=byte_read_disk, second_df_name="Bytes read", num_entries=df_lenght, title="Disk usage")
+        plt.savefig(output_path + '/disk_usage.png')
         plt.close()
 
         build_plot('Disk usage: bytes written', timestamps, byte_write_disk, name_dataset='BYTE_WRITE_DISK', measure='Byte (B)', num_entries=df_lenght)
@@ -113,6 +192,21 @@ def plot_results(folder_pathname):
         build_plot('Disk usage: bytes read', timestamps, byte_read_disk, name_dataset='BYTE_READ_DISK', measure='Byte (B)', num_entries=df_lenght)
         plt.savefig(output_path + '/bytes_read.png')
         plt.close()
+
+    len_lists = len(cpu_usage)
+    for _, l in list_of_cpus.items():
+        len_lists = len(l) if len(l) < len_lists else len_lists
+
+    colors = list(mcolors.TABLEAU_COLORS.values())
+    plt.style.use('ggplot')
+
+    build_plot_nodes(list(timestamps[:len_lists]), df_list=list_of_cpus, num_entries=len_lists, colors=colors, metric_name="CPU")
+    plt.savefig(plots_pathname + 'cpu_nodes.png')
+    plt.close()
+
+    build_plot_nodes(list(timestamps[:len_lists]), df_list=list_of_mems, num_entries=len_lists, colors=colors, metric_name="memory")
+    plt.savefig(plots_pathname + 'mem_nodes.png')
+    plt.close()
 
     return plots_pathname
 

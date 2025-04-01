b33bcfa2cda4a7fbe9acbb00bae6a114468e0751
nicologiacomini <nicolo.giacomini@bsc.es>
2025-04-01 09:55:55 +0200

    fix: get command line from crate

--- compss/tools/reproducibility_service/reproducibility_methods/generate_command_line.py
+++ compss/tools/reproducibility_service/reproducibility_methods/generate_command_line.py
@@ -94,6 +94,12 @@ def generate_command_line(self, sub_directory_path: str) -> list[str]:
 
     # compss_submission_command = clean_command(link_or_path, new_command.split(" "))
 
+    new_command = ""
+    crate = ROCrate(path)
+    for e in crate.get_entities():
+        if "COMPSs_Workflow" in e.id:
+            new_command = e["description"]
+
     # compss_submission_command_path = os.path.join(path, "compss_submission_command_line.txt")
 
     # with open(compss_submission_command_path, 'r', encoding='utf-8') as file:

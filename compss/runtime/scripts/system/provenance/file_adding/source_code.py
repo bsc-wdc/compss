d962b8043a2e1bb5230fd1cd5b214721dbdba605
nicologiacomini <nicolo.giacomini@bsc.es>
2025-04-02 19:28:54 +0200

    fix: add auxiliary files, main entity included

--- compss/runtime/scripts/system/provenance/file_adding/source_code.py
+++ compss/runtime/scripts/system/provenance/file_adding/source_code.py
@@ -182,10 +182,11 @@ def add_file_to_crate(
     else:
         path_in_crate = "application_sources/" + file_path.name
 
+    auxiliary_file_list.append(path_in_crate)
+
     if file_name != main_entity:
         # Or check if the file is an executable (.py or .java)
         # if file_name.endswith(".py") or file_name.endswith(".java")
-        auxiliary_file_list.append(path_in_crate)
         if __debug__:
             print(f"PROVENANCE DEBUG | Adding auxiliary source file: {file_name}")
         compss_crate.add_file(

1a46ce079894498da41b0fb9f9d3b4f2e0b71905
nicologiacomini <nicolo.giacomini@bsc.es>
2025-04-24 14:54:42 +0200

    fix python style

--- compss/tools/reproducibility_service/remote_dataset.py
+++ compss/tools/reproducibility_service/remote_dataset.py
@@ -27,7 +27,7 @@ from rocrate.rocrate import ROCrate
 from utils import download_file, get_Create_Action, print_colored, TextColor, get_by_id
 
 
-def remote_dataset(crate: ROCrate,crate_directory: str) -> bool:
+def remote_dataset(crate: ROCrate, crate_directory: str) -> bool:
     """
     Download the remote datasets mentioned in the metadata file and verify their size.
     Args:
@@ -39,27 +39,42 @@ def remote_dataset(crate: ROCrate,crate_directory: str) -> bool:
     """
 
     create_action = get_Create_Action(crate)
-    remote_datasets= {}
-    if "object" in  create_action:
+    remote_datasets = {}
+    if "object" in create_action:
         temp = create_action["object"]
     else:
         return (False, {})
 
-    for input in temp: # get the remote_datasets mapped with their names
+    for input in temp:  # get the remote_datasets mapped with their names
         if (input.id).startswith("http"):
             remote_datasets[input["name"]] = input.id
 
-    for key,val in remote_datasets.items(): # download the remote datasets with the specified names
+    for (
+        key,
+        val,
+    ) in (
+        remote_datasets.items()
+    ):  # download the remote datasets with the specified names
         try:
-            print_colored(f"Please wait while remote file {key} is being downloaded from {val} ...", TextColor.YELLOW)
-            download_file(val, os.path.join(crate_directory,"remote_dataset")
-                          ,key)
+            print_colored(
+                f"Please wait while remote file {key} is being downloaded from {val} ...",
+                TextColor.YELLOW,
+            )
+            download_file(val, os.path.join(crate_directory, "remote_dataset"), key)
 
-            if "contentSize" in get_by_id(crate,val):
-                if get_by_id(crate,val)["contentSize"] == os.path.getsize(os.path.join(crate_directory,"remote_dataset",key)):
-                    print_colored(f"Remote file {key} has been successfully downloaded with size verified.", TextColor.GREEN)
+            if "contentSize" in get_by_id(crate, val):
+                if get_by_id(crate, val)["contentSize"] == os.path.getsize(
+                    os.path.join(crate_directory, "remote_dataset", key)
+                ):
+                    print_colored(
+                        f"Remote file {key} has been successfully downloaded with size verified.",
+                        TextColor.GREEN,
+                    )
             else:
-                print_colored(f"Remote file {key} has been successfully downloaded.Could not verify size as it is not mentioned in the metadata", TextColor.GREEN)
+                print_colored(
+                    f"Remote file {key} has been successfully downloaded.Could not verify size as it is not mentioned in the metadata",
+                    TextColor.GREEN,
+                )
 
         except ValueError:
             print(f"Remote dataset {key} could not be downloaded.")

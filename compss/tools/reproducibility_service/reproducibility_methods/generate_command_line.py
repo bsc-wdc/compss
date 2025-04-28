f1f9ff2c113ce3cd82d769bccedcc6b2a9b18617
nicologiacomini <nicolo.giacomini@bsc.es>
2025-04-28 12:30:14 +0200

    update method description

--- compss/tools/reproducibility_service/reproducibility_methods/generate_command_line.py
+++ compss/tools/reproducibility_service/reproducibility_methods/generate_command_line.py
@@ -41,7 +41,7 @@ from .utilsr import get_file_names, get_results_dict, check_slurm_cluster
 def generate_command_line(self, sub_directory_path: str) -> list[str]:
     """
     Generates a modified command line based on the contents of a compss_submission_command_line.txt file, if it exists.
-    In alternative, the command line is parsed from thr ro-crate-metadata.json, in the description of the workflow.
+    In alternative, the command line is parsed from the ro-crate-metadata.json, in the description of CreateAction.
     The final command line includes mappings of dataset and application sources found in the crate directory.
 
     Args:

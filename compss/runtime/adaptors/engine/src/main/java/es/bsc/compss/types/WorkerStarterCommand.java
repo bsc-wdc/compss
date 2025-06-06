be1dc271052cef97a06ec6cd559c21f031772c42
nicologiacomini <nicolo.giacomini@bsc.es>
2025-06-06 12:42:09 +0200

    add dataProvenance variable
    (cherry picked from commit 51317afa9488635b5b329eedd9b245f792a9deb0)

--- compss/runtime/adaptors/engine/src/main/java/es/bsc/compss/types/WorkerStarterCommand.java
+++ compss/runtime/adaptors/engine/src/main/java/es/bsc/compss/types/WorkerStarterCommand.java
@@ -118,6 +118,7 @@ public abstract class WorkerStarterCommand implements StarterCommand {
     protected String pythonWorkerCache;
     protected String pythonCacheProfiler;
     protected String ear;
+    protected String dataProvenance;
     protected int totalCPU;
     protected int totalGPU;
     protected int totalFPGA;
@@ -313,6 +314,12 @@ public abstract class WorkerStarterCommand implements StarterCommand {
         this.ear = System.getProperty(COMPSsConstants.EAR);
         if (this.ear == null || this.ear.isEmpty() || this.ear.equals("null")) {
             this.ear = COMPSsDefaults.EAR;
+        }
+
+        // Configure provenance
+        this.dataProvenance = System.getProperty(COMPSsConstants.DATA_PROVENANCE);
+        if (this.dataProvenance == null || this.dataProvenance.isEmpty() || this.dataProvenance.equals("null")) {
+            this.dataProvenance = COMPSsDefaults.DP_ENABLED;
         }
 
         this.lang = System.getProperty(COMPSsConstants.LANG);

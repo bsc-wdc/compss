0eccef22a431928f4b5624ca398185c2fb45e8b0
Cristian Tatu <ctatu@bsc.es>
2025-11-19 10:58:29 +0100

    update CLI build.sh for pyproject.toml

--- builders/specs/cli/PyCOMPSsCLIResources/build.sh
+++ builders/specs/cli/PyCOMPSsCLIResources/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Installing required `build` package..."
+
+python3 -m pip install build
+
 echo "Building..."
 
-python3 setup.py build
+python3 -m build
 
 echo "----- Building finished -----"

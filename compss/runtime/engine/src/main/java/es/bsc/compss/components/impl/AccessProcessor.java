6ebc2066e19f23cee42610b512e2dfc6c94d096b
Francesc Lordan Gomis <francesc.lordan@gmail.com>
2025-11-20 06:18:11 +0100

    AP offer method

--- compss/runtime/engine/src/main/java/es/bsc/compss/components/impl/AccessProcessor.java
+++ compss/runtime/engine/src/main/java/es/bsc/compss/components/impl/AccessProcessor.java
@@ -161,7 +161,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
                 ErrorManager.error("Exception", e);
             } finally {
                 if (Tracer.isActivated()) {
-                    Tracer.emitEventEnd(TraceEventType.RUNTIME);
+                    Tracer.emitEventEnd(request.getEvent());
                 }
             }
 
@@ -170,6 +170,14 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
             Tracer.emitEventEnd(TraceEvent.AP_THREAD_ID);
         }
         LOGGER.info("AccessProcessor shutdown");
+    }
+
+    private boolean offerRequest(APRequest req, String errMsg) {
+        if (!this.requestQueue.offer(req)) {
+            ErrorManager.error(ERROR_QUEUE_OFFER + errMsg);
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -215,10 +223,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
         app.onTaskCreation(currentTask);
 
         LOGGER.debug("Requesting analysis of Task " + currentTask.getId());
-        if (!this.requestQueue.offer(new TaskAnalysisRequest(currentTask))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "new method task");
-        }
-
+        this.offerRequest(new TaskAnalysisRequest(currentTask), "new method task");
         return currentTask.getId();
     }
 
@@ -245,10 +250,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
         app.onTaskCreation(currentTask);
 
         LOGGER.debug("Requesting analysis of new HTTP Task " + currentTask.getId());
-
-        if (!this.requestQueue.offer(new TaskAnalysisRequest(currentTask))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "new HTTP task");
-        }
+        this.offerRequest(new TaskAnalysisRequest(currentTask), "new HTTP task");
         return currentTask.getId();
     }
 
@@ -258,9 +260,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      * @param task Ended task.
      */
     public void notifyTaskEnd(AbstractTask task) {
-        if (!this.requestQueue.offer(new TaskEndNotification(task))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "notify task end");
-        }
+        this.offerRequest(new TaskEndNotification(task), "notify task end");
     }
 
     /**
@@ -303,9 +303,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      * @param ma Access parameters.
      */
     public void finishDataAccess(MainAccess ma, EngineDataInstanceId generatedDaId) {
-        if (!this.requestQueue.offer(new FinishDataAccessRequest(ma, generatedDaId))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "finishing data access");
-        }
+        this.offerRequest(new FinishDataAccessRequest(ma, generatedDaId), "finishing data access");
     }
 
     /**
@@ -318,10 +316,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
     public LogicalData getDataLastVersion(Application app, DataParams data) {
         // Ask for the object
         DataGetLastVersionRequest odr = new DataGetLastVersionRequest(app, data);
-        if (!this.requestQueue.offer(odr)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "data version query");
-        }
-
+        this.offerRequest(odr, "data version query");
         return odr.getData();
     }
 
@@ -334,12 +329,8 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void barrierGroup(Application app, String groupName) throws COMPSsException {
         BarrierGroupRequest bgr = new BarrierGroupRequest(app, groupName);
-        if (!requestQueue.offer(bgr)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "wait for all tasks");
-        }
-
+        this.offerRequest(bgr, "wait for all tasks");
         bgr.waitForCompletion();
-
         LOGGER.info("Group barrier: End of tasks of group " + groupName);
     }
 
@@ -350,9 +341,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void barrier(Application app) {
         BarrierRequest br = new BarrierRequest(app);
-        if (!this.requestQueue.offer(br)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "wait for all tasks");
-        }
+        this.offerRequest(br, "wait for all tasks");
 
         try {
             br.waitForCompletion();
@@ -370,10 +359,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void noMoreTasks(Application app) {
         EndOfAppRequest eoar = new EndOfAppRequest(app);
-        if (!this.requestQueue.offer(eoar)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "no more tasks");
-        }
-
+        this.offerRequest(eoar, "no more tasks");
         try {
             eoar.waitForCompletion();
         } catch (COMPSsException ce) {
@@ -392,9 +378,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public boolean alreadyAccessed(Application app, DataParams data) {
         AlreadyAccessedRequest request = new AlreadyAccessedRequest(app, data);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "already accessed location");
-        }
+        this.offerRequest(request, "already accessed location");
 
         // Wait for response
         return request.getResponse();
@@ -410,9 +394,8 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
         LOGGER.info("Cancelled all remaining tasks for application with id " + appId);
 
         Semaphore sem = new Semaphore(0);
-        if (!this.requestQueue.offer(new CancelApplicationTasksRequest(app, sem))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "wait for task");
-        }
+        this.offerRequest(new CancelApplicationTasksRequest(app, sem), "wait for task");
+
         // Wait for response
         LOGGER.debug("Waiting for finishing tasks cancellation " + appId);
         sem.acquireUninterruptibly();
@@ -431,9 +414,8 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
         LOGGER.info("Cancel remaining tasks for application " + appId + " and group " + groupName);
 
         Semaphore sem = new Semaphore(0);
-        if (!this.requestQueue.offer(new CancelTaskGroupRequest(app, groupName, sem))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "wait for task");
-        }
+        this.offerRequest(new CancelTaskGroupRequest(app, groupName, sem), "wait for task");
+
         // Wait for response
         LOGGER.debug("Waiting for cancellation of tasks in group " + groupName);
         sem.acquireUninterruptibly();
@@ -450,9 +432,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     private EngineDataAccessId registerDataAccess(MainAccess access) throws ValueUnawareRuntimeException {
         RegisterDataAccessRequest request = new RegisterDataAccessRequest(access);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "register data access");
-        }
+        this.offerRequest(request, "register data access");
 
         // Wait for response
         request.waitForCompletion();
@@ -469,9 +449,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void setCurrentTaskGroup(String groupName, Application app) {
         OpenTaskGroupRequest request = new OpenTaskGroupRequest(groupName, app);
-        if (!requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "new task group");
-        }
+        this.offerRequest(request, "new task group");
     }
 
     /**
@@ -481,9 +459,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void closeCurrentTaskGroup(Application app) {
         CloseTaskGroupRequest request = new CloseTaskGroupRequest(app);
-        if (!requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "closure of task group");
-        }
+        this.offerRequest(request, "closure of task group");
     }
 
     /**
@@ -493,9 +469,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void unblockResultFiles(List<ResultFile> resFiles) {
         UnblockResultFilesRequest request = new UnblockResultFilesRequest(resFiles);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "unblock result files");
-        }
+        this.offerRequest(request, "unblock result files");
     }
 
     /**
@@ -503,9 +477,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void shutdown() {
         shutdownSemaphore = new Semaphore(0);
-        if (!this.requestQueue.offer(new ShutdownRequest(shutdownSemaphore))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "shutdown");
-        }
+        this.offerRequest(new ShutdownRequest(shutdownSemaphore), "shutdown");
 
         // Wait for response
         shutdownSemaphore.acquireUninterruptibly();
@@ -519,9 +491,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
     public String getCurrentTaskState() {
         Semaphore sem = new Semaphore(0);
         TasksStateRequest request = new TasksStateRequest(sem);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "get current task state");
-        }
+        this.offerRequest(request, "get current task state");
 
         // Wait for response
         sem.acquireUninterruptibly();
@@ -545,9 +515,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
         if (enableReuse) {
             WaitForDataReadyToDeleteRequest request = new WaitForDataReadyToDeleteRequest(app, data);
             // Wait for data to be ready for deletion
-            if (!this.requestQueue.offer(request)) {
-                ErrorManager.error(ERROR_QUEUE_OFFER + "wait for data ready to delete");
-            }
+            this.offerRequest(request, "wait for data ready to delete");
             try {
                 request.waitForDataReadiness();
             } catch (ValueUnawareRuntimeException vure) {
@@ -566,9 +534,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
         // Request to delete data
         LOGGER.debug("Sending delete request for " + data.getDescription());
         DeleteDataRequest req = new DeleteDataRequest(app, data, applicationDelete);
-        if (!this.requestQueue.offer(req)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "mark for deletion");
-        }
+        this.offerRequest(req, "mark for deletion");
 
         // No need to wait if no reuse
         if (enableReuse && delete) {
@@ -590,17 +556,13 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
     public void getResultFiles(Application app) {
         Semaphore sem = new Semaphore(0);
         GetResultFilesRequest request = new GetResultFilesRequest(app, sem);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "get result files");
-        }
+        this.offerRequest(request, "get result files");
 
         // Wait for response
         sem.acquireUninterruptibly();
 
         UnblockResultFilesRequest urfr = new UnblockResultFilesRequest(request.getBlockedData());
-        if (!this.requestQueue.offer(urfr)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "unlock result files");
-        }
+        this.offerRequest(urfr, "unlock result files");
     }
 
     /**
@@ -612,9 +574,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      */
     public void registerRemoteData(Application app, DataParams accessedValue, String dataId) {
         RegisterRemoteDataRequest request = new RegisterRemoteDataRequest(app, accessedValue, dataId);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "register data");
-        }
+        this.offerRequest(request, "register data");
     }
 
     /**
@@ -625,9 +585,7 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
     public void deleteAllApplicationDataRequest(Application app) {
         Long appId = app.getId();
         DeleteAllApplicationDataRequest request = new DeleteAllApplicationDataRequest(app);
-        if (!this.requestQueue.offer(request)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "delete all data from application " + appId);
-        } else {
+        if (this.offerRequest(request, "delete all data from application " + appId)) {
             request.waitForCompletion();
         }
     }
@@ -638,23 +596,17 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
      * @param app Application .
      */
     public void snapshot(Application app) {
-        if (!this.requestQueue.offer(new SnapshotRequest(app))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "snapshot");
-        }
+        this.offerRequest(new SnapshotRequest(app), "snapshot");
     }
 
     @Override
     public void addCheckpointRequest(APRequest apRequest, String errorMessage) {
-        if (!requestQueue.offer(apRequest)) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + errorMessage);
-        }
+        this.offerRequest(apRequest, errorMessage);
     }
 
     @Override
     public void allAvailableDataCheckpointed() {
-        if (!this.requestQueue.offer(new ShutdownNotificationRequest(shutdownSemaphore))) {
-            ErrorManager.error(ERROR_QUEUE_OFFER + "shutdown");
-        }
+        this.offerRequest(new ShutdownNotificationRequest(shutdownSemaphore), "shutdown");
     }
 
     /**
@@ -698,22 +650,18 @@ public class AccessProcessor implements Runnable, CheckpointManager.User {
                 for (String param : params) {
                     String[] values = param.split(":");
                     if (values[0].equals("period.time")) {
+                        int unit;
                         if (values[1].endsWith("h")) {
-                            values[1] = String.valueOf(
-                                Integer.parseInt(values[1].substring(0, values[1].length() - 1)) * 3600 * 1000);
+                            unit = 3_600_000;
                         } else {
-                            if (values[1].endsWith("m")) {
-                                values[1] = String.valueOf(
-                                    Integer.parseInt(values[1].substring(0, values[1].length() - 1)) * 60 * 1000);
+                            if (values[1].endsWith("s")) {
+                                unit = 1_000;
                             } else {
-                                if (values[1].endsWith("s")) {
-                                    values[1] = String.valueOf(
-                                        Integer.parseInt(values[1].substring(0, values[1].length() - 1)) * 1000);
-                                } else {
-                                    values[1] = String.valueOf(Integer.parseInt(values[1]) * 60 * 1000);
-                                }
+                                unit = 60_000;
                             }
                         }
+                        int period = Integer.parseInt(values[1].substring(0, values[1].length() - 1)) * unit;
+                        values[1] = String.valueOf(period);
                     }
                     paramsMap.put(values[0], values[1]);
                 }

47ee2be1b7e3fe17c619893ba20ad44f31980bb8
fconejer <francisco.conejero@bsc.es>
2026-02-09 16:15:06 +0100

    Updated headers

--- compss/runtime/engine/src/main/java/es/bsc/compss/components/impl/socketserver/ipc/Server.java
+++ compss/runtime/engine/src/main/java/es/bsc/compss/components/impl/socketserver/ipc/Server.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 Francesc Lordan Gomis
+ *  Copyright 2002-2026 Barcelona Supercomputing Center (www.bsc.es)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- compss/runtime/scheduler/predefined/base/src/main/java/es/bsc/compss/scheduler/predefined/PredefinedSchedulingInformation.java
+++ compss/runtime/scheduler/predefined/base/src/main/java/es/bsc/compss/scheduler/predefined/PredefinedSchedulingInformation.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2002-2024 Barcelona Supercomputing Center (www.bsc.es)
+ *  Copyright 2002-2026 Barcelona Supercomputing Center (www.bsc.es)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
